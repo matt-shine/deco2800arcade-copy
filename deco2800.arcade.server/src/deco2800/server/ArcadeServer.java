a27e38aa244f2a1a04d75c5dbd009391bdebe00d
phirrip <ein.hexe@gmail.com>
2013-07-23 10:16:32 +1000

    Initial work on using the JDBC to get credit data from a database

--- deco2800.arcade.server/src/deco2800/server/ArcadeServer.java
+++ deco2800.arcade.server/src/deco2800/server/ArcadeServer.java
@@ -13,6 +13,8 @@ import deco2800.arcade.protocol.connect.ConnectionRequest;
 import deco2800.arcade.protocol.connect.ConnectionResponse;
 import deco2800.arcade.protocol.game.NewGameRequest;
 import deco2800.arcade.protocol.game.NewGameResponse;
+import deco2800.server.database.CreditStorage;
+import deco2800.server.database.DatabaseException;
 import deco2800.server.listener.ConnectionListener;
 
 public class ArcadeServer {
@@ -35,19 +37,13 @@ public class ArcadeServer {
 		
 		server.addListener(new ConnectionListener(connectedUsers));
 		
-		server.addListener(new Listener() {
-			public void received(Connection connection, Object object) {
-				if (object instanceof ConnectionRequest) {
-					ConnectionRequest request = (ConnectionRequest) object;
-					System.out.println("Connection request for user: " + request.username);
-					connectedUsers.add(request.username);
-					connection.sendTCP(ConnectionResponse.OK);
-					System.out.println("Connection granted");
-				} else if (object instanceof NewGameRequest) {
-					connection.sendTCP(NewGameResponse.OK);
-				}
-			}
-		});
+		try {
+			Integer credits = CreditStorage.getUserCredits("Bob");
+			System.out.println(credits == null ? "Null result" : credits);
+		} catch (DatabaseException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 		
 	}
 	

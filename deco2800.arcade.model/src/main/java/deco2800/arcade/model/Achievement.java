cbabaf0befd84bd2ece7ab1b43e0b2d92d8b370c
Leggy <lachlan.j.healey@gmail.com>
2013-07-29 13:13:19 +1000

    Documentation for Arcade.Model.Achievement

--- deco2800.arcade.model/src/main/java/deco2800/arcade/model/Achievement.java
+++ deco2800.arcade.model/src/main/java/deco2800/arcade/model/Achievement.java
@@ -2,16 +2,27 @@ package deco2800.arcade.model;
 
 public class Achievement {
 
-	//TODO shared between server & client?
-	
-	//TODO icon?
-	
+	// TODO shared between server & client?
+
+	// TODO icon?
+
 	private String description;
-	
+
+	/**
+	 * Creates a new Achievement given the description.
+	 * 
+	 * @param description
+	 *            The Achievement's description.
+	 */
 	public Achievement(String description) {
 		this.description = description;
 	}
-	
+
+	/**
+	 * Access method for Achievement's description.
+	 * 
+	 * @return The Achievement's description.
+	 */
 	public String getDescription() {
 		return description;
 	}

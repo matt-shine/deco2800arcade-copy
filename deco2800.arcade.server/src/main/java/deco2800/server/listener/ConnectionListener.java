411680a8d853341498852d93297564776d889ca4
Nathan Woodrow <nathanwoodrow@gmail.com>
2013-07-29 15:15:06 +1000

    commented connection listener
    added comments to connection listener

--- deco2800.arcade.server/src/main/java/deco2800/server/listener/ConnectionListener.java
+++ deco2800.arcade.server/src/main/java/deco2800/server/listener/ConnectionListener.java
@@ -9,7 +9,7 @@ import deco2800.arcade.protocol.connect.ConnectionRequest;
 import deco2800.arcade.protocol.connect.ConnectionResponse;
 
 public class ConnectionListener extends Listener {
-
+	//list of all connected users
 	private Set<String> connectedUsers;
 	
 	public ConnectionListener(Set<String> connectedUsers){
@@ -17,6 +17,13 @@ public class ConnectionListener extends Listener {
 	}
 	
 	@Override
+	/**
+	 * takes Connection object called connection and an Object called object
+	 * checks if object is an instance of ConnectionRequest. If true prints out
+	 * connection feedback and adds the user to the list of connected users.
+	 * Then sends a response to the users machine and prints out Connection
+	 * Granted. Else do nothing.
+	 */
 	public void received(Connection connection, Object object) {
 		super.received(connection, object);
 		

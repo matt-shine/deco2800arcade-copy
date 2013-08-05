9add12f7e35043bd9671b480323161071fb70b4a
Alex Hope-O'Connor <alex.hope.oconnor@live.com.au>
2013-08-05 21:26:22 +1000

    Fixed project files.

---
+++ deco2800.arcade.mixmaze/src/main/java/deco2800/arcade/mixmaze/MixMaze.java
@@ -0,0 +1,57 @@
+package deco2800.arcade.mixmaze;
+
+import deco2800.arcade.model.Game;
+import deco2800.arcade.model.Game.ArcadeGame;
+import deco2800.arcade.model.Player;
+import deco2800.arcade.client.GameClient;
+import deco2800.arcade.client.network.NetworkClient;
+
+@ArcadeGame(id="mixmaze")
+public class MixMaze extends GameClient {
+	public MixMaze(Player player, NetworkClient networkClient) {
+		super(player, networkClient);
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	public void create() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void dispose() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void pause() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void render() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void resize(int arg0, int arg1) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void resume() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public Game getGame() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+}

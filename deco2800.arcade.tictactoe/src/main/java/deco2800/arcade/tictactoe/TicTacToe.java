14b16ae33c3d1b49b51076bfa6a403a97f65fe7d
herotl111 <herotl111@hotmail.com>
2013-08-06 14:58:10 +1000

    testing commit

--- deco2800.arcade.tictactoe/src/main/java/deco2800/arcade/tictactoe/TicTacToe.java
+++ deco2800.arcade.tictactoe/src/main/java/deco2800/arcade/tictactoe/TicTacToe.java
@@ -62,11 +62,11 @@ public class TicTacToe extends GameClient {
 		dropImage = new Texture(Gdx.files.classpath("resources/droplet.png"));
 		bucketImage = new Texture(Gdx.files.classpath("resources/bucket.png"));
 		
-		// Load the drop sound effect and the rain background "music"
+		// load the drop sound effect and the rain background "music"
 		dropSound = Gdx.audio.newSound(Gdx.files.internal("resources/drop.wav"));
 		rainMusic = Gdx.audio.newMusic(Gdx.files.internal("resources/rain.mp3"));
 
-		// Start the playback of the background music immediately
+		// start the playback of the background music immediately
 		rainMusic.setLooping(true);
 		rainMusic.play();
 

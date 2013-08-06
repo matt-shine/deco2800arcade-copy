5729f783bf4a96b1b50a83b813bcb7b57096cfa0
Luran Li <luran.all@gmail.com>
2013-08-07 09:53:43 +1000

    Add Mix Maze to Arcade
    Game has yet to be built at the moment.
    User can select Mix Maze from Arcade now.

--- deco2800.arcade.mixmaze/src/main/java/deco2800/arcade/mixmaze/MixMaze.java
+++ deco2800.arcade.mixmaze/src/main/java/deco2800/arcade/mixmaze/MixMaze.java
@@ -1,5 +1,9 @@
 package deco2800.arcade.mixmaze;
 
+import java.util.HashSet;
+import java.util.Set;
+
+import deco2800.arcade.model.Achievement;
 import deco2800.arcade.model.Game;
 import deco2800.arcade.model.Game.ArcadeGame;
 import deco2800.arcade.model.Player;
@@ -16,42 +20,52 @@ public class MixMaze extends GameClient {
 	@Override
 	public void create() {
 		// TODO Auto-generated method stub
-		
+
 	}
 
 	@Override
 	public void dispose() {
 		// TODO Auto-generated method stub
-		
+
 	}
 
 	@Override
 	public void pause() {
 		// TODO Auto-generated method stub
-		
+
 	}
 
 	@Override
 	public void render() {
 		// TODO Auto-generated method stub
-		
+
 	}
 
 	@Override
 	public void resize(int arg0, int arg1) {
 		// TODO Auto-generated method stub
-		
+
 	}
 
 	@Override
 	public void resume() {
-		// TODO Auto-generated method stub
-		
+		// This method will never be called on the desktop.
+	}
+
+	private static Set<Achievement> achievements =
+			new HashSet<Achievement>();
+
+	private static final Game game;
+	static {
+		game = new Game();
+		game.gameId = "mixmaze";
+		game.name = "Mix Maze";
+		game.availableAchievements = achievements;
 	}
 
 	@Override
 	public Game getGame() {
 		// TODO Auto-generated method stub
-		return null;
+		return game;
 	}
 }

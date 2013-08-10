077cbb063dcc489992a455e22d34968d500feddd
Luran Li <luran.all@gmail.com>
2013-08-10 16:41:35 +1000

    Add simple drawing to MixMaze
    This should not really be a commit, but I cannot find a way to checkout
    another branch with uncommitted files.

--- deco2800.arcade.mixmaze/src/main/java/deco2800/arcade/mixmaze/MixMaze.java
+++ deco2800.arcade.mixmaze/src/main/java/deco2800/arcade/mixmaze/MixMaze.java
@@ -3,6 +3,13 @@ package deco2800.arcade.mixmaze;
 import java.util.HashSet;
 import java.util.Set;
 
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Input;
+import com.badlogic.gdx.graphics.GL10;
+import com.badlogic.gdx.graphics.OrthographicCamera;
+import com.badlogic.gdx.graphics.glutils.ShapeRenderer;
+import com.badlogic.gdx.graphics.glutils.ShapeRenderer.ShapeType;
+
 import deco2800.arcade.model.Achievement;
 import deco2800.arcade.model.Game;
 import deco2800.arcade.model.Game.ArcadeGame;
@@ -12,6 +19,10 @@ import deco2800.arcade.client.network.NetworkClient;
 
 @ArcadeGame(id="mixmaze")
 public class MixMaze extends GameClient {
+	private OrthographicCamera camera;
+	private ShapeRenderer shapeRenderer;
+	private int angle;
+
 	public MixMaze(Player player, NetworkClient networkClient) {
 		super(player, networkClient);
 		// TODO Auto-generated constructor stub
@@ -19,7 +30,18 @@ public class MixMaze extends GameClient {
 
 	@Override
 	public void create() {
-		// TODO Auto-generated method stub
+		super.create();
+
+		// Initialise camera
+		camera = new OrthographicCamera();
+		camera.setToOrtho(false);
+
+		shapeRenderer = new ShapeRenderer();
+
+		angle = 0;
+
+		System.out.println(Gdx.graphics.getHeight());
+		System.out.println(Gdx.graphics.getWidth());
 
 	}
 
@@ -37,8 +59,23 @@ public class MixMaze extends GameClient {
 
 	@Override
 	public void render() {
-		// TODO Auto-generated method stub
+		Gdx.gl.glClearColor(0, 0, 0, 1);
+		Gdx.gl.glClear(GL10.GL_COLOR_BUFFER_BIT);
 
+		camera.update();
+		shapeRenderer.setProjectionMatrix(camera.combined);
+
+		if (Gdx.input.isTouched()) {
+			angle = (angle + 15) % 360;
+			System.out.println("screen touched" + angle);
+		}
+
+		shapeRenderer.rotate(0, 0, 1, angle);
+
+		shapeRenderer.begin(ShapeType.Line);
+		shapeRenderer.setColor(1, 1, 0, 1);
+		shapeRenderer.line(10, 10, 630, 470);
+		shapeRenderer.end();
 	}
 
 	@Override

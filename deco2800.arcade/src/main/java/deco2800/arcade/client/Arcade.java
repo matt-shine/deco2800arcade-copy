e1e13d52442ed250e1616a9824df90570c3ae7d0
navrav <Naveen@Naveen-Kumars-MacBook-Pro-2.local>
2013-08-11 20:38:14 +1000

    Created Java Project
    Created the intial java project folder. Imported libGDX and created
    build.gradle file. DIDNT INCLUDE the project in the build.gradle of
    deco2800.arcade

--- deco2800.arcade/src/main/java/deco2800/arcade/client/Arcade.java
+++ deco2800.arcade/src/main/java/deco2800/arcade/client/Arcade.java
@@ -32,7 +32,7 @@ import deco2800.arcade.protocol.game.GameRequestType;
 import deco2800.arcade.protocol.game.NewGameRequest;
 
 /**
- * The client application for running arcade games. testing branches
+ * The client application for running arcade games.
  *
  */
 public class Arcade extends JFrame {

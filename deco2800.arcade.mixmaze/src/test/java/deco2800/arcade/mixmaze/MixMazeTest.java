db288501459d9c61ca9eaf707eb01df602238c91
Alex Hope-O'Connor <alex.hope.oconnor@live.com.au>
2013-08-12 15:38:34 +1000

    Added test dependencies.
    Added domain package.

--- deco2800.arcade.mixmaze/src/test/java/deco2800/arcade/mixmaze/MixMazeTest.java
+++ deco2800.arcade.mixmaze/src/test/java/deco2800/arcade/mixmaze/MixMazeTest.java
@@ -1,5 +1,18 @@
 package deco2800.arcade.mixmaze;
 
+import org.junit.Before;
+import org.junit.Test;
+import org.mockito.MockitoAnnotations;
+
 public class MixMazeTest {
+	@Before
+	public void setUp()
+	{
+		MockitoAnnotations.initMocks(this);
+	}
 	
+	@Test
+	public void test()
+	{
+	}
 }

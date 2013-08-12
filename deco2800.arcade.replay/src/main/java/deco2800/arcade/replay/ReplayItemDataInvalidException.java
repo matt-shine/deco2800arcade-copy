ed33c6887cdf2e1141d7f79fcf8dffca564dd03e
Ben Follington <bfollington@gmail.com>
2013-08-12 13:35:05 +1000

    Fixed for JRE 1.6

--- deco2800.arcade.replay/src/main/java/deco2800/arcade/replay/ReplayItemDataInvalidException.java
+++ deco2800.arcade.replay/src/main/java/deco2800/arcade/replay/ReplayItemDataInvalidException.java
@@ -2,29 +2,29 @@ package deco2800.arcade.replay;
 
 public class ReplayItemDataInvalidException extends RuntimeException {
 
-	public ReplayItemDataInvalidException() {
-		// TODO Auto-generated constructor stub
-	}
+    public ReplayItemDataInvalidException() {
+        // TODO Auto-generated constructor stub
+    }
 
-	public ReplayItemDataInvalidException(String message) {
-		super(message);
-		// TODO Auto-generated constructor stub
-	}
+    public ReplayItemDataInvalidException(String message) {
+        super(message);
+        // TODO Auto-generated constructor stub
+    }
 
-	public ReplayItemDataInvalidException(Throwable cause) {
-		super(cause);
-		// TODO Auto-generated constructor stub
-	}
+    public ReplayItemDataInvalidException(Throwable cause) {
+        super(cause);
+        // TODO Auto-generated constructor stub
+    }
 
-	public ReplayItemDataInvalidException(String message, Throwable cause) {
-		super(message, cause);
-		// TODO Auto-generated constructor stub
-	}
+    public ReplayItemDataInvalidException(String message, Throwable cause) {
+        super(message, cause);
+        // TODO Auto-generated constructor stub
+    }
 
-	public ReplayItemDataInvalidException(String message, Throwable cause,
-			boolean enableSuppression, boolean writableStackTrace) {
-		super(message, cause, enableSuppression, writableStackTrace);
-		// TODO Auto-generated constructor stub
-	}
+    public ReplayItemDataInvalidException(String message, Throwable cause,
+            boolean enableSuppression, boolean writableStackTrace) {
+        super(message, cause);
+        // TODO Auto-generated constructor stub
+    }
 
 }

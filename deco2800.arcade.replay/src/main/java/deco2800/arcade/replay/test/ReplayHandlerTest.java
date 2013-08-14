2835e8c3037c321258a2d42d066b6f82a193c35f
Ben Follington <bfollington@gmail.com>
2013-08-15 08:43:09 +1000

    Added build.gradle, jUnit does not work yet though.

---
+++ deco2800.arcade.replay/src/main/java/deco2800/arcade/replay/test/ReplayHandlerTest.java
@@ -0,0 +1,14 @@
+package deco2800.arcade.replay.test;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class ReplayHandlerTest {
+
+    @Test
+    public void test() {
+        fail("Not yet implemented");
+    }
+
+}

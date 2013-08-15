eb9b7246a0820d4ff0dc7888394bf7b48354426d
CSmits <carlie.smits@uqconnect.edu.au>
2013-08-15 21:57:15 +1000

    Brick class and image
    basic features for the brick

---
+++ deco2800.arcade.breakout/src/main/java/deco2800/arcade/breakout/Brick.java
@@ -0,0 +1,32 @@
+package deco2800.arcade.breakout;
+
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.Texture;
+
+public class Brick {
+	
+	private Texture bricksImage;
+	protected int x;
+	protected int y;
+	protected int width;
+	protected int height;
+	private boolean state;
+	
+	public Brick(int x, int y) {
+		this.bricksImage = new Texture(Gdx.files.classpath("resources/bricks.png"));
+		this.x = x;
+		this.y = y;
+		
+		this.width = bricksImage.getWidth();
+		this.height = bricksImage.getHeight();
+		this.state = true;
+	}
+	
+	public boolean getState() {
+		return this.state;
+	}
+	
+	public void setState(boolean state) {
+		this.state = state;
+	}
+}

c85e222c0aaeec065099f7d8164843d3da0e755a
abbjohn <abbjohn@gmail.com>
2013-08-19 13:44:42 +1000

    Preliminary work on changing login to be a standard form.

--- deco2800.arcade.ui/src/main/java/deco2800/arcade/arcadeui/LoginScreen.java
+++ deco2800.arcade.ui/src/main/java/deco2800/arcade/arcadeui/LoginScreen.java
@@ -2,14 +2,16 @@ package deco2800.arcade.arcadeui;
 
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Input.Keys;
-import com.badlogic.gdx.graphics.Color;
-import com.badlogic.gdx.graphics.GL10;
-import com.badlogic.gdx.graphics.OrthographicCamera;
+import com.badlogic.gdx.graphics.*;
 import com.badlogic.gdx.graphics.g2d.BitmapFont;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.graphics.glutils.ShapeRenderer;
 import com.badlogic.gdx.graphics.glutils.ShapeRenderer.ShapeType;
+import com.badlogic.gdx.scenes.scene2d.Actor;
+import com.badlogic.gdx.scenes.scene2d.Stage;
+import com.badlogic.gdx.scenes.scene2d.ui.*;
 
+import com.badlogic.gdx.scenes.scene2d.utils.ChangeListener;
 import deco2800.arcade.client.ArcadeSystem;
 import deco2800.arcade.client.GameScreen;
 
@@ -20,15 +22,64 @@ public class LoginScreen extends GameScreen {
 	private ShapeRenderer shapeRenderer;
 	private BitmapFont font;
 	private SpriteBatch batch;
+    private Skin skin; // Move this somewhere appropriate
+    private Stage stage;
 	
 	
 	
 	public LoginScreen() {
-		font = new BitmapFont(true);
-		batch = new SpriteBatch();
+        // Move skin stuff to an overall class
+        skin = new Skin();
+        Pixmap pixmap = new Pixmap(1, 1, Pixmap.Format.RGBA8888);
+        pixmap.setColor(Color.WHITE);
+        pixmap.fill();
+        skin.add("white", new Texture(pixmap));
+        skin.add("default", new BitmapFont());
+        Label.LabelStyle labelStyle = new Label.LabelStyle();
+        labelStyle.font = skin.getFont("default");
+        skin.add("default", labelStyle);
+        TextField.TextFieldStyle textFieldStyle = new TextField.TextFieldStyle();
+        textFieldStyle.font = skin.getFont("default");
+        skin.add("default", textFieldStyle);
+        TextButton.TextButtonStyle textButtonStyle = new TextButton.TextButtonStyle();
+        textButtonStyle.up = skin.newDrawable("white", Color.DARK_GRAY);
+        textButtonStyle.down = skin.newDrawable("white", Color.DARK_GRAY);
+        textButtonStyle.checked = skin.newDrawable("white", Color.BLUE);
+        textButtonStyle.over = skin.newDrawable("white", Color.LIGHT_GRAY);
+        textButtonStyle.font = skin.getFont("default");
+        skin.add("default", textButtonStyle);
+
+
+        stage = new Stage();
+        Gdx.input.setInputProcessor(stage);
+
+        Table table = new Table();
+        table.setFillParent(true);
+        stage.addActor(table);
+
+        Label usernameLabel = new Label("Username:", skin);
+        //TextField usernameText = new TextField("debuguser", skin);
+        Label passwordLabel = new Label("Password", skin);
+        //TextField passwordText = new TextField("", skin);
+        TextButton loginButton = new TextButton("Login", skin);
+        TextButton exitButton = new TextButton("Exit", skin);
+
+        table.add(usernameLabel);
+        //table.add(usernameText).width(100);
+        table.row();
+        table.add(passwordLabel);
+        //table.add(passwordText).width(100);
+        table.row();
+        table.add(loginButton);
+        table.add(exitButton);
+
+        loginButton.addListener(new ChangeListener() {
+            public void changed (ChangeEvent event, Actor actor) {
+                ArcadeSystem.login("debuguser");
+            }
+        });
 	}
-	
-	
+
 
 	@Override
 	public void show() {
@@ -47,42 +98,19 @@ public class LoginScreen extends GameScreen {
 	
 	@Override
 	public void render(float arg0) {
-		
-		camera.update();
-	    shapeRenderer.setProjectionMatrix(camera.combined);
-	    batch.setProjectionMatrix(camera.combined);
-	    
-		Gdx.gl.glClearColor(0, 0, 0.2f, 1);
 		Gdx.gl.glClear(GL10.GL_COLOR_BUFFER_BIT);
-		
-		//draw a placeholder shape
-	    shapeRenderer.begin(ShapeType.FilledRectangle);
-	    
-	    shapeRenderer.filledRect(100,
-	        100,
-	        getWidth() - 200,
-	        getHeight() - 200);
-	    
-	    shapeRenderer.end();
-	    
-	    batch.begin();
-	    font.setColor(Color.BLACK);
-	    font.draw(batch, "This is the login screen. Press space to log in as 'debuguser'.", 110, 110);
-	    batch.end();
-	    
-	    if (Gdx.input.isKeyPressed(Keys.SPACE)) {
-	    	ArcadeSystem.login("debuguser");
-	    }
-	    
+        stage.act(Gdx.graphics.getDeltaTime());
+        stage.draw();
+
 	    if (ArcadeSystem.isLoggedIn()) {
 	    	ArcadeSystem.goToGame("arcadeui");
 	    }
-	    
-	    
 	}
 
 	@Override
 	public void dispose() {
+        stage.dispose();
+        skin.dispose();
 	}
 
 	@Override

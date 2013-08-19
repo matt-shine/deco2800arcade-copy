c756de6f3ae674b6f00828cc508b91d4939da2fe
Steven Sheriff <sjs250@gmail>
2013-08-19 20:39:40 +1000

    added description to game, and get/set methods

--- deco2800.arcade.model/src/main/java/deco2800/arcade/model/Game.java
+++ deco2800.arcade.model/src/main/java/deco2800/arcade/model/Game.java
@@ -31,11 +31,47 @@ public class Game {
 	public @interface InternalGame {
 	}
 	
-
 	public String id; //A machine-processable identifier for the game
 	
 	public String name; //A human-readable name for the game
+	
+	public String description; //The description for the game
 		
 	public int pricePerPlay = 1;
+	
+	/**
+	 * Sets the description for a game
+	 * @param description
+	 */
+	public void setDescription(String description) {
+		this.description = description;
+		return;
+	}
+	
+	/**
+	 * returns the description of a game as a string
+	 * @return description
+	 */
+	public String getDescription() {
+		return description;
+	}
+	
+	/**
+	 * Sets the name of a game
+	 * @param name
+	 */
+	public void setName(String name) {
+		this.name = name;
+		return;
+	}
+	
+	/**
+	 * Returns the name of a game as a string
+	 * @return name
+	 */
+	public String getName() {
+		return name;
+	}
+	
 	
 }

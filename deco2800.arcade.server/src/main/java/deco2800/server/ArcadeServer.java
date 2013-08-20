b36abc470fe78c34a0927d6762b29689f87d1895
Ben Follington <bfollington@gmail.com>
2013-08-20 12:12:22 +1000

    Fixed post-merge

--- deco2800.arcade.server/src/main/java/deco2800/server/ArcadeServer.java
+++ deco2800.arcade.server/src/main/java/deco2800/server/ArcadeServer.java
@@ -11,6 +11,7 @@ import deco2800.arcade.protocol.Protocol;
 import deco2800.server.database.CreditStorage;
 import deco2800.server.database.DatabaseException;
 import deco2800.server.listener.CommunicationListener;
+import deco2800.server.listener.ReplayListener;
 import deco2800.server.listener.ConnectionListener;
 import deco2800.server.listener.CreditListener;
 import deco2800.server.listener.GameListener;
@@ -103,6 +104,7 @@ public class ArcadeServer {
 		server.addListener(new ConnectionListener(connectedUsers));
 		server.addListener(new CreditListener());
 		server.addListener(new GameListener());
+		server.addListener(new ReplayListener());
 		server.addListener(new CommunicationListener(server));
 	}
 }

595f0ec2e8d0e922e1611708e9a05989bc7cf3d9
Joeyn1993 <nicholas.joseph@uqconnect.edu.au>
2013-08-21 00:18:32 +1000

    Basic shell for listeners

---
+++ deco2800.arcade.protocol/src/main/java/deco2800/arcade/protocol/multiplayerGame/NewMultiGameRequest.java
@@ -0,0 +1,10 @@
+package deco2800.arcade.protocol.multiplayerGame;
+
+import deco2800.arcade.protocol.game.GameRequest;
+import deco2800.arcade.protocol.game.GameRequestType;
+
+public class NewMultiGameRequest extends GameRequest {
+
+	public GameRequestType requestType;
+	
+}

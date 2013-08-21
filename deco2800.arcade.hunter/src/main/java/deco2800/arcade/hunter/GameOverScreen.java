da9744ebb98925b610a23f16e1b83b5dd4112a0c
Nathan Essex <nathan@essex.id.au>
2013-08-21 00:15:36 +0000

    Started adding a basic skeleton for the code.

---
+++ deco2800.arcade.hunter/src/main/java/deco2800/arcade/hunter/GameOverScreen.java
@@ -0,0 +1,88 @@
+package deco2800.arcade.hunter;
+
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Screen;
+import com.badlogic.gdx.graphics.Color;
+import com.badlogic.gdx.graphics.GL10;
+import com.badlogic.gdx.graphics.OrthographicCamera;
+import com.badlogic.gdx.graphics.glutils.ShapeRenderer;
+import com.badlogic.gdx.graphics.glutils.ShapeRenderer.ShapeType;
+
+/**
+ * A Hunter game for use in the Arcade
+ * @author Nessex
+ *
+ */
+public class GameOverScreen implements Screen {
+	private OrthographicCamera camera;
+	private Hunter parent;
+	
+	private MenuInput input = new MenuInput();
+	
+	private ShapeRenderer shapeRenderer;
+	
+	public GameOverScreen(Hunter p){
+		parent = p;
+		//Initialise camera
+		camera = new OrthographicCamera();
+		camera.setToOrtho(false, parent.screenWidth, parent.screenHeight);
+		
+		shapeRenderer = new ShapeRenderer();
+	}
+
+	@Override
+	public void dispose() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void hide() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void pause() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void render(float delta) {
+		//Black background
+		Gdx.gl.glClearColor(0, 0, 0, 1);
+		Gdx.gl.glClear(GL10.GL_COLOR_BUFFER_BIT);
+		
+		shapeRenderer.setProjectionMatrix(camera.combined);
+	    
+	    shapeRenderer.begin(ShapeType.FilledRectangle);
+	    shapeRenderer.setColor(Color.RED);
+	    shapeRenderer.filledRect(0, 0, 100, 100);
+	    
+	    shapeRenderer.setColor(Color.BLUE);
+	    shapeRenderer.filledRect(parent.screenWidth - 100, parent.screenHeight - 100, 100, 100);
+	    
+	    shapeRenderer.end();
+		
+	}
+
+	@Override
+	public void resize(int width, int height) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void resume() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void show() {
+		// TODO Auto-generated method stub
+		
+	}
+	
+}
--- deco2800.arcade.hunter/src/main/java/deco2800/arcade/hunter/Hunter.java
+++ deco2800.arcade.hunter/src/main/java/deco2800/arcade/hunter/Hunter.java
@@ -1,24 +1,10 @@
 package deco2800.arcade.hunter;
 
-import java.util.HashSet;
-import java.util.Set;
+import com.badlogic.gdx.scenes.scene2d.InputListener;
 
-import com.badlogic.gdx.Gdx;
-import com.badlogic.gdx.graphics.Color;
-import com.badlogic.gdx.graphics.GL10;
-import com.badlogic.gdx.graphics.OrthographicCamera;
-import com.badlogic.gdx.graphics.g2d.BitmapFont;
-import com.badlogic.gdx.graphics.g2d.SpriteBatch;
-import com.badlogic.gdx.graphics.glutils.ShapeRenderer;
-import com.badlogic.gdx.graphics.glutils.ShapeRenderer.ShapeType;
-import com.badlogic.gdx.math.Vector2;
-
-import deco2800.arcade.model.Achievement;
 import deco2800.arcade.model.Game;
 import deco2800.arcade.model.Game.ArcadeGame;
 import deco2800.arcade.model.Player;
-import deco2800.arcade.protocol.achievement.AddAchievementRequest;
-import deco2800.arcade.protocol.game.GameStatusUpdate;
 import deco2800.arcade.client.GameClient;
 import deco2800.arcade.client.network.NetworkClient;
 /**
@@ -27,40 +13,14 @@ import deco2800.arcade.client.network.NetworkClient;
  *
  */
 @ArcadeGame(id="hunter")
-public class Hunter extends GameClient {
+public class Hunter extends GameClient {	
+	private String username = new String();
+	private MenuScreen menuScreen;
+	private GameScreen gameScreen;
+	private GameOverScreen gameOverScreen;
 	
-	private OrthographicCamera camera;
+	int screenWidth, screenHeight;
 	
-	private String username = new String(); // The names of the players: the local player is always players[0]
-
-	private HunterGame game;
-
-	private enum States = {
-		SPLASH_SCREEN, //also loading screen
-		MENU,
-		GAME_RUNNING,
-		GAME_PAUSED,
-		GAME_ENDED
-	};
-
-	private int state = 0;
-	
-	public static final int SCREENHEIGHT = 480;
-	public static final int SCREENWIDTH = 800;
-
-	//Reusable list of achievements
-	private static Set<Achievement> achievements = new HashSet<Achievement>();
-	static {
-		Achievement huntTwenty = new Achievement("Hunt down 20 animals in one game."); //Example achievement
-		achievements.add(huntTwenty);
-		Achievement surviveOneMin = new Achievement("Survive for one minute.");
-		achievements.add(surviveOneMin);
-	}
-	
-	//Network client for communicating with the server.
-	//Should games reuse the client of the arcade somehow? Probably!
-	private NetworkClient networkClient;
-
 	/**
 	 * Basic constructor for the Hunter game
 	 * @param userName The name of the player
@@ -69,7 +29,6 @@ public class Hunter extends GameClient {
 	public Hunter(Player player, NetworkClient networkClient) {
 		super(player, networkClient);
 		username = player.getUsername();
-		this.networkClient = networkClient; //this is a bit of a hack
 	}
 	
 	/**
@@ -77,13 +36,16 @@ public class Hunter extends GameClient {
 	 */
 	@Override
 	public void create() {
-		super.create();
+		screenWidth = 640;
+		screenHeight = 480;
 		
-		//Initialise camera
-		camera = new OrthographicCamera();
-		camera.setToOrtho(false, SCREENWIDTH, SCREENHEIGHT);
-
-		startGame();
+		menuScreen = new MenuScreen(this);
+		gameScreen = new GameScreen(this);
+		gameOverScreen = new GameOverScreen(this);
+		
+		
+		setScreen(gameScreen);
+		super.create();
 	}
 
 	@Override
@@ -101,30 +63,8 @@ public class Hunter extends GameClient {
 	 */
 	@Override
 	public void render() {
-		//Black background
-		Gdx.gl.glClearColor(0, 0, 0, 1);
-	    Gdx.gl.glClear(GL10.GL_COLOR_BUFFER_BIT);
-
-	    camera.update();
-	    
-	    switch (state) {
-	    	case States.GAME_RUNNING:
-	    		game.render();
-	    }
 
 		super.render();	
-	}
-	
-	/**
-	 * Create an update object to send to the server notifying of a score change or game outcome
-	 * @return
-	 */
-	private GameStatusUpdate createScoreUpdate() {
-		GameStatusUpdate update = new GameStatusUpdate();
-		update.gameId = game.gameId;
-		update.username = username;
-		//TODO Should also send the score!
-		return update;
 	}
 
 	@Override
@@ -136,18 +76,12 @@ public class Hunter extends GameClient {
 	public void resume() {
 		super.resume();
 	}
-
-	private void startGame() {
-		game = new HunterGame();
-		state = States.GAME_RUNNING;
-	}
 	
 	private static final Game game;
 	static {
 		game = new Game();
-		game.gameId = "Hunter";
+		game.id = "Hunter";
 		game.name = "Hunter";
-		game.availableAchievements = achievements;
 	}
 	
 	public Game getGame() {

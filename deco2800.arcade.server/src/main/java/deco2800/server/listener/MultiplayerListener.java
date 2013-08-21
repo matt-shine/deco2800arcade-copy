51077c2bb59954049e94deda93c0bee1fabce602
Joeyn1993 <nicholas.joseph@uqconnect.edu.au>
2013-08-21 10:35:26 +1000

    Added Listener

--- deco2800.arcade.server/src/main/java/deco2800/server/listener/MultiplayerListener.java
+++ deco2800.arcade.server/src/main/java/deco2800/server/listener/MultiplayerListener.java
@@ -9,43 +9,60 @@ import com.esotericsoftware.kryonet.Connection;
 import com.esotericsoftware.kryonet.Listener;
 
 import deco2800.arcade.protocol.game.GameRequestType;
-import deco2800.arcade.protocol.game.GameStatusUpdate;
-import deco2800.arcade.protocol.game.GameStatusUpdateResponse;
-import deco2800.arcade.protocol.game.NewGameRequest;
-import deco2800.arcade.protocol.game.NewGameResponse;
 import deco2800.arcade.protocol.multiplayerGame.NewMultiGameRequest;
+import deco2800.arcade.protocol.multiplayerGame.NewMultiResponse;
 
 public class MultiplayerListener extends Listener {
-	private Map<String, Map<String, Set<Connection>>> queueSession;
+	//Map holding GameId, username, userconnection, and connection destination
+	private Map<String, Map<String, Connection[]>> queueSession;
 	
 	public MultiplayerListener() {
-		this.queueSession = new HashMap<String, Map<String, Set<Connection>>>();
+		this.queueSession = new HashMap<String, Map<String, Connection[]>>();
 	}
 	
 	@Override
 	/**
 	 * received takes a connection input and an object input and stores the data
-	 * in a userConnections map. 
+	 * in a queueSessions map. 
 	 * @require inputs are connection and object 
 	 */
 	public void received(Connection connection, Object object) {
 		super.received(connection, object);
 		
+		//Fill out map
 		if (object instanceof NewMultiGameRequest) {
 			NewMultiGameRequest multiRequest = (NewMultiGameRequest) object;
 			String username = multiRequest.username;
 			String gameId = multiRequest.gameId;
 			GameRequestType requestType = multiRequest.requestType;
+			Connection connectTo = multiRequest.connectTo;
 			
 			switch (requestType){
 			case NEW:
-				handleNewMultiRequest(connection, username, gameId);
+				handleNewMultiRequest(connection, username, gameId, connectTo);
 			}	
 			
 		}
 	}
 	
-	private void handleNewMultiRequest(Connection connection, String username, String gameId) {
+	/*For 2P Games with no MMR*/
+	private void handleNewMultiRequest(Connection connection, String username, String gameId,
+			Connection connectTo) {
+		if (queueSession.get(gameId) == null) {
+			Connection[] gameConnections = new Connection[2];
+			gameConnections[0] = connection;
+			gameConnections[1] = connectTo;
+			Map<String, Connection[]> userConnection = new HashMap<String, Connection[]>();
+			userConnection.put(username, gameConnections);
+			queueSession.put(gameId, userConnection);
+			return;
+		} else {
+			connection.sendTCP(connectTo);
+			connection.sendTCP(NewMultiResponse.OK);
+			connectTo.sendTCP(connection);
+			connectTo.sendTCP(NewMultiResponse.OK);
+		}
+		
 		return;
 	}
 	

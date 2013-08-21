5d74d5449bfd011828ad863e3ce19a8e07e8391d
Joeyn1993 <nicholas.joseph@uqconnect.edu.au>
2013-08-21 10:35:26 +1000

    Added Listener

--- deco2800.arcade.protocol/src/main/java/deco2800/arcade/protocol/multiplayerGame/NewMultiGameRequest.java
+++ deco2800.arcade.protocol/src/main/java/deco2800/arcade/protocol/multiplayerGame/NewMultiGameRequest.java
@@ -1,10 +1,13 @@
 package deco2800.arcade.protocol.multiplayerGame;
 
+import com.esotericsoftware.kryonet.Connection;
+
 import deco2800.arcade.protocol.game.GameRequest;
 import deco2800.arcade.protocol.game.GameRequestType;
 
 public class NewMultiGameRequest extends GameRequest {
 
 	public GameRequestType requestType;
+	public Connection connectTo;
 	
 }
---
+++ deco2800.arcade.protocol/src/main/java/deco2800/arcade/protocol/multiplayerGame/NewMultiResponse.java
@@ -0,0 +1,9 @@
+package deco2800.arcade.protocol.multiplayerGame;
+
+public enum NewMultiResponse {
+
+	OK,
+	UNAVAILABLE,
+	REFUSED,
+	ERROR;
+}

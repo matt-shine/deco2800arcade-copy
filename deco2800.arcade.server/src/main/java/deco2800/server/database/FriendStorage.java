0c24357fb59d4de66343fbc86d23207c87f9001a
Andrew Cleland <andrew.cleland3@gmail.com>
2013-08-21 20:21:12 +1000

    Added new methods to FriendStorage class to find friends in the database.

--- deco2800.arcade.server/src/main/java/deco2800/server/database/FriendStorage.java
+++ deco2800.arcade.server/src/main/java/deco2800/server/database/FriendStorage.java
@@ -4,6 +4,8 @@ import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * FriendStorage handles database access for Friends List data
@@ -34,8 +36,8 @@ public class FriendStorage {
 				statement.execute("CREATE TABLE FRIENDS(U1 INT NOT NULL,"
 						+ "U2 INT NOT NULL," 
 						+ "PRIMARY KEY(U1, U2),"
-						+ "FOREIGN KEY U1 REFERENCES FRIENDS(playerID),"
-						+ "FOREIGN KEY U2 REFERENCES FRIENDS(playerID)");
+						+ "FOREIGN KEY U1 REFERENCES PLAYER(playerID),"
+						+ "FOREIGN KEY U2 REFERENCES PLAYER(playerID)");
 			}
 		} catch (SQLException e) {
 			e.printStackTrace();
@@ -45,4 +47,70 @@ public class FriendStorage {
 	}
 	
 	
+	/**
+	 * Returns a list of friends for the given playerID
+	 * 
+	 * @param playerID
+	 * @return 
+	 * 		Returns List of friends
+	 * @throws DatabaseException
+	 */
+	public List<String> getFriendsList(int playerID) throws DatabaseException {
+		if (!initialised) {
+			initialise();
+		}
+
+		// Get a connection to the database
+		Connection connection = Database.getConnection();
+
+		Statement statement = null;
+		ResultSet resultSet = null;
+		try {
+			statement = connection.createStatement();
+			resultSet = statement.executeQuery("SELECT * from PLAYERS;");
+			return findFriends(playerID, resultSet);
+		} catch (SQLException e) {
+			e.printStackTrace();
+			throw new DatabaseException(
+					"Unable to get player informtion from database", e);
+		} finally {
+			try {
+				if (resultSet != null) {
+					resultSet.close();
+				}
+				if (statement != null) {
+					statement.close();
+				}
+				if (connection != null) {
+					connection.close();
+				}
+			} catch (SQLException e) {
+				e.printStackTrace();
+			}
+		}
+	}
+	
+
+	/**
+	 * Searches through a ResultSet for a player's friends.
+	 * 
+	 * @param playerID
+	 *            The playerID of the player
+	 * @param results
+	 *            The query result set
+	 * @return Returns the player's friends.
+	 * @throws SQLException
+	 */
+	private ArrayList<String> findFriends(int playerID, ResultSet results)
+			throws SQLException {
+		ArrayList<String> result = new ArrayList<String>();
+		while (results.next()) {
+			String user1 = results.getString("U1");
+			String user2 = results.getString("U2");
+			if (user1.equals(playerID)) {
+				result.add(user2);
+			}
+		}
+		return result;
+	}
 }

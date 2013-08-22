7c9855b547cc160ab38daab4bd897bb9ca9f8827
matt-shine <matthew.shine@uqconnect.edu.au>
2013-08-22 18:06:57 +1000

    built out MatchmakerQueue class

---
+++ deco2800.arcade.protocol/src/main/java/deco2800/arcade/protocol/multiplayerGame/NewMatchmakingRequest.java
@@ -0,0 +1,12 @@
+package deco2800.arcade.protocol.multiplayerGame;
+
+import com.esotericsoftware.kryonet.Connection;
+
+import deco2800.arcade.protocol.game.GameRequest;
+import deco2800.arcade.protocol.game.GameRequestType;
+
+public class NewMatchmakingRequest extends GameRequest {
+
+	public GameRequestType requestType;
+	
+}

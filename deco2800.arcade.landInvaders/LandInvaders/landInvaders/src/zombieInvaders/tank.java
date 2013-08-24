b6322078382506a8e3b75215a9d4a7ef429beb32
Gary Foo <foominglui@yahoo.com.sg>
2013-08-24 23:34:35 +1000

    Added new files and functions to be implemented but not working yet

--- deco2800.arcade.landInvaders/LandInvaders/landInvaders/src/zombieInvaders/tank.java
+++ deco2800.arcade.landInvaders/LandInvaders/landInvaders/src/zombieInvaders/tank.java
@@ -3,13 +3,13 @@ import java.awt.event.KeyEvent;
 import javax.swing.ImageIcon;
 
 //Need to do up class Sprite
-public class tank extends Sprite implements Stats{
+public class tank extends Sprites implements Stats{
     public final int START_Y = 300;
     public final int START_X = 300;
     private final String player = "AngryRedBird.png";
     private int iconWidth;
     
-    public Tank(){
+    public tank(){
 
 
     	ImageIcon playerImage = new ImageIcon(this.getClass().getResource(player));
@@ -18,5 +18,47 @@ public class tank extends Sprite implements Stats{
     	setImage(playerImage.getImage());
     	setX(START_X);
     	setY(START_Y);
+    	
+    }
+    public void move() {
+    	xCoords += directCoords;
+        if (xCoords <= 2)
+        {
+        	xCoords = 2;
+        }
+        
+        if (xCoords >= BOARD_WIDTH - 2*iconWidth)
+        {
+        	xCoords = BOARD_WIDTH - 2*iconWidth;
+        }
+    }
+
+    public void pressKey(KeyEvent e) {
+        int keyCode = e.getKeyCode();
+
+        if (keyCode == KeyEvent.VK_LEFT)
+        {
+        	directCoords = -2;
+        }
+
+        if (keyCode == KeyEvent.VK_RIGHT)
+        {
+        	directCoords = 2;
+        }
+
+    }
+
+    public void releaseKey(KeyEvent e) {
+        int keyCode = e.getKeyCode();
+
+        if (keyCode == KeyEvent.VK_LEFT)
+        {
+        	directCoords = 0;
+        }
+
+        if (keyCode == KeyEvent.VK_RIGHT)
+        {
+        	directCoords = 0;
+        }
     }
 }

2d15fbe8a7578e52ce1949be6644bfb6e764b02d
matt-shine <matthew.shine@uqconnect.edu.au>
2013-08-24 12:57:09 +1000

    updates to Lobby related classes

---
+++ deco2800.arcade.server/src/main/java/deco2800/server/Lobby.java
@@ -0,0 +1,15 @@
+package deco2800.server;
+
+import java.util.Map;
+
+import com.esotericsoftware.kryonet.Connection;
+
+public class Lobby {
+	
+	/* Holds the users in the lobby */
+	private Map<String, Map<String, Connection>> lobbyUsers;
+
+	
+	
+	
+}

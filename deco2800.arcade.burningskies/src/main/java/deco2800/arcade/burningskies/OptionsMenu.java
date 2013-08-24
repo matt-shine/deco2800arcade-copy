3a326e8f3c8202a8966132abada37c2fa5d20ce1
AsuraE <asurae7@hotmail.com>
2013-08-24 19:30:49 +1000

    OptionsMenu skeleton

---
+++ deco2800.arcade.burningskies/src/main/java/deco2800/arcade/burningskies/OptionsMenu.java
@@ -0,0 +1,47 @@
+package deco2800.arcade.burningskies;
+
+import com.badlogic.gdx.Screen;
+
+public class OptionsMenu implements Screen {
+	@Override
+	public void dispose() {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void hide() {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void pause() {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void render(float delta) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void resize(int width, int height) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void resume() {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void show() {
+		// TODO Auto-generated method stub
+
+	}
+}

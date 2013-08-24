c613d78d48dd20d74810b4d6ba72e585ea664097
Cameron Darragh <cammyd2@hotmail.com>
2013-08-24 10:10:25 +1000

    Added progress file

---
+++ deco2800.arcade.junglejump/src/main/java/deco2800/arcade/junglejump/Progress.java
@@ -0,0 +1,26 @@
+
+package deco2800.arcade.junglejump.controller;
+
+import java.util.*;
+
+public class Progress {
+	
+	ArrayList<ArrayList> bananas; // List of each level with a list of which bananas were found
+	
+	/**
+	 * Initialises progress variables
+	 */
+	public void Progress {
+		int levelsComplete = 0;
+		bananas = new ArrayList<ArrayList<boolean>>;
+		int foundBananas = 0;
+		int lives = 3;
+	}
+	
+	/**
+	 * Adds a banana to the total found
+	 */
+	public void addBanana(int level, int banana) {
+	}
+
+}

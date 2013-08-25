7612ee8956547465c89f486c7cc137d1176a00d3
muirbren <brendanjamesmuir@gmail.com>
2013-08-26 03:54:24 +1000

    Created branch - added basic classes
    Added the classes:
    PlatformerGame, Entity, MovableEntity, Player, Enemy.

---
+++ deco2800.arcade.platformergame/src/main/java/deco2800/arcade/platformergame/model/Player.java
@@ -0,0 +1,71 @@
+package deco2800.arcade.platformergame.model;
+
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.math.Rectangle;
+import com.badlogic.gdx.math.Vector2;
+
+public abstract class Player extends MovableEntity{
+	
+	public static final float SPEED;
+	public static final float JUMP_VELOCITY;
+	public static final float WIDTH;
+	public static final float HEIGHT;
+	public static final float JUMP_TIME;
+	public static final float GRAVITY;
+	public static final float MAX_FALL_VELOCITY;
+	
+	private static float rotation;
+	private boolean facingRight;
+	float jumpTime;
+
+	
+	
+	public Player(Vector2 position) {
+		super(SPEED, rotation, position, WIDTH, HEIGHT);
+		// TODO Auto-generated constructor stub
+	}
+
+	public void resetJumpTime() {
+		jumpTime = JUMP_TIME;
+	}
+	
+	public void clearJumpTime() {
+		jumpTime = 0f;
+	}
+	
+	/* ----- Getter methods ----- */
+	public Rectangle getProjectionRect() {
+		Rectangle rect = new Rectangle (position.x, position.y, width, height);
+		rect.x += velocity.x*Gdx.graphics.getDeltaTime();
+		rect.y += velocity.y*Gdx.graphics.getDeltaTime();
+
+		return rect;
+	}
+	
+	public Rectangle getXProjectionRect() {
+		Rectangle rect = new Rectangle (position.x, position.y, width, height);
+		rect.x += velocity.x*Gdx.graphics.getDeltaTime();
+
+		return rect;
+	}
+	
+	public Rectangle getYProjectionRect() {
+		Rectangle rect = new Rectangle (position.x, position.y, width, height);
+		rect.y += velocity.y*Gdx.graphics.getDeltaTime();
+	
+		return rect;
+	}
+	
+	public boolean isFacingRight() {
+		return facingRight;
+	}
+	
+	/* ----- Setter methods ----- */
+	public void setFacingRight(boolean right) {
+		facingRight = right;
+	}
+	
+	public void update() {
+		super.update();
+	}
+}

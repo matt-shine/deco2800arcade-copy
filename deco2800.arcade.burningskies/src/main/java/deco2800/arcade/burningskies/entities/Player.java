54e91a9708ebcc3caaec90fc6e71c1189c710baa
Azuris <allen.teh@uqconnect.edu.au>
2013-08-26 17:50:16 +1000

    Redid the commit of the ship skeleton class and its extended subclasses.

--- deco2800.arcade.burningskies/src/main/java/deco2800/arcade/burningskies/entities/Player.java
+++ deco2800.arcade.burningskies/src/main/java/deco2800/arcade/burningskies/entities/Player.java
@@ -1,8 +1,68 @@
 package deco2800.arcade.burningskies.entities;
 
+import deco2800.arcade.burningskies.*;
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Input.Keys;
+import com.badlogic.gdx.math.Vector2;
 
 public class Player extends Ship {
+
+	/**
+	 * Construct a playable ship for the user(s).
+	 * @ensure health && hitbox1 && hitbox2 > 0
+	 */
+	public Player(int health, Vector2 hitbox1, Vector2 hitbox2) {
+		super(health, hitbox1, hitbox2);
+	}
+
+	/**
+	 * Sets the current speed of the ship. 
+	 */
+	@Override
+	void velocity(float speed) {
+		this.velocity = speed;		
+	}
+
+	/**
+	 * Moves the player ship in the X direction.
+	 */
+	void moveX(float x) {
+		bounds.x += x;
+	}
 	
-	private int health = 1; // player is 1 hit kill for now
-	//TODO: write this
+	/**
+	 * Moves the player ship in the Y direction.
+	 */
+	void moveY(float y) {
+		bounds.y += y;
+	}
+	
+	@Override
+	/**
+	 * Keeps the player within the screen bounds.(hopefully)
+	 */
+	void outBounds() {
+		if (bounds.x > BurningSkies.SCREENWIDTH - bounds.width) {
+			bounds.x = BurningSkies.SCREENWIDTH - bounds.width;
+		}
+    	if (bounds.x < 0) bounds.x = 0;
+    	
+		if (bounds.y > BurningSkies.SCREENHEIGHT - bounds.height) {
+			bounds.y = BurningSkies.SCREENHEIGHT - bounds.height;
+		}
+    	if (bounds.y < 0) bounds.y = 0;
+    	
+    	if(Gdx.input.isKeyPressed(Keys.UP)) {
+    		moveY(this.velocity * Gdx.graphics.getDeltaTime());
+    	}
+    	if(Gdx.input.isKeyPressed(Keys.DOWN)) {
+    		moveY(-this.velocity * Gdx.graphics.getDeltaTime());
+    	}
+    	if(Gdx.input.isKeyPressed(Keys.LEFT)) {
+    		moveX(this.velocity * Gdx.graphics.getDeltaTime());
+    	}
+    	if(Gdx.input.isKeyPressed(Keys.RIGHT)) {
+    		moveX(-this.velocity * Gdx.graphics.getDeltaTime());
+    	}
+	}
 }
--- deco2800.arcade.burningskies/src/main/java/deco2800/arcade/burningskies/entities/Ship.java
+++ deco2800.arcade.burningskies/src/main/java/deco2800/arcade/burningskies/entities/Ship.java
@@ -1,15 +1,58 @@
 package deco2800.arcade.burningskies.entities;
 
-public class Ship {
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.math.Vector2;
+import com.badlogic.gdx.math.Rectangle;
+import com.badlogic.gdx.graphics.Texture;
+
+public abstract class Ship {
+
+	private int health;	
+	float velocity;
+	private Texture sprite;
+	Rectangle bounds = new Rectangle();
 	
-	private int health;
-	
-	public void damage(int damage) {
-		
+	/**
+	 * Basic constructor for a ship.
+	 * @ensure health && velocity > 0
+	 */
+	public Ship(int health, Vector2 hitbox1, Vector2 hitbox2) {
+		this.health = health;
+		this.bounds.x = hitbox1.x;
+		this.bounds.y = hitbox1.y;
+		this.bounds.width = hitbox2.x;
+		this.bounds.width = hitbox2.y;
 	}
-	
+	/**
+	 * Checks if the current ship is alive.
+	 */
 	public boolean isAlive() {
+		if (health == 0) {
+			return false;
+		}		
 		return true;
 	}
-	// TODO: write me
+	
+	/**
+	 * Modifies the current health of the ship accordingly.
+	 * May be negative or positive value.
+	 */
+	public void setHealth(int healthchange) {
+		this.health += healthchange;
+	}
+	
+	abstract void velocity(float speed);
+	
+	/**
+	 * Sets the sprite for the ship.
+	 */
+	public void ship_sprite(String sprite) {
+		this.sprite = new Texture(Gdx.files.internal(sprite));
+	}
+	
+	/**
+	 * Handles if the ship is out of bounds. The subclasses will determine
+	 * what to do if this is so.
+	 */
+	abstract void outBounds();
 }

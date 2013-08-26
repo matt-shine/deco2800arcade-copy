a4c86c3224fecfdbe7f3709cd53759ab7faf9082
Leggy <lachlan.j.healey@gmail.com>
2013-08-26 22:42:58 +1000

    Creation of User Class

---
+++ deco2800.arcade.model/src/main/java/deco2800/arcade/model/User.java
@@ -0,0 +1,22 @@
+package deco2800.arcade.model;
+
+
+/**
+ * 
+ * @author Leggy (Lachlan Healey: lachlan.j.healey@gmail.com)
+ *
+ */
+public class User {
+	
+	
+	private int playerID;
+
+	public User(int id){
+		this.playerID = id;
+	}
+	
+	public int getID(){
+		return playerID;
+	}
+
+}

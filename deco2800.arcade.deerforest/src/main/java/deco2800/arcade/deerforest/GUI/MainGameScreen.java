01de7c15c2fb51ac36124a99d5e769410fcccb12
Segmelsian <jcksonmoes@gmail.com>
2013-08-26 12:19:41 +1000

    Should work
    blarg

--- deco2800.arcade.deerforest/src/main/java/deco2800/arcade/deerforest/GUI/MainGameScreen.java
+++ deco2800.arcade.deerforest/src/main/java/deco2800/arcade/deerforest/GUI/MainGameScreen.java
@@ -8,23 +8,32 @@ import java.util.Map;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Screen;
 import com.badlogic.gdx.assets.AssetManager;
+import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.graphics.GL10;
 import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.glutils.ShapeRenderer;
+import com.badlogic.gdx.graphics.glutils.ShapeRenderer.ShapeType;
+import com.badlogic.gdx.math.Rectangle;
 
 public class MainGameScreen implements Screen {
 	
 	private final MainGame game;
 	private OrthographicCamera camera;
 	private AssetManager manager;
-	
+	private ShapeRenderer shapeRenderer;
+
 	//Variables for Card locations and what they contain
 	private int p1DeckSize;
 	private int p2DeckSize;
 	
+	private float glowSize;
+	private boolean glowDirection;
+	
 	//assets
 	private Map<String, List<ExtendedSprite>> spriteMap;
 	private Arena arena;
+	private List<Rectangle> highlightedZones;
 	
 	public MainGameScreen(final MainGame gam) {
 		this.game = gam;
@@ -33,13 +42,20 @@ public class MainGameScreen implements Screen {
 		camera = new OrthographicCamera();
 		camera.setToOrtho(false, getWidth(), getHeight());
 		
+		//initialise shapeRender / glowSize
+		shapeRenderer = new ShapeRenderer();
+		glowSize = 0f;
+		glowDirection = true;
+		
 		//load some assets
 		manager = new AssetManager();
 		loadAssets();
 		manager.finishLoading();
 		
+		//create game arena
 		arena = new Arena(manager.get("DeerForestAssets/background.png", Texture.class));
 		
+		//create map of sprites
 		spriteMap = new HashMap<String, List<ExtendedSprite>>();
 		List<ExtendedSprite> p1Hand = new ArrayList<ExtendedSprite>();
 		ExtendedSprite s1 = new ExtendedSprite(manager.get("DeerForestAssets/generalCard.png", Texture.class));
@@ -50,6 +66,9 @@ public class MainGameScreen implements Screen {
 		s2.setPosition(200, 200);
 	    p1Hand.add(s2);
 	    spriteMap.put("P1Hand", p1Hand);
+	    
+	    //list of highlighted zones
+	    highlightedZones = arena.getAvailableZones(1, true, true);
 	}
 
 	private void loadAssets() {
@@ -70,38 +89,61 @@ public class MainGameScreen implements Screen {
 		//tell the SpriteBatch to render in the
 		//coordinate system specified by the camera
 		game.batch.setProjectionMatrix(camera.combined);
+		shapeRenderer.setProjectionMatrix(camera.combined);
+		 
+		game.batch.begin();
 		
-		//draw some random text  
-	    game.batch.begin();
-
+		//Draw game board
 	    arena.draw(game.batch);
-	    
+
+	    //draw the sprites currently in map 
 	    for(String key : spriteMap.keySet()) {
 	    	for(ExtendedSprite s : spriteMap.get(key)) {
 		    	s.draw(game.batch);
 		    }
 	    }
+	    
+	    //draw highlighted zone
+	    if(!highlightedZones.isEmpty()) {
+	    	Gdx.gl.glEnable(GL10.GL_BLEND);
+		    Gdx.gl.glBlendFunc(GL10.GL_SRC_ALPHA, GL10.GL_ONE_MINUS_SRC_ALPHA);
 
+		    shapeRenderer.begin(ShapeType.FilledRectangle);
+		    
+		    for(Rectangle r: highlightedZones) {
+		    	shapeRenderer.filledRect(r.getX(), r.getY(), r.getWidth(), r.getHeight()+glowSize, Color.BLUE, Color.CLEAR, Color.CLEAR, Color.CLEAR);
+		    	
+		    	if(glowSize > 10 && glowDirection == true) {
+		    		glowDirection = false;
+		    	} else if(glowSize < 0 && glowDirection == false) {
+		    		glowDirection = true;
+		    	}
+		    	
+		    	if(glowDirection) glowSize += 0.05;
+		    	else glowSize -= 0.05;
+		    }
+		    
+		    shapeRenderer.end();
+		    Gdx.gl.glDisable(GL10.GL_BLEND);
+	    }
+	    
 	    game.batch.end();
 		
 	}
 	
 	@Override
 	public void dispose() {
-		// TODO Auto-generated method stub
-		
+		manager.dispose();
 	}
 
 	@Override
 	public void hide() {
-		// TODO Auto-generated method stub
-		
+
 	}
 
 	@Override
 	public void pause() {
-		// TODO Auto-generated method stub
-		
+
 	}
 
 	@Override
@@ -113,14 +155,12 @@ public class MainGameScreen implements Screen {
 
 	@Override
 	public void resume() {
-		// TODO Auto-generated method stub
-		
+
 	}
 
 	@Override
 	public void show() {
-		// TODO Auto-generated method stub
-		
+
 	}
 
 	public Map<String, List<ExtendedSprite>> getSpriteMap() {
@@ -137,5 +177,13 @@ public class MainGameScreen implements Screen {
 	
 	public int getHeight() {
 		return Gdx.graphics.getHeight();
+	}
+	
+	public void setHighlightedZones(List<Rectangle> highlight) {
+//		highlightedZones.clear();
+//		for(Rectangle r: highlight) {
+//			highlightedZones.add(new Rectangle(r));
+//		}
+		highlightedZones = highlight;
 	}
 }

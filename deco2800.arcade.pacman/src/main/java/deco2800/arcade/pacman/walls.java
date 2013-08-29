0a1944863a50849d9e7fca28131197845bfd586e
Aaron <azzazza_91@hotmail.com>
2013-08-29 11:01:47 +1000

    walls added

---
+++ deco2800.arcade.pacman/src/main/java/deco2800/arcade/pacman/walls.java
@@ -0,0 +1,9 @@
+package deco2800.arcade.pacman;
+
+import com.badlogic.gdx.graphics.Color;
+import com.badlogic.gdx.graphics.glutils.ShapeRenderer;
+import com.badlogic.gdx.math.Rectangle;
+
+public class walls {
+
+}

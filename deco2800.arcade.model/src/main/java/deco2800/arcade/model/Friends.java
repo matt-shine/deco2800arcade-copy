66f57af8447280749621537a2c46a297b6d9097f
Leggy <lachlan.j.healey@gmail.com>
2013-08-31 18:30:50 +1000

    Implemented

--- deco2800.arcade.model/src/main/java/deco2800/arcade/model/Friends.java
+++ deco2800.arcade.model/src/main/java/deco2800/arcade/model/Friends.java
@@ -1,10 +1,23 @@
 package deco2800.arcade.model;
 
+import java.util.HashSet;
 import java.util.Set;
 
 public class Friends {
 
 	private Set<User> friends;
+	
+	public Friends(){
+		this.friends = new HashSet<User>();
+	}
+	
+	public Friends(Friends friends){
+		this.friends = new Hashset<User>(friends.getSet);
+	}
+	
+	private Set<User> getSet(){
+		return friends;
+	}
 	
 	
 	

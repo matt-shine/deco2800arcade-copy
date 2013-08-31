40eea34e8677528ade1775eb88d61bd386ebcca1
Leggy <lachlan.j.healey@gmail.com>
2013-08-31 19:45:52 +1000

    Documentation for Friends

--- deco2800.arcade.model/src/main/java/deco2800/arcade/model/Friends.java
+++ deco2800.arcade.model/src/main/java/deco2800/arcade/model/Friends.java
@@ -3,34 +3,66 @@ package deco2800.arcade.model;
 import java.util.HashSet;
 import java.util.Set;
 
+/**
+ * Friends is an abstraction of a Player's Friends List.
+ * @author Leggy
+ *
+ */
 public class Friends {
 
 	private Set<User> friends;
-	
-	public Friends(){
+
+	/**
+	 * Creates a new Friends.
+	 */
+	public Friends() {
 		this.friends = new HashSet<User>();
 	}
-	
-	public Friends(Friends f){
+
+	/**
+	 * Created a new Friends given an existing Friends, copying the entries.
+	 * 
+	 * @param f
+	 *            The Friends instance to be copied.
+	 */
+	public Friends(Friends f) {
 		this.friends = new HashSet<User>(f.friends);
 	}
-	
-	public Set<User> getSet(){
+
+	/**
+	 * Access method for a Set representation of this.
+	 * 
+	 * @return Returns a Set representation of this.
+	 */
+	public Set<User> getSet() {
 		return new HashSet<User>(friends);
 	}
-	
-	public void add(User user){
+
+	/**
+	 * Adds a User to this.
+	 * 
+	 * @param user
+	 *            The User to be added.
+	 */
+	public void add(User user) {
 		this.friends.add(new User(user));
 	}
-	
-	public void remove(User user){
+
+	/**
+	 * Removes a User from this.
+	 * @param user	The User to be removed.
+	 */
+	public void remove(User user) {
 		this.friends.remove(new User(user));
 	}
-	
-	public boolean contains(User user){
+
+	/**
+	 * Checks if a User is in this.
+	 * @param user	The User to check.
+	 * @return	Returns true if the User is in this, false otherwise.
+	 */
+	public boolean contains(User user) {
 		return this.friends.contains(new User(user));
 	}
-	
-	
-	
+
 }

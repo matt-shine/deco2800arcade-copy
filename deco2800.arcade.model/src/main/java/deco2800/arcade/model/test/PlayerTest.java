dc2eb7ea18f54dfd5e7a9530071a31e817d696ec
taylah123 <taylah.hasaballah@uqconnect.edu.au>
2013-09-01 11:37:17 +1000

    Updated Player tests
    Now checking for non-unique username updates and invalid chars

--- deco2800.arcade.model/src/main/java/deco2800/arcade/model/test/PlayerTest.java
+++ deco2800.arcade.model/src/main/java/deco2800/arcade/model/test/PlayerTest.java
@@ -10,29 +10,38 @@ public class PlayerTest {
 	@Test
 	public void PlayerTest() {
 		Player p1 = new Player(111, "Bob", "String filepath");
-		Player p2 = new Player(112, "Betty", "String filepath");
-		
+		Player p2 = new Player(112, "Betty", "String filepath");	
 	}
 
 	@Test
 	public void GetUsernameTest() {
 		Player p1 = new Player(111, "Ezmerelda", "String filepath");
 		Player p2 = new Player(112, " ", "String filepath");
-		Player p3 = new Player(113, "%$^&%@@#$", "String filepath");
 		Assert.assertEquals("Mismatched player name", "Ezmerelda", p1.getUsername());
 		Assert.assertEquals("Player name error", "Invalid username", p2.getUsername());
-		Assert.assertEquals("Invalid player name characters", "Invalid chars", p3.getUsername());
+
 	}
 	
 	@Test
 	public void SetUsernameTest() {
-		
+		Player p1 = new Player(111, "Bob", "String filepath");
+		Player p2 = new Player(112, null, "String filepath");
+		Player p3 = new Player(113, "Genie", "String filepath");
+		Player p4 = new Player(114, "Zeus", "String filepath");
+		Player p5 = new Player(115, "Dekota", "String filepath");
+		p1.setUsername("Cornelious");
+		p2.setUsername("GameDestroya");
+		p3.setUsername("%$^&%@@#$");
+		p4.setUsername(" ");
+		p5.setUsername("Cornelious");
+		Assert.assertEquals("Mismatched player name update", "Cornelious", p1.getUsername());
+		Assert.assertEquals("Player name assigned to null value", "Invalid username update", p2.getUsername());
+		Assert.assertEquals("Invalid player name characters", "Invalid chars", p3.getUsername());
+		Assert.assertEquals("Invalid player name characters", "Invalid chars", p4.getUsername());
+		Assert.assertEquals("Invalid player name, already in use", "Non-unique Username", p5.getUsername());
 	}
 	
-	@Test
-	public void GetGamesTest() {
-		
-	}
+	
 	
 
 }

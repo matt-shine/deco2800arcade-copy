0e23dcfd1d13600a637ed07c9a0bea0f7b59f1fb
iamwinrar <andrew.cleland3@gmail.com>
2013-09-01 23:10:23 +1000

    Added stuff to PlayerListener.

--- deco2800.arcade.server/src/main/java/deco2800/server/listener/PlayerListener.java
+++ deco2800.arcade.server/src/main/java/deco2800/server/listener/PlayerListener.java
@@ -1,7 +1,49 @@
 package deco2800.server.listener;
 
 import com.esotericsoftware.kryonet.Listener;
+import com.esotericsoftware.kryonet.Server;
+import com.esotericsoftware.kryonet.Connection;
+
+import deco2800.arcade.protocol.player.*;
 
 public class PlayerListener extends Listener{
-
+	
+	
+	//uses DatabaseManager to talk to database
+	public PlayerListener() {
+		
+	}
+	
+	@Override
+	public void received(Connection connection, Object object) {
+		super.received(connection, object);
+		
+		if (object instanceof BioUpdateRequest) {
+			BioUpdateRequest bioUpdateRequest = (BioUpdateRequest) object;
+			
+		} else if (object instanceof BlockedUpdateRequest) {
+			BlockedUpdateRequest blockedUpdateRequest  = (BlockedUpdateRequest) object;
+			
+		} else if (object instanceof EmailUpdateRequest) {
+			EmailUpdateRequest emailUpdateRequest = (EmailUpdateRequest) object;
+			
+		} else if (object instanceof FriendInvitesUpdateRequest) {
+			FriendInvitesUpdateRequest friendInviteUpdateRequest = (FriendInvitesUpdateRequest) object;
+			
+		} else if (object instanceof FriendsUpdateRequest) {
+			FriendsUpdateRequest friendsUpdateRequest = (FriendsUpdateRequest) object;
+			
+		} else if (object instanceof GamesUpdateRequest) {
+			GamesUpdateRequest gamesUpdateRequest = (GamesUpdateRequest) object;
+			
+		} else if (object instanceof NameUpdateRequest) {
+			NameUpdateRequest nameUpdateRequest = (NameUpdateRequest) object;
+			
+		} else if (object instanceof ProgramUpdateRequest) {
+			ProgramUpdateRequest programUpdateRequest = (ProgramUpdateRequest) object;
+			
+		} else if (object instanceof UsernameUpdateRequest) {
+			UsernameUpdateRequest usernameUpdateRequest = (UsernameUpdateRequest) object;
+		}
+	}
 }

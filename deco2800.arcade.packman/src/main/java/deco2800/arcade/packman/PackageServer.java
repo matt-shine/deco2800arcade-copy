f32540a026e64b20ce691770c813699e78c94ee6
merrick.heley <merrick.heley@gmail.com>
2013-09-01 23:45:37 +1000

    Added creation of 'Release' folder in Package Manager

--- deco2800.arcade.packman/src/main/java/deco2800/arcade/packman/PackageServer.java
+++ deco2800.arcade.packman/src/main/java/deco2800/arcade/packman/PackageServer.java
@@ -1,13 +1,28 @@
 package deco2800.arcade.packman;
 
+import java.io.File;
+
 public class PackageServer {
 	
 	
 	/**
 	 * Initialiser
+	 * 
+	 * Create the 'Release' directory if it does not exist
 	 */
 	public PackageServer() {
+		File releaseDir = new File("Release");
 		
+		// Create the Release directory if it doesn't exist
+		if (!releaseDir.exists()) {
+			System.out.println("Creating directory: " + releaseDir);
+			
+			if (releaseDir.mkdirs()) {
+				System.out.println("Created: " + releaseDir);
+			} else {
+				System.out.println("Failed creating: " + releaseDir);
+			}
+		}
 	}
 	
 	/**

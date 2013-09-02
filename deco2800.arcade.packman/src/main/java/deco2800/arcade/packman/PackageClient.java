0080bb0e5aea863fa5231320e010a07f587ac1b3
merrick.heley <merrick.heley@gmail.com>
2013-09-02 15:01:17 +1000

    Added creation of 'Games' folder to Package Manager Client

--- deco2800.arcade.packman/src/main/java/deco2800/arcade/packman/PackageClient.java
+++ deco2800.arcade.packman/src/main/java/deco2800/arcade/packman/PackageClient.java
@@ -1,8 +1,31 @@
 package deco2800.arcade.packman;
 
+import java.io.File;
+
 /**
  * Client for package manager
  */
 public class PackageClient {
+	
+	
+	/**
+	 * Initialiser
+	 * 
+	 * Create the 'Games' directory if it does not exist
+	 */
+	public PackageClient() {
+		File releaseDir = new File("Games");
+		
+		// Create the Release directory if it doesn't exist
+		if (!releaseDir.exists()) {
+			System.out.println("Creating directory: " + releaseDir);
+			
+			if (releaseDir.mkdirs()) {
+				System.out.println("Created: " + releaseDir);
+			} else {
+				System.out.println("Failed creating: " + releaseDir);
+			}
+		}
+	}
 	
 }

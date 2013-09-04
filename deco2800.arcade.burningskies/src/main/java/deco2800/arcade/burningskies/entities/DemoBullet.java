59d167208580ead08980023e6b0806fce80a74bb
Azuris <allen.teh@uqconnect.edu.au>
2013-09-04 21:39:02 +1000

    Minor name changes mainly.

--- deco2800.arcade.burningskies/src/main/java/deco2800/arcade/burningskies/entities/DemoBullet.java
+++ deco2800.arcade.burningskies/src/main/java/deco2800/arcade/burningskies/entities/DemoBullet.java
@@ -6,7 +6,7 @@ import com.badlogic.gdx.math.Vector2;
 public class DemoBullet extends Bullet {
 
 	public DemoBullet(Affinity affinity, int damage, Ship parent,
-			Player player, Vector2 initialPosition, float direction, Texture image) {
+			PlayerShip player, Vector2 initialPosition, float direction, Texture image) {
 		super(affinity, damage, parent, player, initialPosition, direction, image);
 		velocity = new Vector2(150,150);
 		setRotation(direction);

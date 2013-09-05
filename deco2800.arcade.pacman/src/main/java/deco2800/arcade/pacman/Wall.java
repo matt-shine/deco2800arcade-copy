e106f27dd6fcb818dd43e543c5256e322e1e3500
Aaron <azzazza_91@hotmail.com>
2013-09-05 11:12:48 +1000

    wall hopefully drawing

--- deco2800.arcade.pacman/src/main/java/deco2800/arcade/pacman/Wall.java
+++ deco2800.arcade.pacman/src/main/java/deco2800/arcade/pacman/Wall.java
@@ -1,34 +1,57 @@
 package deco2800.arcade.pacman;
 
+import com.badlogic.gdx.ApplicationListener;
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.graphics.glutils.ShapeRenderer;
 import com.badlogic.gdx.scenes.scene2d.Actor;
 
 import static com.badlogic.gdx.graphics.glutils.ShapeRenderer.ShapeType.*;
 
-public class Wall extends Actor {
+public class Wall implements ApplicationListener {
 
-	static enum WallType {
-		OUTER_HOR, OUTER_VERT, OUTER_CORN_1
-	}
+	private SpriteBatch batch;
+	private Texture wallTexture;
 	
-	private final ShapeRenderer shapeRenderer;
-	private final WallType type;
-	
-	private boolean built;
-	
-	Wall(WallType type, ShapeRenderer shapeRenderer) {
-		this.shapeRenderer = shapeRenderer;
-		this.type = type;
-		this.built = false;
+	@Override
+	public void create() {
+		batch = new SpriteBatch();
+		wallTexture = new Texture(Gdx.files.internal("testwall.png"));	
+		
 	}
 	
 	@Override
-	public void draw(SpriteBatch batch, float parentAlpha) {
+	public void render() {
+		batch.begin();
+		batch.draw(wallTexture,100,100);
+		batch.end();
+	}
+
+	@Override
+	public void dispose() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void pause() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void resize(int arg0, int arg1) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void resume() {
+		// TODO Auto-generated method stub
 		
 	}
 	
 	
-	// Add draw function. Look at mixmaze.Wall.java
 }
 

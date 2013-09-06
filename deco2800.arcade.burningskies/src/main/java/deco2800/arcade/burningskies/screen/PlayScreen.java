b0cc6f2ce59097abe3046e04e5de45b5259f910b
William Toohey <william.toohey@uqconnect.edu.au>
2013-09-06 11:36:04 +1000

    Remove warnings
    #2good

--- deco2800.arcade.burningskies/src/main/java/deco2800/arcade/burningskies/screen/PlayScreen.java
+++ deco2800.arcade.burningskies/src/main/java/deco2800/arcade/burningskies/screen/PlayScreen.java
@@ -11,7 +11,6 @@ import com.badlogic.gdx.scenes.scene2d.Stage;
 import com.badlogic.gdx.math.Vector2;
 
 import deco2800.arcade.burningskies.BurningSkies;
-import deco2800.arcade.burningskies.entities.DemoPattern;
 import deco2800.arcade.burningskies.entities.PlayerShip;
 
 

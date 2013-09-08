904553b7a36af6ad765523dff80f71dad458c977
YuhaoLiu <x523091366@gmail.com>
2013-09-09 00:39:48 +1000

    new class

---
+++ deco2800.arcade.landInvaders/src/main/java/deco2800/arcade/landInvaders/LandInvaders.java
@@ -0,0 +1,35 @@
+package deco2800.arcade.landInvaders;
+import deco2800.arcade.client.GameClient;
+import deco2800.arcade.client.network.NetworkClient;
+import deco2800.arcade.model.Game;
+import deco2800.arcade.model.Game.ArcadeGame;
+import deco2800.arcade.model.Player;
+
+
+@ArcadeGame(id = "LandInvaders")
+public class LandInvaders extends GameClient  {
+	
+	private static final Game GAME;
+
+	public LandInvaders(Player player, NetworkClient networkClient) {
+		super(player, networkClient);
+		
+	}
+
+	public void resume() {
+		super.resume();
+	}
+
+	static {
+		GAME = new Game();
+		GAME.id = "LandInvaders";
+		GAME.name = "LandInvaders";
+		GAME.description = "funny game!";
+	}
+	@Override
+	public Game getGame() {
+		
+		return GAME;
+	}
+
+}

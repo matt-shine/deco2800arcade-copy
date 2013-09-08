875b723810f42aafd194134026a062e194d21bc6
Matthew Henry <matt.henry@me.com>
2013-09-08 17:03:14 +1000

    More work on ImageStorage

--- deco2800.arcade.server/src/main/java/deco2800/server/database/ImageStorage.java
+++ deco2800.arcade.server/src/main/java/deco2800/server/database/ImageStorage.java
@@ -12,12 +12,12 @@ public class ImageStorage {
     // max image size of 1M
 
     public void initialise() throws DatabaseException {
-        Connection conn = Database.getConnection();
+        Connection connection = Database.getConnection();
         try {
-            ResultSet tableData = conn.getMetaData().getTables(null, null,
+            ResultSet tableData = connection.getMetaData().getTables(null, null,
 					"IMAGES", null);
 			if (!tableData.next()){
-				Statement statement = conn.createStatement();
+				Statement statement = connection.createStatement();
 				statement.execute("CREATE TABLE IMAGES(id VARCHAR(255) PRIMARY KEY," +
 						"data BLOB(1M) NOT NULL)");
 			}
@@ -35,8 +35,10 @@ public class ImageStorage {
      */
     public void set(String id, File imgFile) {
         // Read in the data and create an EncodedImage
-
+        
+        
         // use our set(String,EncodedImage) overload to do the DB work
+
     }
     
     /**
@@ -47,7 +49,34 @@ public class ImageStorage {
 
     }
 
+    /**
+     * Returns the image associated with the given id.
+     */
     public EncodedImage get(String id) {
+        //Get a connection to the database
+		Connection connection = Database.getConnection();
+
+		Statement statement = null;
+		ResultSet resultSet = null;
+		try {
+		    statement = connection.createStatement();
+            resultSet = statement.executeQuery("SELECT * FROM IMAGES" +
+						" WHERE ID='" + id + "'");
+
+		} catch (SQLException e) {
+			e.printStackTrace();
+			throw new DatabaseException("Unable to get image with id " + id, e);
+		} finally {
+			try {
+				if (resultSet != null) resultSet.close();
+				if (statement != null) statement.close();
+  				if (connection != null)	connection.close();
+			} catch (SQLException e) {
+				e.printStackTrace();
+                throw new DatabaseException("Couldn't close resultSet/statement/connection", e);
+			}
+		}
+
         return new EncodedImage();
     }
 

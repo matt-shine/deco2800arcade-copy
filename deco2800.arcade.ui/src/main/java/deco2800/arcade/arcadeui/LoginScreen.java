d6136b27beceb0b606d6e259f073d0a6a9dca2ff
abbjohn <abbjohn@gmail.com>
2013-09-09 02:42:01 +1000

    Setting up for proper login screen styling.

--- deco2800.arcade.ui/src/main/java/deco2800/arcade/arcadeui/LoginScreen.java
+++ deco2800.arcade.ui/src/main/java/deco2800/arcade/arcadeui/LoginScreen.java
@@ -7,7 +7,6 @@ import com.badlogic.gdx.scenes.scene2d.Actor;
 import com.badlogic.gdx.scenes.scene2d.Stage;
 import com.badlogic.gdx.scenes.scene2d.ui.*;
 import com.badlogic.gdx.scenes.scene2d.utils.ChangeListener;
-import com.badlogic.gdx.scenes.scene2d.utils.Align;
 import deco2800.arcade.client.ArcadeInputMux;
 import deco2800.arcade.client.ArcadeSystem;
 
@@ -19,18 +18,6 @@ public class LoginScreen implements Screen {
 	public LoginScreen() {
         skin = new Skin(Gdx.files.internal("loginSkin.json"));
         skin.add("background", new Texture("homescreen_bg.png"));
-        
-        Pixmap pixmap = new Pixmap(1, 1, Pixmap.Format.RGBA8888);
-        pixmap.setColor(Color.WHITE);
-        pixmap.fill();
-        skin.add("white", new Texture(pixmap));
-        
-        TextButton.TextButtonStyle textButtonStyle = new TextButton.TextButtonStyle();
-        textButtonStyle.up = skin.newDrawable("white", Color.DARK_GRAY);
-        textButtonStyle.down = skin.newDrawable("white", Color.DARK_GRAY);
-        textButtonStyle.over = skin.newDrawable("white", Color.LIGHT_GRAY);
-        textButtonStyle.font = skin.getFont("default");
-        skin.add("default", textButtonStyle);
 
         stage = new Stage();
         ArcadeInputMux.getInstance().addProcessor(stage);
@@ -49,24 +36,26 @@ public class LoginScreen implements Screen {
         passwordText.setPasswordCharacter('*');
         final TextField serverText = new TextField("", skin);
         serverText.setMessageText("Server") ;
-        //CheckBox rememberBox = new CheckBox("Remember Me", skin);
+        CheckBox rememberBox = new CheckBox("Remember Me", skin);
         TextButton loginButton = new TextButton("Login", skin);
         TextButton forgotLogButton = new TextButton("Forgot Login?", skin);
         TextButton registerButton = new TextButton("Register", skin);
 
-        table.debug();  // Shows table debug lines.  Remove for final product.
+        //table.debug();  // Shows table debug lines.  Remove for final product.
         table.add(errorLabel).colspan(2);
         table.row();
-        table.add(usernameText).width(400).colspan(2);
+        table.add(usernameText).width(400).pad(5).colspan(2);
         table.row();
-        table.add(passwordText).width(400).colspan(2);
+        table.add(passwordText).width(400).pad(5).colspan(2);
         table.row();
-        table.add(serverText).width(400).colspan(2);
+        table.add(serverText).width(400).pad(5).colspan(2);
         table.row();
-        table.add(loginButton).width(200);
-        table.add(registerButton).width(200);
+        table.add(rememberBox);
         table.row();
-        table.add(forgotLogButton);
+        table.add(loginButton).width(200).pad(5);
+        table.add(registerButton).width(200).pad(5);
+        table.row();
+        table.add(forgotLogButton).pad(5);
 
         loginButton.addListener(new ChangeListener() {
             public void changed (ChangeEvent event, Actor actor) {
@@ -86,7 +75,7 @@ public class LoginScreen implements Screen {
 		Gdx.gl.glClear(GL10.GL_COLOR_BUFFER_BIT);
         stage.act(Gdx.graphics.getDeltaTime());
         stage.draw();
-        Table.drawDebug(stage);  // Shows table debug lines.  Remove for final product.
+        //Table.drawDebug(stage);  // Shows table debug lines.  Remove for final product.
 
 	    if (ArcadeSystem.isLoggedIn()) {
 	    	dispose();

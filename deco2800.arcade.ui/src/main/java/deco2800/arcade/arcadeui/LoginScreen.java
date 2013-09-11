40ca21b6364e2e1d4343a7b017f3ebf9193cf3cc
abbjohn <abbjohn@gmail.com>
2013-09-11 20:39:23 +1000

    Change the rendering of Manteka to java from native. Gets rid of some of the letter spacing issues. Add a new font, and change to using it.

--- deco2800.arcade.ui/src/main/java/deco2800/arcade/arcadeui/LoginScreen.java
+++ deco2800.arcade.ui/src/main/java/deco2800/arcade/arcadeui/LoginScreen.java
@@ -6,6 +6,7 @@ import com.badlogic.gdx.graphics.*;
 import com.badlogic.gdx.scenes.scene2d.Actor;
 import com.badlogic.gdx.scenes.scene2d.Stage;
 import com.badlogic.gdx.scenes.scene2d.ui.*;
+import com.badlogic.gdx.scenes.scene2d.utils.Align;
 import com.badlogic.gdx.scenes.scene2d.utils.ChangeListener;
 import deco2800.arcade.client.ArcadeInputMux;
 import deco2800.arcade.client.ArcadeSystem;
@@ -29,7 +30,9 @@ public class LoginScreen implements Screen {
         stage.addActor(table);
 
         final Label tempLabel = new Label("To access the store\nlogin with username: store", skin);  // Temporary label to display a message
+        tempLabel.setAlignment(Align.center);
         final Label errorLabel = new Label("", skin, "error");
+        errorLabel.setAlignment(Align.center);
         final TextField usernameText = new TextField("", skin);
         usernameText.setMessageText("Username");
         final TextField passwordText = new TextField("", skin);
@@ -43,23 +46,22 @@ public class LoginScreen implements Screen {
         TextButton registerButton = new TextButton("Register", skin);
         TextButton forgotLogButton = new TextButton("Forgot Login?", skin, "alt");
 
-        //table.debug();
         table.add(tempLabel).colspan(2);  // Temporary label to display a message
         table.row();
-        table.add(errorLabel).width(400).colspan(2);
+        table.add(errorLabel).width(400).pad(5).colspan(2);
         table.row();
-        table.add(usernameText).width(400).colspan(2);
+        table.add(usernameText).width(400).pad(5).colspan(2);
         table.row();
-        table.add(passwordText).width(400).colspan(2);
+        table.add(passwordText).width(400).pad(5).colspan(2);
         table.row();
-        table.add(serverText).width(400).colspan(2);
+        table.add(serverText).width(400).pad(5).colspan(2);
         table.row();
         //table.add(rememberBox);
         //table.row();
-        table.add(loginButton).width(200).height(50).pad(5);
-        table.add(registerButton).width(200).height(50).pad(5);
+        table.add(loginButton).width(190).height(50).pad(5);
+        table.add(registerButton).width(190).height(50).pad(5);
         table.row();
-        table.add(forgotLogButton).width(400).height(30).pad(5).colspan(2);
+        table.add(forgotLogButton).width(400).height(50).pad(5).colspan(2);
         
         loginButton.addListener(new ChangeListener() {
             public void changed(ChangeEvent event, Actor actor) {
@@ -99,7 +101,6 @@ public class LoginScreen implements Screen {
 		Gdx.gl.glClear(GL10.GL_COLOR_BUFFER_BIT);
         stage.act(Gdx.graphics.getDeltaTime());
         stage.draw();
-        //Table.drawDebug(stage);
 
 	    if (ArcadeSystem.isLoggedIn()) {
 	    	ArcadeSystem.goToGame("arcadeui");

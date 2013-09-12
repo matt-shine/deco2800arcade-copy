a5a0c02526f576daa75b57b1063c63b1eddd0f5e
Nathan Woodrow <nathanwoodrow@gmail.com>
2013-09-12 14:04:16 +1000

    Added some documentation and an insert method
    Added some documentation and an insert method

--- deco2800.arcade.server/src/main/java/deco2800/server/database/GamePath.java
+++ deco2800.arcade.server/src/main/java/deco2800/server/database/GamePath.java
@@ -7,6 +7,8 @@ import java.sql.SQLException;
 import java.sql.Statement;
 
 public class GamePath {
+	
+	private boolean initialised = false;
 
 	public void initialise() throws DatabaseException {
 
@@ -26,22 +28,36 @@ public class GamePath {
 			e.printStackTrace();
 			throw new DatabaseException("Unable to create game path table", e);
 		}
+		initialised = true;
 	}
-	
+	/**
+	 * Searches for the path of the game with a particular gameID
+	 * @param gameID
+	 *          ID for the requested game
+	 * @return
+	 *          Returns the path of the requested game
+	 * @throws DatabaseException
+	 *          If SQLException occurs
+	 */
 	public String getPath(int gameID) throws DatabaseException{
+		if(!initialised){
+			initialise();
+		}
 		Connection connection = Database.getConnection();
 
 		Statement statement = null;
 		ResultSet resultSet = null;
 		try {
 			statement = connection.createStatement();
-			resultSet = statement.executeQuery("SELECT path FROM GAMEPATH WHERE gameID=" + gameID);
+			resultSet = statement.executeQuery("SELECT path FROM GAMEPATH WHERE"
+					+ " gameID=" + gameID);
 			String result = resultSet.getString(1);
 
 			return result;
 		} catch (SQLException e) {
 			e.printStackTrace();
-			throw new DatabaseException("Unable to get game path from database", e);
+			throw new DatabaseException("Unable to get game path from database",
+					e);
 		} finally {
 			try {
 				if (resultSet != null){
@@ -54,6 +70,39 @@ public class GamePath {
 					connection.close();
 				}
 			} catch (SQLException e) {
+				e.printStackTrace();
+			}
+		}
+	}
+	/**
+	 * adds a game to the table with the specified ID and Path
+	 * @param gameID
+	 *          ID of the game to be added
+	 * @param gamePath
+	 *          Path of the game to be added
+	 * @throws DatabaseException
+	 *          If SQLException occurs
+	 */
+	public void insertGame(int gameID,String gamePath) throws DatabaseException{
+		Connection connection = Database.getConnection();
+		
+		Statement statement = null;
+		String query = "INSERT INTO GAMEPATH VALUES ('"+gameID+"','"+
+		gamePath+"')";
+		try {
+			statement = connection.createStatement();
+			statement.executeUpdate(query);
+		} catch(SQLException e) {
+			throw new DatabaseException("unable to add game to database",e);
+		} finally {
+			try {
+				if (statement != null){
+					statement.close();
+				}
+				if (connection != null){
+					connection.close();
+				}
+			} catch (SQLException e){
 				e.printStackTrace();
 			}
 		}

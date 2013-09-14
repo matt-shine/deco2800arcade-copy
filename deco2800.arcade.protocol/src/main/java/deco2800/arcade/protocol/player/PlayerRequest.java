1cd9a57dea7f370a1149faba04bd50c0ddfe240c
iamwinrar <andrew.cleland3@gmail.com>
2013-09-14 13:42:08 +1000

    Implemented PlayerRequest and PlayerResponse.

--- deco2800.arcade.protocol/src/main/java/deco2800/arcade/protocol/player/PlayerRequest.java
+++ deco2800.arcade.protocol/src/main/java/deco2800/arcade/protocol/player/PlayerRequest.java
@@ -3,5 +3,5 @@ package deco2800.arcade.protocol.player;
 import deco2800.arcade.protocol.BlockingMessage;
 
 public class PlayerRequest extends BlockingMessage {
-	
+	public int playerID;
 }
--- deco2800.arcade.protocol/src/main/java/deco2800/arcade/protocol/player/PlayerResponse.java
+++ deco2800.arcade.protocol/src/main/java/deco2800/arcade/protocol/player/PlayerResponse.java
@@ -1,7 +1,8 @@
 package deco2800.arcade.protocol.player;
 
+import deco2800.arcade.model.Player;
 import deco2800.arcade.protocol.BlockingMessage;
 
 public class PlayerResponse extends BlockingMessage {
-	
+	public Player player;
 }

daf372f76bbf64e7c3fcda729344a3c303d30fe5
AAAlpha <andy.tran@uqconnect.edu.au>
2013-09-15 22:14:17 +1000

    Added enemies images and implemented enemy class

--- deco2800.arcade.burningskies/src/main/java/deco2800/arcade/burningskies/entities/Enemy.java
+++ deco2800.arcade.burningskies/src/main/java/deco2800/arcade/burningskies/entities/Enemy.java
@@ -2,12 +2,43 @@ package deco2800.arcade.burningskies.entities;
 
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.math.Vector2;
+import deco2800.arcade.burningskies.screen.PlayScreen;
 
 
 public class Enemy extends Ship {
 
-	public Enemy(int health, Texture image, Vector2 pos) {
+	private float xPos;
+	private float yPos;
+	
+	//test parameters
+	private float xMin = 50;
+	private float xMax = 1050;
+	private float velocity = 400;
+	
+	
+	public Enemy(int health, Texture image, Vector2 pos, PlayScreen screen) {
 		super(health, image, pos);
-		// TODO Auto-generated constructor stub
+
+		xPos = pos.x;
+		yPos = pos.y;
+
+		// resizing the enemy unit to 1/3 of the original
+		this.setHeight((float) 100);
+		this.setWidth((float) 100);
+	}
+	
+	public void onRender(float delta) {
+		if(xPos < xMax && velocity > 0) { // check to make sure it can still move right
+			xPos += (float) velocity*delta;
+			if(xPos >= xMax)
+				velocity *= -1;
+		}
+		else if(xPos > xMin && velocity < 0) { // check to make sure it can still move left
+			xPos += (float) velocity*delta;
+			if(xPos <= xMin)
+				velocity *= -1;
+		}		
+		
+		this.setPosition(xPos, yPos);		
 	}
 }

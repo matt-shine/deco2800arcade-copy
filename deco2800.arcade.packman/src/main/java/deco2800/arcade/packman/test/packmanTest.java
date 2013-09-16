1bd430a01590cc601b7ddaca79550d49950d3a62
Egginator <sfegg0@hotmail.com>
2013-09-16 10:52:03 +1000

    added test attempt, copied games.txt so it could build.

---
+++ deco2800.arcade.packman/src/main/java/deco2800/arcade/packman/test/packmanTest.java
@@ -0,0 +1,17 @@
+/**package deco2800.arcade.packman.test;
+
+import org.junit.Test;
+import org.junit.Assert;
+/**import deco2800.arcade.packman.*;
+ * for the test to interact with the packman
+
+
+public class packmanTest {
+	
+	@Test
+	public void test1() {
+		Assert.assertTrue(true);
+	}
+	
+}
+*/

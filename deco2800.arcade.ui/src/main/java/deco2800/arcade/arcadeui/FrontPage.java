950e102a654004fed3a2f4e41eecea7d05c38dac
william-forsyth <WilliamForsyth@HalBook2000.local>
2013-09-20 13:13:24 +1000

    Revert b2e09d3..3d29747
    This rolls back to commit b2e09d30c9809a65bfe532561f4c6bad4f103785.

--- deco2800.arcade.ui/src/main/java/deco2800/arcade/arcadeui/FrontPage.java
+++ deco2800.arcade.ui/src/main/java/deco2800/arcade/arcadeui/FrontPage.java
@@ -13,11 +13,10 @@ import deco2800.arcade.client.ArcadeSystem;
 
 public class FrontPage {
 	
-	private class FrontPage extends Stage {}
+	private class LoginScreenStage extends Stage {}
 	
 	private Skin skin;
-    private Stage stage;
-    private menuBar sprite;
+    private LoginScreenStage stage;
     
     public FrontPage() {
     	
@@ -30,50 +29,6 @@ public class FrontPage {
         table.setFillParent(true);
         table.setBackground(skin.getDrawable("background"));
         stage.addActor(table);
-        
-        
     }
-    
-    @Override
-	public void show() {
-		ArcadeInputMux.getInstance().addProcessor(stage);
-	}
-    
-    @Override
-	public void render(float arg0) {
-		Gdx.gl.glClearColor(0, 0, 0, 1);
-		Gdx.gl.glClear(GL10.GL_COLOR_BUFFER_BIT);
-		
-		batch.begin();
-		bgSprite.draw(batch);
-		batch.end();
-		
-        stage.act(Gdx.graphics.getDeltaTime());
-        stage.draw();
-        Table.drawDebug(stage);  // Shows table debug lines
-	}
-	
-	@Override
-	public void dispose() {
-        stage.dispose();
-        skin.dispose();
-        ArcadeInputMux.getInstance().removeProcessor(stage);
-	}
-	
-	@Override
-	public void hide() {
-	}
-	
-	@Override
-	public void pause() {
-	}
-	
-	@Override
-	public void resume() {
-	}
-	
-	@Override
-	public void resize(int arg0, int arg1) {
-	}
 
 }

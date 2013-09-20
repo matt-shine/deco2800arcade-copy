5f297b5ffbafde036a222867fa61d40e0b81f9dd
william-forsyth <WilliamForsyth@HalBook2000.local>
2013-09-20 11:20:32 +1000

    Overrides.
    Added override methods to the end of FrontPage

--- deco2800.arcade.ui/src/main/java/deco2800/arcade/arcadeui/FrontPage.java
+++ deco2800.arcade.ui/src/main/java/deco2800/arcade/arcadeui/FrontPage.java
@@ -13,10 +13,11 @@ import deco2800.arcade.client.ArcadeSystem;
 
 public class FrontPage {
 	
-	private class LoginScreenStage extends Stage {}
+	private class FrontPage extends Stage {}
 	
 	private Skin skin;
-    private LoginScreenStage stage;
+    private Stage stage;
+    private menuBar sprite;
     
     public FrontPage() {
     	
@@ -29,6 +30,50 @@ public class FrontPage {
         table.setFillParent(true);
         table.setBackground(skin.getDrawable("background"));
         stage.addActor(table);
+        
+        
     }
+    
+    @Override
+	public void show() {
+		ArcadeInputMux.getInstance().addProcessor(stage);
+	}
+    
+    @Override
+	public void render(float arg0) {
+		Gdx.gl.glClearColor(0, 0, 0, 1);
+		Gdx.gl.glClear(GL10.GL_COLOR_BUFFER_BIT);
+		
+		batch.begin();
+		bgSprite.draw(batch);
+		batch.end();
+		
+        stage.act(Gdx.graphics.getDeltaTime());
+        stage.draw();
+        Table.drawDebug(stage);  // Shows table debug lines
+	}
+	
+	@Override
+	public void dispose() {
+        stage.dispose();
+        skin.dispose();
+        ArcadeInputMux.getInstance().removeProcessor(stage);
+	}
+	
+	@Override
+	public void hide() {
+	}
+	
+	@Override
+	public void pause() {
+	}
+	
+	@Override
+	public void resume() {
+	}
+	
+	@Override
+	public void resize(int arg0, int arg1) {
+	}
 
 }

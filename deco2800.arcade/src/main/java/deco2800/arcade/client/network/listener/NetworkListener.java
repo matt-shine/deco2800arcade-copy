a1176219f02aa9511581243991981254ce8cb179
herotl111 <herotl111@hotmail.com>
2013-09-21 20:26:54 +1000

    clean arcade

--- deco2800.arcade/src/main/java/deco2800/arcade/client/network/listener/NetworkListener.java
+++
@@ -1,7 +0,0 @@
-package deco2800.arcade.client.network.listener;
-
-import com.esotericsoftware.kryonet.Listener;
-
-public abstract class NetworkListener extends Listener {
-
-}
--- deco2800.arcade/src/main/java/deco2800/arcade/client/replay/ReplayEventListener.java
+++
@@ -1,7 +0,0 @@
-package deco2800.arcade.client.replay;
-
-import java.util.EventListener;
-
-public interface ReplayEventListener extends EventListener {
-	public void replayEventReceived( String eType, ReplayNode eData );
-}

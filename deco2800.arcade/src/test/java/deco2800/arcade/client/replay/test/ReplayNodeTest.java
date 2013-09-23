83fec1ad76e1f0c62e296f24a6824e121db71f6d
Ben Follington <bfollington@gmail.com>
2013-09-23 14:21:55 +1000

    ReplayNodeTest done..!

--- deco2800.arcade/src/test/java/deco2800/arcade/client/replay/test/ReplayNodeTest.java
+++ deco2800.arcade/src/test/java/deco2800/arcade/client/replay/test/ReplayNodeTest.java
@@ -14,11 +14,12 @@ public class ReplayNodeTest {
     private ReplayItem r1 = new ReplayItem(123);
     private ReplayItem r2 = new ReplayItem(45.345f);
     private ReplayItem r3 = new ReplayItem("test");
-    private HashMap<String, ReplayItem> items = new HashMap<String, ReplayItem>();
+    private HashMap<String, ReplayItem> items;
 
     @Test
     public void testGetType()
     {
+        items = new HashMap<String, ReplayItem>();
         items.put("x", r1);
         items.put("y", r2);
         items.put("name", r3);
@@ -31,6 +32,7 @@ public class ReplayNodeTest {
     @Test
     public void testGetItems()
     {
+        items = new HashMap<String, ReplayItem>();
         items.put("x", r1);
         items.put("y", r2);
         items.put("name", r3);
@@ -44,5 +46,76 @@ public class ReplayNodeTest {
         assertEquals(45.345f, r2, 0);
         assertEquals("test", rn.getItemForString("name").stringVal());
     }
+    
+    @Test
+    public void testGetTime()
+    {
+        items = new HashMap<String, ReplayItem>();
+        items.put("x", r1);
+        items.put("y", r2);
+        items.put("name", r3);
+        
+        ReplayNode rn = new ReplayNode("test_event", items);
+        
+        assertEquals(0, rn.getTime());
+    }
+    
+    @Test
+    public void testSetTime()
+    {
+        items = new HashMap<String, ReplayItem>();
+        items.put("x", r1);
+        items.put("y", r2);
+        items.put("name", r3);
+        
+        ReplayNode rn = new ReplayNode("test_event", items);
+        
+        rn.setTime(123);
+        assertEquals(123, rn.getTime());
+    }
+    
+    @Test
+    public void testItems()
+    {
+        items = new HashMap<String, ReplayItem>();
+        items.put("x", r1);
+        items.put("y", r2);
+        items.put("name", r3);
+        
+        ReplayNode rn = new ReplayNode("test_event", items);
+        
+        assertEquals(rn.getItems(), items);
+    }
+    
+    @Test
+    public void testSmallConstructor()
+    {        
+        ReplayNode rn = new ReplayNode("test_event");
+        
+        assertEquals(rn.getItems(), new HashMap<String, ReplayItem>());
+        assertEquals(rn.getTime(), 0);
+    }
+    
+    @Test
+    public void testAlternateConstructor()
+    {
+        ReplayNode rn = new ReplayNode("test_event", 999);
+        
+        assertEquals(rn.getTime(), 999);
+    }
+    
+    @Test
+    public void testFinalConstructor()
+    {
+        items = new HashMap<String, ReplayItem>();
+        items.put("x", r1);
+        items.put("y", r2);
+        items.put("name", r3);
+        
+        ReplayNode rn = new ReplayNode("test_event", items, 999);
+        
+        assertEquals(rn.getTime(), 999);
+    }
+    
 
 }

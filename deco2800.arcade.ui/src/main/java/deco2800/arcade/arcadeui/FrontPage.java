3d0ed4c8f71a58ecd449e3513a3fdf779e7e705a
william-forsyth <WilliamForsyth@HalBook2000.local>
2013-09-23 19:01:22 +1000

    Icon Animation
    Fixed hoverover

--- deco2800.arcade.ui/src/main/java/deco2800/arcade/arcadeui/FrontPage.java
+++ deco2800.arcade.ui/src/main/java/deco2800/arcade/arcadeui/FrontPage.java
@@ -46,6 +46,7 @@ public class FrontPage implements Screen {
     private int tokens;
     
     private boolean bclicked;
+    
     Texture bg;
     Sprite bgSprite;
     SpriteBatch batch;
@@ -75,64 +76,79 @@ public class FrontPage implements Screen {
         
         
         //Text Buttons
-        final TextButton storeButton = new TextButton("Store", skin, "blue");
+        final TextButton storeButton = new TextButton("Store", skin, "green");
         //skin.add("Store", new Texture("homescreen_bg.png"));
         final TextButton chatButton = new TextButton("Chat", skin);
-        final TextButton libraryButton = new TextButton("Library", skin);
-        final TextButton recentButton = new TextButton("Recently Played", skin);
+        final TextButton libraryButton = new TextButton("Library", skin, "magenta");
+        final TextButton recentButton = new TextButton("Recently Played", skin, "blue");
         
+        
+        final int bWidth = 300;
+        final int bHeight = 300;
+        final int bX = 150;
+        final int bY = 220;
+        final int enlarge = 50;
+        final int bX2= bX + bWidth + (enlarge);
+        final int bX3= bX + 2*(bWidth + enlarge);
         //make button sizes and positioning
-        storeButton.setSize(300, 300);
-        storeButton.setPosition(190, 200);
         
-        libraryButton.setSize(190, 190);
-        libraryButton.setPosition(490, 300);
+      
+        recentButton.setSize(bWidth, bHeight);        
+        recentButton.setPosition(bX, bY);
+       
+        libraryButton.setSize(bWidth, bHeight);
+        libraryButton.setPosition(bX2, bY);
         
-        recentButton.setSize(190, 190);
-        recentButton.setPosition(790, 300);
+        storeButton.setSize(bWidth, bHeight);
+        storeButton.setPosition(bX3, bY);
         
-        
-        //adding to stage
-        stage.addActor(storeButton);
-        stage.addActor(libraryButton);
-        stage.addActor(recentButton);
-        
-        storeButton.addListener((new ClickListener() {
-        	
-            public void enter (InputEvent event, float x, float y, int pointer, Actor fromActor) {
-            	 if (storeButton.isOver() == true){ 
-         			System.out.println("store moved");
-         			storeButton.setSize(600, 600);
-            	 }
- 
-            }
-            public void exit (InputEvent event, float x, float y, int pointer, Actor fromActor){
-            	
-            	System.out.println("store moved back");
-       		 storeButton.setSize(300, 300);
-            	
-            }
-        }));   
         
        
         
-        storeButton.addListener((new ChangeListener() {
-        	
-        	
-        	
-            public void changed (ChangeEvent event, Actor actor) {
-            	
-                
+        //float height = storeButton.getHeight();
+    	//float width = storeButton.getWidth();
 
-            	
-            	ArcadeSystem.login("store");
-            	bclicked = true;
-            	System.out.println("store clicked");
-            }
-        })
         
-        	
-        		); 
+        recentButton.addListener((new ClickListener() {        	
+            public void enter (InputEvent event, float x, float y, int pointer, Actor fromActor) {	
+           		recentButton.setSize(bWidth + enlarge,  bHeight + enlarge);
+           		recentButton.setPosition(bX -(enlarge/2), bY-(enlarge/2));
+           		recentButton.setText(null);
+               	recentButton.setText("Recently Played");
+            		
+            		 			     			
+           
+            }
+            public void exit (InputEvent event, float x, float y, int pointer, Actor fromActor){
+            	recentButton.setSize(bWidth, bHeight);
+            	recentButton.setPosition(bX, bY);
+            	recentButton.setText(null);
+            	recentButton.setText("Recently Played");
+            }}));   
+        
+	    
+		recentButton.addListener((new ChangeListener() {
+		    public void changed (ChangeEvent event, Actor actor) {
+		    	ArcadeSystem.login("recent");
+		    	bclicked = true;
+		    	System.out.println("recent clicked");
+		    }
+		})); 
+        
+        libraryButton.addListener((new ClickListener() {        	
+            public void enter (InputEvent event, float x, float y, int pointer, Actor fromActor) {	    	
+           		 libraryButton.setSize(bWidth + enlarge,  bHeight + enlarge);
+           		 libraryButton.setPosition(bX2 -(enlarge/2), bY-(enlarge/2));	
+           		 libraryButton.setText(null);
+           		 libraryButton.setText("Library");
+            	 
+            }
+            public void exit (InputEvent event, float x, float y, int pointer, Actor fromActor){
+            	libraryButton.setSize(bWidth, bHeight);
+            	libraryButton.setPosition(bX2, bY);
+            	libraryButton.setText(null);
+            	libraryButton.setText("Library");
+            }}));   
         
 	    libraryButton.addListener((new ChangeListener() {
 	        public void changed (ChangeEvent event, Actor actor) {
@@ -142,177 +158,40 @@ public class FrontPage implements Screen {
 	        }
 	    })); 
 	    
-	
-		recentButton.addListener((new ChangeListener() {
-		    public void changed (ChangeEvent event, Actor actor) {
-		    	ArcadeSystem.login("recent");
-		    	bclicked = true;
-		    	System.out.println("recent clicked");
-		    }
-		})); }
+        
+        storeButton.addListener((new ClickListener() {        	
+            public void enter (InputEvent event, float x, float y, int pointer, Actor fromActor) {	
+            	  
+       			storeButton.setSize(bWidth + enlarge,  bHeight + enlarge);
+         		storeButton.setPosition(bX3 -(enlarge/2), bY-(enlarge/2));			     			
+         		storeButton.setText(null);
+         		storeButton.setText("Store");
+            }
+            public void exit (InputEvent event, float x, float y, int pointer, Actor fromActor){
+            	storeButton.setSize(bWidth, bHeight);
+                storeButton.setPosition(bX3, bY);
+                storeButton.setText(null);
+     			storeButton.setText("Store");
+            }}));   
+        
+        storeButton.addListener((new ChangeListener() {	
+            public void changed (ChangeEvent event, Actor actor) {
+            	ArcadeSystem.login("store");
+            	System.out.println("store clicked");
+            	bclicked = true;
+            }
+        })); 
+        
+		
+        
+        //adding to stage
+        stage.addActor(recentButton);
+        stage.addActor(libraryButton);
+        stage.addActor(storeButton);
+    }
 
         
-        
-            	
-        
-        /*Pixmap pixmap = new Pixmap(1, 1, Pixmap.Format.RGBA8888);
-        pixmap.setColor(Color.WHITE);
-        pixmap.fill();
-        skin.add("white", new Texture(pixmap));
-        
-        TextField.TextFieldStyle textFieldStyle = new TextField.TextFieldStyle();
-        textFieldStyle.font = skin.getFont("default");
-        textFieldStyle.fontColor = Color.WHITE;
-        textFieldStyle.cursor = skin.newDrawable("white", Color.WHITE);
-        textFieldStyle.selection = skin.newDrawable("white", Color.WHITE);
-        skin.add("default", textFieldStyle);
-        
-        TextButton.TextButtonStyle textButtonStyle = new TextButton.TextButtonStyle();
-        textButtonStyle.up = skin.newDrawable("white", Color.DARK_GRAY);
-        textButtonStyle.down = skin.newDrawable("white", Color.DARK_GRAY);
-        //textButtonStyle.checked = skin2.newDrawable("white", Color.WHITE);
-        textButtonStyle.over = skin.newDrawable("white", Color.LIGHT_GRAY);
-        textButtonStyle.font = skin.getFont("default");
-        skin.add("default", textButtonStyle);*/
-        
-       
-        
-        /*final Table leftBox = new Table();
-        final Table featuredBox = new Table();		// 'featured' box at top left of screen
-        final Table leftButtonBox = new Table();	// buttons to cycle through featured games
-        final Table rightButtonBox = new Table();	// ...
-        final Table topBox = new Table();
-        final Table botBox = new Table();
-        final Table transactions = new Table();
-        final Table search = new Table();
-        
-        leftBox.setSize(680, 500);
-        leftBox.setPosition(20, 20);
-        featuredBox.setSize(540, 160);
-        featuredBox.setPosition(90, 540);
-        leftButtonBox.setSize(50, 160);
-        leftButtonBox.setPosition(20, 540);
-        rightButtonBox.setSize(50, 160);
-        rightButtonBox.setPosition(650, 540);
-        topBox.setSize(540, 160);
-        topBox.setPosition(720, 540);
-        botBox.setSize(540, 500);
-        botBox.setPosition(720, 20);
-        
-        stage.addActor(leftBox);
-        stage.addActor(featuredBox);
-        stage.addActor(leftButtonBox);
-        stage.addActor(rightButtonBox);
-        stage.addActor(topBox);
-        stage.addActor(botBox);
-        
-        final Label searchLabel = new Label("Search:", skin);
-        final TextField searchField = new TextField("", skin);
-        final TextButton searchButton = new TextButton("Search", skin);
-        searchField.setMessageText("Search");
-        
-        final TextButton transactionsButton = new TextButton("Transactions", skin);
-        final TextButton buyGameButton = new TextButton("Buy Game", skin);
-        final TextButton buyTokenButton = new TextButton("Buy Tokens", skin);
-        final TextButton addFundsButton = new TextButton("Add Funds", skin);
-        final TextField buyField = new TextField("0", skin);
-        final Label fundLabel = new Label("$" + funds, skin);
-        final Label tokenLabel = new Label(tokens + " Tokens", skin);
-        
-        leftBox.debug(); // Shows table debug lines
-        featuredBox.debug();
-        leftButtonBox.debug();
-        rightButtonBox.debug();
-        topBox.debug();
-        botBox.debug();
-        
-        leftBox.add(search);
-        search.add(searchLabel).width(65);
-        search.add(searchField).width(135);
-        search.add(searchButton);
-        
-        // 'Featured games' box
-        final Label featuredLabel = new Label("Featured games!", skin);
-        
-        featuredBox.add(featuredLabel);
-        
-        
-        // Left and right button boxes
-        final TextButton prevFeatButton = new TextButton("<<", skin);
-        final TextButton nextFeatButton = new TextButton(">>", skin);
-        leftButtonBox.add(prevFeatButton).width(50);
-        rightButtonBox.add(nextFeatButton).width(50);
-        
-        
-        // Transactions Tab
-        botBox.add(transactionsButton).width(200);
-        botBox.row();
-        botBox.add(transactions);
-        
-        
-        topBox.add(fundLabel).width(100);
-        topBox.add(tokenLabel).width(100)*/
-        
-        /*searchButton.addListener(new ChangeListener() {
-            public void changed (ChangeEvent event, Actor actor) {
-                System.out.println("Searching For: " + searchField.getText());
-            }
-        });
-        transactionsButton.addListener(new ChangeListener() {
-            public void changed (ChangeEvent event, Actor actor) {
-                System.out.println("toggle");
-                if(transactions.isVisible()) {
-                	transactions.setVisible(false);
-                } else {
-                	transactions.setVisible(true);
-                }
-            }
-        });
-        buyGameButton.addListener(new ChangeListener() {
-            public void changed (ChangeEvent event, Actor actor) {
-                System.out.println("bought game");
-            }
-        });
-        buyTokenButton.addListener(new ChangeListener() {
-            public void changed (ChangeEvent event, Actor actor) {
-            	if (isInteger(buyField.getText())) {
-            		tokens += Integer.parseInt(buyField.getText());
-                    tokenLabel.setText(tokens + " Tokens");
-                    System.out.println("bought " + buyField.getText() + " tokens\nNow have " + tokens);
-            	}
-            	else {
-            		System.out.println("please enter positive integers only");
-            	}
-                
-            }
-        });
-        addFundsButton.addListener(new ChangeListener() {
-            public void changed (ChangeEvent event, Actor actor) {
-            	if (Float.parseFloat(buyField.getText()) < 0) {
-            		// prevent users from subtracting from their funds
-            		System.out.println("please enter positive values only");
-            	}
-            	else {
-            		// add funds
-            		funds += Float.parseFloat(buyField.getText());
-                    fundLabel.setText("$" + funds);
-                    System.out.println("added " + buyField.getText() + " funds\nNow have $" + funds);
-            	}
-                
-            }
-        });*/
     
-    
-    
-    /*public static boolean isInteger(String s) {
-        try { 
-            Integer.parseInt(s); 
-        } catch(NumberFormatException e) { 
-            return false; 
-        }
-        // only got here if we didn't return false
-        return true;
-    }*/
     
     
     

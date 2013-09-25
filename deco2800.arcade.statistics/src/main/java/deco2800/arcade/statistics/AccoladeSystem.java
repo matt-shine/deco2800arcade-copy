428885c56c9050c9cfb1cdfb0307ea3bd649b7d1
Mitchell Klein <mitchell931993@gmail.com>
2013-09-25 11:45:25 +1000

    This is the barebones skeleton class for the AccoladeSystem. It has zero functionality because this required file will just be to allow initial placement within other games. Immeadiate work after commit on functionality with occur

---
+++ deco2800.arcade.statistics/src/main/java/deco2800/arcade/statistics/AccoladeSystem.java
@@ -0,0 +1,55 @@
+package deco2800.arcade.statistics;
+
+public class AccoladeSystem {
+	
+	//constructor
+	public AccoladeSystem(){
+		//start the local variables for the overlay system
+	}
+	
+	public void push(String accolade, int increment){
+		//say HIIIII to the server, then pass along 
+		//"pXXXX,aXXXXXX, int"
+	}
+	
+	//returns the Primary key for the string the accolade is associated with
+	public int fetchID(String accolade){
+		return -1;
+	}
+	
+	public Watcher watch(){
+		return new Watcher();
+	}
+	
+	
+	
+	public class Watcher{
+		
+		public Watcher(){
+			
+		}
+		
+		public void start(){
+			
+		}
+		
+		public void stop(){
+			
+		}
+		
+		public void pause(){
+			
+		}
+		
+		public void unpause(){
+			
+		}
+	}
+}
+
+
+
+
+
+
+

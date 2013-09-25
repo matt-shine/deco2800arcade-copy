0393e9e8848351c3a7000712dd1931262dcf65e9
yanbo-zhao <zhaoyanbo_ivy@hotmail.com>
2013-09-25 20:02:14 +1000

    add files

---
+++ deco2800.arcade.snakeLadder/src/main/java/deco2800/arcade/snakeLadderModel/RuleExcutingState.java
@@ -0,0 +1,14 @@
+package deco2800.arcade.snakeLadderModel;
+
+import deco2800.arcade.snakeLadder.SnakeLadder;
+import com.badlogic.gdx.Gdx;
+
+public class RuleExcutingState extends GameState {
+
+	@Override
+	public void handleInput(SnakeLadder context) {
+		// TODO Auto-generated method stub
+
+	}
+
+}

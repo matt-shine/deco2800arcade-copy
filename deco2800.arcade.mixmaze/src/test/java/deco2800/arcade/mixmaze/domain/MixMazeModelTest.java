9f64e5c17a962c990a1ee31f018fe200676e91ca
Luran Li <luran.all@gmail.com>
2013-09-28 06:30:43 +1000

    Fix test MixMazeModelTest #32

--- deco2800.arcade.mixmaze/src/test/java/deco2800/arcade/mixmaze/domain/MixMazeModelTest.java
+++ deco2800.arcade.mixmaze/src/test/java/deco2800/arcade/mixmaze/domain/MixMazeModelTest.java
@@ -1,25 +1,24 @@
 package deco2800.arcade.mixmaze.domain;
 
-import static org.junit.Assert.*;
-
 import org.junit.Test;
 
-import deco2800.arcade.mixmaze.domain.view.IMixMazeModel.MixMazeDifficulty;
+import static deco2800.arcade.mixmaze.domain.view.IMixMazeModel.Difficulty.*;
+import static org.junit.Assert.*;
 
 public class MixMazeModelTest {
 	@Test(expected=IllegalArgumentException.class)
 	public void mixMazeSizeOutOfRange() {
-		new MixMazeModel(-1, MixMazeDifficulty.Beginner, 60);
+		new MixMazeModel(-1, BEGINNER, 60);
 	}
 
 	@Test(expected=IllegalArgumentException.class)
 	public void mixMazeTimeOutOfRange() {
-		new MixMazeModel(5, MixMazeDifficulty.Beginner, -1);
+		new MixMazeModel(5, BEGINNER, -1);
 	}
 
 	@Test
 	public void mixMazeInitialization() {
-		MixMazeModel mixMaze = new MixMazeModel(5, MixMazeDifficulty.Beginner, 60);
+		MixMazeModel mixMaze = new MixMazeModel(5, BEGINNER, 60);
 		assertEquals(5, mixMaze.getBoardSize());
 		for(int row = 0; row < mixMaze.getBoardSize(); ++row) {
 			for(int column = 0; column < mixMaze.getBoardSize(); ++column) {
@@ -29,7 +28,7 @@ public class MixMazeModelTest {
 				assertEquals(row, tile.getY());
 			}
 		}
-		assertEquals(MixMazeDifficulty.Beginner, mixMaze.getGameDifficulty());
+		assertEquals(BEGINNER, mixMaze.getGameDifficulty());
 		assertEquals(60, mixMaze.getGameMaxTime());
 		assertEquals(null, mixMaze.getGameStartTime());
 		assertEquals(null, mixMaze.getGameEndTime());

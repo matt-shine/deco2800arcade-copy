4050fc399cd840e4b5c457403c32c74e1c28e0b8
abbjohn <abbjohn@gmail.com>
2013-09-27 12:55:48 +1000

    Working on the clock.

--- deco2800.arcade.ui/src/main/java/deco2800/arcade/arcadeui/Overlay/Clock.java
+++ deco2800.arcade.ui/src/main/java/deco2800/arcade/arcadeui/Overlay/Clock.java
@@ -6,7 +6,11 @@ import com.badlogic.gdx.graphics.g2d.NinePatch;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.scenes.scene2d.Group;
 import com.badlogic.gdx.scenes.scene2d.Stage;
+import com.badlogic.gdx.scenes.scene2d.ui.Label;
+import com.badlogic.gdx.scenes.scene2d.ui.Skin;
 import deco2800.arcade.client.ArcadeInputMux;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 
 public class Clock extends Group {
@@ -16,24 +20,33 @@ public class Clock extends Group {
     private Overlay overlay;
     private ClockStage contentStage = new ClockStage();
     private Group contentGroup = new Group();
+    private Skin skin;
     private NinePatch texture;
     private int width;
     private int height;
+    private SimpleDateFormat simpleDateFormat;
+    private Label clock;
 
     public Clock(Overlay overlay) {
         this.overlay = overlay;
+        skin = new Skin(Gdx.files.internal("loginSkin.json"));
         texture = new NinePatch(new Texture(Gdx.files.internal("iconMagenta.png")), 75, 75, 75, 75);
+
+        simpleDateFormat = new SimpleDateFormat("h:mm a");
+        System.out.println(simpleDateFormat.format(new Date()));
+        clock = new Label("", skin);
+        clock.setText(simpleDateFormat.format(new Date()));
+
         contentStage.addActor(contentGroup);
         ArcadeInputMux.getInstance().addProcessor(contentStage);
-        //width = overlay.getWidth() - 320;
-        //height = overlay.getHeight() - 200;
-        width = 400;
-        height = 200;
+        width = 350;
+        height = 150;
     }
 
     @Override
     public void draw(SpriteBatch batch, float parentAlpha) {
         texture.draw(batch, overlay.getWidth() - width + 25, overlay.getHeight() - height + 25, width, height);
+        clock.setPosition(overlay.getWidth() - width + 25, overlay.getHeight() - height + 25);
         super.draw(batch, parentAlpha);
     }
 

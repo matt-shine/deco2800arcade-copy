4baa47905379c4b69404177423a7b28ee1d32826
Luran Li <luran.all@gmail.com>
2013-09-27 19:58:38 +1000

    Remove build files and restore .gitignore

--- deco2800.arcade.wl6/build/resources/main/wl6DoodadFragShader.glsl
+++
@@ -1,9 +0,0 @@
-
-uniform sampler2D texture;
-uniform vec2 u_texoffset;
-varying vec2 v_texpos;
-
-void main (void)  
-{
-   gl_FragColor = texture2D(texture, v_texpos + u_texoffset);
-}
--- deco2800.arcade.wl6/build/resources/main/wl6TerrainFragShader.glsl
+++
@@ -1,8 +0,0 @@
-
-uniform sampler2D texture;
-varying vec2 v_texpos;
-
-void main (void)  
-{
-    gl_FragColor = texture2D(texture, v_texpos);
-}

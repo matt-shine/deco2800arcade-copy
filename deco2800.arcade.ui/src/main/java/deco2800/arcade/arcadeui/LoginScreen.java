9bbba83b0d59e87888d2ad0241bfbcd2bdf18edd
matt-shine <matthew.shine@uqconnect.edu.au>
2013-09-29 08:13:36 +1000

    LoginScreen merge issues fixed.

--- deco2800.arcade.ui/src/main/java/deco2800/arcade/arcadeui/LoginScreen.java
+++ deco2800.arcade.ui/src/main/java/deco2800/arcade/arcadeui/LoginScreen.java
@@ -13,42 +13,12 @@ import deco2800.arcade.client.ArcadeInputMux;
 import deco2800.arcade.client.ArcadeSystem;
 
 public class LoginScreen implements Screen {
-	
+
+	private class LoginScreenStage extends Stage {}
+
 	private Skin skin;
-	private Skin skin2;
     private LoginScreenStage stage;
     private ArcadeUI arcadeUI;
-	
-	private class LoginScreenStage extends Stage {}
-	
-	public LoginScreen() {
-
-		// Move skin stuff to an overall class
-
-        //skin = new Skin(Gdx.files.internal("loginSkin.json"));
-
-
-        skin = new Skin();
-
-        // skin is the skin loaded from loginSkin.json
-        // skin2 is for the skin created programatically
-        // skin2 will eventually disappear
-        skin = new Skin(Gdx.files.internal("loginSkin.json"));
-        skin2 = new Skin();
-        
-        Pixmap pixmap = new Pixmap(1, 1, Pixmap.Format.RGBA8888);
-        pixmap.setColor(Color.WHITE);
-        pixmap.fill();
-		
-        skin.add("white", new Texture(pixmap));
-        skin2.add("white", new Texture(pixmap));
-        
-        skin.add("default", new BitmapFont());
-        
-        Label.LabelStyle labelStyle = new Label.LabelStyle();
-        labelStyle.font = skin.getFont("default");
-        skin.add("default", labelStyle);
-
 
 	public LoginScreen(ArcadeUI ui) {
         arcadeUI = ui;
@@ -73,15 +43,6 @@ public class LoginScreen implements Screen {
         passwordText.setMessageText("Password");
         passwordText.setPasswordMode(true);
         passwordText.setPasswordCharacter('*');
-        TextButton loginButton = new TextButton("Login", skin2);
-        TextButton exitButton = new TextButton("Exit", skin2);
-
-        table.add(usernameLabel);
-        table.add(usernameText).width(100);
-        
-        table.row();
-        table.add(passwordLabel);
-        table.add(passwordText).width(100);
         final TextField serverText = new TextField("", skin);
         serverText.setMessageText("Server") ;
         CheckBox rememberBox = new CheckBox("Remember Me", skin);
@@ -140,18 +101,6 @@ public class LoginScreen implements Screen {
         });
 	}
 
-
-	@Override
-	public void show() {
-	}
-	
-
-	public void firstResize() {
-/*
-		ArcadeSystem.openConnection();
-*/		
-	}
-	
 	@Override
 	public void render(float arg0) {
 		Gdx.gl.glClear(GL10.GL_COLOR_BUFFER_BIT);
@@ -168,7 +117,6 @@ public class LoginScreen implements Screen {
         ArcadeInputMux.getInstance().addProcessor(stage);
     }
 
-
 	@Override
 	public void hide() {
         ArcadeInputMux.getInstance().removeProcessor(stage);
@@ -180,13 +128,6 @@ public class LoginScreen implements Screen {
 
 	@Override
 	public void resume() {
-	}
-
-
-	@Override
-	public void resize(int arg0, int arg1) {
-		// TODO Auto-generated method stub
-		
 	}
 
     @Override

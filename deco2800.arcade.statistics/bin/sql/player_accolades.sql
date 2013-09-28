deaf876ea838386b519b0d2dd04b076f50f9e1e4
abbjohn <abbjohn@gmail.com>
2013-09-28 19:21:21 +1000

    remove /bin folders.

--- deco2800.arcade.statistics/bin/sql/player_accolades.sql
+++
@@ -1,8 +0,0 @@
-CREATE TABLE Player_Accolades (
-accolade_id int(11) NOT NULL,
-player_id int(11) NOT NULL,
-score int(32) NOT NULL,
-PRIMARY KEY (accolade_id, player_id),
-FOREIGN KEY (accolade_id) REFERENCE Game_Accolades(accolade_id)
-/**, FOREIGN KEY (player_id) REFERENCE Players(accolade_id)
-**/ );

4fd0ee6d3d36cee1ed4029c35df9c1609a3816cd
Justin Comino <4288850@student.uq.edu.au>
2013-10-01 14:19:16 -0700

    Began filling space with triangles, polygons are broken!

--- deco2800.arcade.lunarlander/src/main/java/deco2800/arcade/lunarlander/LunarLander.java
+++ deco2800.arcade.lunarlander/src/main/java/deco2800/arcade/lunarlander/LunarLander.java
@@ -3,21 +3,22 @@ package deco2800.arcade.lunarlander;
 import java.util.*;
 
 import com.badlogic.gdx.Gdx;
-import com.badlogic.gdx.math.Polygon;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.TextureRegion;
 import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.graphics.GL10;
 import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.g2d.BitmapFont;
-import com.badlogic.gdx.graphics.g2d.PolygonRegion;
 import com.badlogic.gdx.graphics.g2d.PolygonSprite;
 import com.badlogic.gdx.graphics.g2d.PolygonSpriteBatch;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.graphics.glutils.ShapeRenderer;
 import com.badlogic.gdx.graphics.glutils.ShapeRenderer.ShapeType;
+
+import java.awt.Graphics;
+import java.awt.Polygon;
+
 import com.badlogic.gdx.Input.Keys;
-import com.badlogic.gdx.math.Polygon;
 
 import deco2800.arcade.model.Game;
 import deco2800.arcade.model.Game.ArcadeGame;
@@ -32,6 +33,7 @@ public class LunarLander extends GameClient {
 	private OrthographicCamera camera;
 	public static final int SCREENHEIGHT = 800;
 	public static final int SCREENWIDTH = 1200;
+	private static final Graphics Graphics = null;
 	
 	// Gdx tools
 	private ShapeRenderer shapeRenderer; // draws shapes
@@ -50,17 +52,6 @@ public class LunarLander extends GameClient {
 	private float initialPositionX; // the initial position of the lander - x coordinate
 	private float initialPositionY; // the initial postiion of the lander - y coordinate
 	
-	private float acceleration;
-	private double angle;
-	//private double speed;
-	private double scaleX;
-	private double scaleY;
-	private double velocityX;
-	private double velocityY;
-	private int finalX;
-	private int finalY;
-	private int gravity;
-	private boolean moving;
 	
 	//terrain generation
 	private List<List<Integer>> terrain;
@@ -69,12 +60,6 @@ public class LunarLander extends GameClient {
 	PolygonSprite poly;
 	PolygonSpriteBatch polyBatch;
 	Texture textureSolid;
-	
-	// coordinates of the lander pad
-	private float landerPadLeftX;
-	private float landerPadLeftY;
-	private float landerPadRightX;
-	private float landerPadRightY;
 
 	
 	// statistics to be displayed on game screen
@@ -103,19 +88,6 @@ public class LunarLander extends GameClient {
         this.networkClient = networkClient;  
 	}
 	
-//	    float a = 100;
-//	    float b = 100;
-//	    PolygonRegion polyReg = new PolygonRegion(
-//	      backgroundTextureRegion2, new float[] {
-//	        a*0, b*0,
-//	        a*0, b*2,
-//	        a*3, b*2,
-//	        a*3, b*0,
-//	        a*2, b*0,
-//	        a*2, b*1,
-//	        a*1, b*1,
-//	        a*1, b*0,
-//	    });
 	
 	/**
 	 * Creates the game
@@ -144,24 +116,10 @@ public class LunarLander extends GameClient {
 		initialPositionY = 750;
 		initialPositionX = 600;
 		landerX = 30;
-		landerY = 30;		
-		acceleration = 0;
-		angle = 5;
+		landerY = 30;
 		speed = 1;
-		acceleration = 2;
-		scaleX = Math.cos(angle);
-		scaleY = Math.sin(angle);
-		velocityX = (speed*scaleX);
-		velocityY = (speed*scaleY);
-		gravity = 3;
-		moving = true;
 		
 		randomMap = true;
-		
-		landerPadLeftX = 25;
-		landerPadLeftY = 50;
-		landerPadRightX = 100;
-		landerPadRightY = 100;
 		terrain = createMap();
 	}
 
@@ -170,7 +128,10 @@ public class LunarLander extends GameClient {
 	 */
 	@Override
 	public void render() {
-			
+		
+		
+		Graphics gragon = Graphics;
+		
 		// move lander left
 		if ((Gdx.input.isKeyPressed(Keys.A)) || (Gdx.input.isKeyPressed(Keys.LEFT))) { 
 			initialPositionX -= Gdx.graphics.getDeltaTime() * sideSpeed;
@@ -220,32 +181,27 @@ public class LunarLander extends GameClient {
 	    //set map to be randomonly made, or not
 	    if(randomMap == true){
 	    for (int i = 0; i < terrain.size(); i++){
+	    	
+	    	shapeRenderer.begin(ShapeType.Line);
 	    	shapeRenderer.line(terrain.get(i).get(0), terrain.get(i).get(1), terrain.get(i).get(2), terrain.get(i).get(3));
+	    	shapeRenderer.end();
+	    	
+	    	shapeRenderer.begin(ShapeType.FilledTriangle);
+		    shapeRenderer.setColor(1, 1, 1, 1);
+			shapeRenderer.filledTriangle(terrain.get(i).get(0), terrain.get(i).get(1), terrain.get(i).get(2), terrain.get(i).get(3),34,34);		
+			shapeRenderer.end();
 	    }
 	    }else{
 	    	//load premade ArrayList of points, and background texture
 	    }
 	    
-//	    polyBatch.begin();
-//	    poly.draw(polyBatch);
-//	    polyBatch.end();
-//	    poly.rotate(1.1f);
-	    
 	    //colors the landing pad green
 	    shapeRenderer.setColor(255, 0, 0, 1);
 	    shapeRenderer.line(terrain.get(0).get(0), terrain.get(0).get(1), terrain.get(0).get(2), terrain.get(0).get(3));
 	    shapeRenderer.setColor(1, 1, 1, 1);
-	    //End drawing of shapes
 	    shapeRenderer.end();
 	    
-	    shapeRenderer.begin(ShapeType.FilledTriangle);
-	    shapeRenderer.setColor(1, 1, 1, 1);
-		shapeRenderer.filledTriangle(300, 300, 100, 500, 500, 500);
-	    shapeRenderer.end();
-	    
-	    //shapeRenderer.begin(ShapeType.);
-	    //shapeRenderer. //(new float[] {0,0,5,5,5,2.5f, 10,12.5f, 10,0});
-	    
+
 	    
 	    /*if(!(initPosition + finalY <= 20) && moving == true){
 	    	
@@ -264,15 +220,6 @@ public class LunarLander extends GameClient {
 	    	initialPositionY -= 0.25;
 	    }
 	    
-	/*    	velocityY = velocityY - gravity;
-	    	speed = speed + acceleration;
-		    velocityX = (int) (speed * scaleX);
-		    velocityY = (int) (speed * scaleY);
-		    finalX = (int) (finalX + velocityX);
-		    finalY = (int) (finalY + velocityY);
-		    moving = false;
-	*/
-	    
 		super.render();
 		
 	}
@@ -281,12 +228,9 @@ public class LunarLander extends GameClient {
 		
 		List<List<Integer>> terrain = new ArrayList<List<Integer>>();
 		
-		//example code for a random int ranging from 50 to 100
-		//int randomNumber = 50 + new Random().nextInt( 100 - 50 + 1 );
-		
 		//creating the landing pads coordinates
-		int landPadLeftX = 100 + new Random().nextInt( 700 - 100 + 1 );
-		int landPadLeftY = 50 + new Random().nextInt( 200 - 50 + 1 );
+		int landPadLeftX = randNum(100, 700);
+		int landPadLeftY = randNum(50, 200);
 		int landPadRightX = landPadLeftX + 50; //makes the pad 50 pixels wide
 		int landPadRightY = landPadLeftY;
 		
@@ -304,8 +248,8 @@ public class LunarLander extends GameClient {
 		//automatically adds lines to the right of the landing pad
 		while (pointX < 1300){
 
-			int newPointX = (50 + new Random().nextInt( 200 - 50 + 1 ));
-			int newPointY = 100 + new Random().nextInt( 400 - 100 + 1 );
+			int newPointX = randNum(50, 200);
+			int newPointY = randNum(100, 400);
 			
 			terrain.add(new ArrayList<Integer>());
 			terrain.get(arrayPosition).add(pointX);
@@ -326,8 +270,8 @@ public class LunarLander extends GameClient {
 		//adds lines to the left of the platform/landing pad
 		while (pointX2 > -100){
 
-			int newPointX2 = (50 + new Random().nextInt( 200 - 50 + 1 ));
-			int newPointY2 = 100 + new Random().nextInt( 400 - 100 + 1 );
+			int newPointX2 = randNum(50, 200);
+			int newPointY2 = randNum(100, 400);
 			
 			terrain.add(new ArrayList<Integer>());
 			terrain.get(arrayPosition2).add(pointX2 - newPointX2);
@@ -339,14 +283,17 @@ public class LunarLander extends GameClient {
 			pointY2 = newPointY2;
 			arrayPosition2++;
 			
+			
 		}
+		
+		
 		return terrain;
 	}
 	
 	//creates a random length of a line, used by createMap()
-	public int randomLength(){
-		int randomLength = 10 + new Random().nextInt( 50 - 10 + 1 );
-		return randomLength;
+	public int randNum(int start, int finish){
+		int number = start + new Random().nextInt(finish - start + 1 );
+		return number;
 	}
 	
 	@Override

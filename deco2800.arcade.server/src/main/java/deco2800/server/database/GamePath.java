15c4d722905e0787ca3f1e5534f3987cfbfe0967
Nathan Woodrow <nathanwoodrow@gmail.com>
2013-10-03 13:58:30 +1000

    Added MD5Hash field to table, modified the insert method to generate a hash for each game and insert it into the table, added method to get a hash from the table when given a game ID

--- deco2800.arcade.server/src/main/java/deco2800/server/database/GamePath.java
+++ deco2800.arcade.server/src/main/java/deco2800/server/database/GamePath.java
@@ -6,7 +6,11 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
 
+import deco2800.arcade.packman.PackageUtils;
+
 public class GamePath {
+
+	private boolean initialised = false;
 
 	public void initialise() throws DatabaseException {
 
@@ -20,28 +24,113 @@ public class GamePath {
 				Statement statement = connection.createStatement();
 				statement
 						.execute("CREATE TABLE GAMEPATH(gameID INT PRIMARY KEY,"
-								+ "path VARCHAR(200));");
+								+ "path VARCHAR(200),"
+								+ "md5Hash LONGVARCHAR(2048));");
 			}
 		} catch (SQLException e) {
 			e.printStackTrace();
 			throw new DatabaseException("Unable to create game path table", e);
 		}
+		initialised = true;
 	}
-	
+	/**
+	 * Searches for the path of the game with a particular gameID
+	 * @param gameID
+	 *          ID for the requested game
+	 * @return
+	 *          Returns the path of the requested game
+	 * @throws DatabaseException
+	 *          If SQLException occurs
+	 */
 	public String getPath(int gameID) throws DatabaseException{
+		if(!initialised){
+			initialise();
+		}
 		Connection connection = Database.getConnection();
 
 		Statement statement = null;
 		ResultSet resultSet = null;
 		try {
 			statement = connection.createStatement();
-			resultSet = statement.executeQuery("SELECT path FROM GAMEPATH WHERE gameID=" + gameID);
+			resultSet = statement.executeQuery("SELECT path FROM GAMEPATH WHERE"
+					+ " gameID=" + gameID);
 			String result = resultSet.getString(1);
 
 			return result;
 		} catch (SQLException e) {
 			e.printStackTrace();
-			throw new DatabaseException("Unable to get game path from database", e);
+			throw new DatabaseException("Unable to get game path from database",
+					e);
+		} finally {
+			try {
+				if (resultSet != null){
+					resultSet.close();
+				}
+				if (statement != null){
+					statement.close();
+				}
+				if (connection != null){
+					connection.close();
+				}
+			} catch (SQLException e) {
+				e.printStackTrace();
+			}
+		}
+	}
+	/**
+	 * adds a game to the table with the specified ID and Path
+	 * @param gameID
+	 *          ID of the game to be added
+	 * @param gamePath
+	 *          Path of the game to be added
+	 * @throws DatabaseException
+	 *          If SQLException occurs
+	 */
+	public void insertGame(int gameID,String gamePath) throws DatabaseException{
+		Connection connection = Database.getConnection();
+
+		String gameMd5 = PackageUtils.genMD5(gamePath);
+		Statement statement = null;
+		String query = "INSERT INTO GAMEPATH VALUES ('"+gameID+"','"+
+		gamePath+"','"+gameMd5+"')";
+		try {
+			statement = connection.createStatement();
+			statement.executeUpdate(query);
+		} catch(SQLException e) {
+			throw new DatabaseException("unable to add game to database",e);
+		} finally {
+			try {
+				if (statement != null){
+					statement.close();
+				}
+				if (connection != null){
+					connection.close();
+				}
+			} catch (SQLException e){
+				e.printStackTrace();
+			}
+		}
+	}
+	
+	public String getMD5(int gameID) throws DatabaseException{
+		if(!initialised){
+			initialise();
+		}
+		Connection connection = Database.getConnection();
+
+		Statement statement = null;
+		ResultSet resultSet = null;
+		try {
+			statement = connection.createStatement();
+			resultSet = statement.executeQuery("SELECT md5Hash FROM GAMEPATH WHERE"
+					+ " gameID=" + gameID);
+			String result = resultSet.getString(1);
+
+			return result;
+		} catch (SQLException e) {
+			e.printStackTrace();
+			throw new DatabaseException("Unable to get MD5 from database",
+					e);
 		} finally {
 			try {
 				if (resultSet != null){

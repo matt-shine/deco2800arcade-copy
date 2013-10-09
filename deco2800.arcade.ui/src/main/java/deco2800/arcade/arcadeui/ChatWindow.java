6c2278ebe497b327463814f73f4e56a150bf25ea
Michael <michael-henderson@live.com.au>
2013-10-09 21:40:13 +1000

    overlay work

---
+++ deco2800.arcade.ui/src/main/java/deco2800/arcade/arcadeui/ChatWindow.java
@@ -0,0 +1,47 @@
+package deco2800.arcade.arcadeui;
+
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.scenes.scene2d.ui.Skin;
+import com.badlogic.gdx.scenes.scene2d.ui.TextField;
+
+public class ChatWindow extends OverlayWindowContent {
+
+    Overlay overlay = null;
+    Skin skin = null;
+    boolean initialised = false;
+
+    
+    public ChatWindow(Overlay overlay, Skin skin) {
+        this.overlay = overlay;
+        this.skin = skin;
+    }
+
+
+    @Override
+    public void act(float d) {
+        if (!initialised) {
+            initialised = true;
+            generateInterface();
+        }
+    }
+
+    @Override
+    public void resize(int w, int h) {
+    	generateInterface();
+    }
+    
+    private void generateInterface() {
+    	//skin.add("TextFieldStyle", new Texture("textField.png"));
+    	
+        final TextField inputField = new TextField("Input", skin); 
+        final TextField outputField = new TextField("Output", skin);
+        
+        inputField.setSize(this.getWidth() - 220, 50);
+        inputField.setPosition(110, 75);
+        this.addActor(inputField);
+        
+        outputField.setSize(this.getWidth() - 220, 250);
+        outputField.setPosition(110, 165);
+        this.addActor(outputField);
+    }
+}

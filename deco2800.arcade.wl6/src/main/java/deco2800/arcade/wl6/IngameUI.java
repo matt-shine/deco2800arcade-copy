952ea0326971d601d413bc2f7102ab546447967d
simon-paris <simon.paris.93@gmail.com>
2013-10-09 21:01:50 +1000

    health, ammo, points, and guns

--- deco2800.arcade.wl6/src/main/java/deco2800/arcade/wl6/IngameUI.java
+++ deco2800.arcade.wl6/src/main/java/deco2800/arcade/wl6/IngameUI.java
@@ -1,13 +1,18 @@
 package deco2800.arcade.wl6;
 
 import com.badlogic.gdx.graphics.Color;
+import com.badlogic.gdx.graphics.g2d.BitmapFont;
 import com.badlogic.gdx.graphics.glutils.ShapeRenderer;
-import com.badlogic.gdx.scenes.scene2d.Group;
+import com.badlogic.gdx.scenes.scene2d.Stage;
+import com.badlogic.gdx.scenes.scene2d.ui.Label;
+import com.badlogic.gdx.scenes.scene2d.ui.Skin;
 
-public class IngameUI extends Group {
+public class IngameUI extends Stage {
 
     private static ShapeRenderer r = new ShapeRenderer();
-
+    private Label text = null;
+    
+    
     public static void drawCeiling(int color, float w, float h) {
 
         r.begin(ShapeRenderer.ShapeType.FilledRectangle);
@@ -16,8 +21,28 @@ public class IngameUI extends Group {
         r.setColor(c);
         r.filledRect(0, h / 2, w, h / 2);
         r.end();
-
-
+        
+    }
+    
+    
+    public IngameUI() {
+    	Skin s = new Skin();
+    	s.add("default", new Label.LabelStyle(new BitmapFont(), Color.WHITE));
+        text = new Label("test", s, "default");
+        this.addActor(text);
+    }
+    
+    
+    public void draw(GameModel game) {
+    	super.draw();
+    	
+    	int health = game.getPlayer().getHealth();
+    	int ammo = game.getPlayer().getAmmo();
+    	int gun = game.getPlayer().getCurrentGun();
+    	int points = game.getPlayer().getPoints();
+    	
+    	text.setText("Health: " + health + " Ammo: " + ammo + " Gun: " + gun + " Points: " + points);
+    	
     }
 
 

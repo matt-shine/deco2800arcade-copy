d3eac1b746ef78be263bfe93ec74d37f9d3b0c44
abbjohn <abbjohn@gmail.com>
2013-10-11 14:13:43 +1000

    Comment out a line that was causing a null value error. Proper fix incoming at a later date.

--- deco2800.arcade.ui/src/main/java/deco2800/arcade/arcadeui/Overlay/Topbar.java
+++ deco2800.arcade.ui/src/main/java/deco2800/arcade/arcadeui/Overlay/Topbar.java
@@ -39,7 +39,8 @@ public class Topbar extends Group {
         int height = overlay.getHeight();
         texture.draw(batch, width - 675, height - 125, 700, 150);
         username.setPosition(width - 620, height - 70);
-        username.setText(overlay.getPlayer().getUsername());
+        // Temporary comment out until I fix this.  Prevents a null value error.
+        //username.setText(overlay.getPlayer().getUsername());
         clock.setPosition(width - 170, height - 70);
         clock.setText(simpleDateFormat.format(new Date()));
         this.addActor(username);

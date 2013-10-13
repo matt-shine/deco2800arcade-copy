bddd8e067fafb6002b063c1c8b83003790978df9
JosieWang <josie-wq@hotmail.com>
2013-10-13 18:10:00 +1000

    Implement LadderSnakeRule

--- deco2800.arcade.snakeLadder/src/main/java/deco2800/arcade/snakeLadderGameState/MovingState.java
+++ deco2800.arcade.snakeLadder/src/main/java/deco2800/arcade/snakeLadderGameState/MovingState.java
@@ -11,8 +11,8 @@ public class MovingState extends GameState {
 	public void handleInput(SnakeLadder context) {
 		int turn=context.getturns();
 		int playerIndex = turn%context.gamePlayers.length;
+		//int playerIndex=turn;
 		movePlayer(context,context.gamePlayers[playerIndex]);	    	
-    	//If the ball gets to the left edge then player 2 wins
     	if (context.gamePlayers[playerIndex].getBounds().x <= (60-20f) && context.gamePlayers[0].getBounds().y >= (540)) {
     		context.gamePlayers[playerIndex].reset();
     		context.statusMessage = context.gamePlayers[playerIndex].getPlayerName() +" Win! ";
@@ -25,21 +25,25 @@ public class MovingState extends GameState {
 	 */
 	private void movePlayer(SnakeLadder context, GamePlayer gamePlayer) {
 		gamePlayer.move(Gdx.graphics.getDeltaTime());
+		if(context.getMap().getTileList()[gamePlayer.newposition()].getIndex()>=100)
+		{
+			context.gameState=new GameOverState();
+		}
 		if(Math.abs(context.getMap().getTileList()[gamePlayer.newposition()].getCoorX() - gamePlayer.getBounds().x) <(1f)&&Math.abs(context.getMap().getTileList()[gamePlayer.newposition()].getCoorY() - gamePlayer.getBounds().y) <(1f))
 		{
 		    	//context.stopPoint();
 //		    	context.updateScore(gamePlayer);
 				gamePlayer.reset();
-				context.gamePlayers[1].reset();
+				//context.gamePlayers[1].reset();
 				// If we've reached the victory point then update the display
 				if (gamePlayer.getBounds().x <= (60-20f) && gamePlayer.getBounds().y >= (540)) {			   
 					context.gameState = new GameOverState();
 				    //Update the game state to the server
 				    //networkClient.sendNetworkObject(createScoreUpdate());
 				} 
-				if (context.gamePlayers[1].getBounds().x<=(60-20f)&&context.gamePlayers[1].getBounds().y>=540){
-					context.gameState = new GameOverState();
-				}
+//				if (context.gamePlayers[1].getBounds().x<=(60-20f)&&context.gamePlayers[1].getBounds().y>=540){
+//					context.gameState = new GameOverState();
+//				}
 				else {
 					// No winner yet, get ready for another point
 					context.gameState = new RuleExcutingState();
@@ -50,8 +54,10 @@ public class MovingState extends GameState {
 		}
 
 		//If the player reaches the end of each line , move up to another line
-		if (gamePlayer.getBounds().x >= (600-20f) || gamePlayer.getBounds().x <=0){
+		if (gamePlayer.getBounds().x >= (600-20f) || gamePlayer.getBounds().x <=0)
+		{
 			gamePlayer.moveUp();
 		}
+		
 	}
 }

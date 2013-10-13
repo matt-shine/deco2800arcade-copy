3b70103ed19a9e7cbe8a1898a167050e5a84f094
m-koopman <m_koopman@me.com>
2013-10-13 16:52:48 +1000

    Replay webserver - added item count for each log

--- deco2800.arcade.server/src/main/java/deco2800/server/webserver/ArcadeWebserver.java
+++ deco2800.arcade.server/src/main/java/deco2800/server/webserver/ArcadeWebserver.java
@@ -24,6 +24,9 @@ import org.simpleframework.transport.Server;
 import org.simpleframework.transport.connect.Connection;
 import org.simpleframework.transport.connect.SocketConnection;
 
+import deco2800.arcade.protocol.replay.types.Session;
+import deco2800.server.ArcadeServer;
+
 public class ArcadeWebserver implements Container {
 	
 	static String readFile(String path, Charset encoding) 
@@ -42,53 +45,79 @@ public class ArcadeWebserver implements Container {
 		return new String(bytes, encoding);
 			}
 
+	void respondToReplayRequest( Response response ) throws Exception {
+		PrintStream body = response.getPrintStream();
+		long time = System.currentTimeMillis();
 
+		response.setValue("Content-Type", "text/html");
+		response.setValue("Server", "HelloWorld/1.0 (Simple 4.0)");
+		response.setDate("Date", time);
+		response.setDate("Last-Modified", time);
+		
+		String bodyString = "";
+
+		ArrayList<String> replayStrings = new ArrayList<String>();
+		replayStrings = ArcadeServer.instance().getReplayStorage().getSessionsForGame( "connect4" );
+
+		replayStrings.add( "100001, false, replayers, 1381645619190, " );
+		replayStrings.add( "100001, false, replayers, 1381645632133, " );
+		
+		ArrayList<Session> replays = new ArrayList<Session>();
+		//Convert back to sessions
+        if (replayStrings != null)
+        {
+            for (String s : replayStrings)
+            {
+                String[] tokens = s.split(",");
+
+                int sessionId = Integer.parseInt(tokens[0].replaceAll("\\s+",""));
+                boolean recording = Boolean.parseBoolean(tokens[1]);
+                String user = tokens[2];
+                long dateTime = Long.parseLong(tokens[3].replaceAll("\\s+",""));
+                String comments = tokens[4];
+                
+                Session session = new Session();
+                session.sessionId = sessionId;
+                session.time = dateTime;
+                session.username = user;
+                session.recording = recording;
+                session.comments = comments;
+                
+                replays.add( session );
+            }
+        }
+		
+		System.out.println( replays );
+		
+		bodyString = readFile( "webserver/html/template.html", Charset.forName("UTF-8" ) );
+		
+		String contentString = readFile( "webserver/html/replays.html", Charset.forName("UTF-8" ) );
+		
+		String tableString = "";
+		for ( Session replay : replays ) {
+			Date replayDate = new Date( replay.time );
+			String replayDateString = DateFormat.getDateTimeInstance().format( replayDate );
+			
+			Integer itemCount = 0;
+			if ( !replay.sessionId.equals( 100001 ) ) {
+				 itemCount = ( ArcadeServer.instance().getReplayStorage().getReplay( replay.sessionId ) ).size();
+			}
+			tableString += String.format( "<tr><td>%s</td><td>%d</td><td>%s</td></tr>", replay.username, itemCount, replayDateString );
+		}
+		
+		contentString = contentString.replace( "#{{tablebody}}", tableString );
+		
+		bodyString = bodyString.replace( "#{{content}}", contentString );
+	
+		body.println( bodyString );
+		body.close();
+	}
+	
 	public void handle(Request request, Response response) {
 		try {
 			
 			if ( request.getPath().toString().equals( "/") ) {
-				PrintStream body = response.getPrintStream();
-				long time = System.currentTimeMillis();
-	
-				response.setValue("Content-Type", "text/html");
-				response.setValue("Server", "HelloWorld/1.0 (Simple 4.0)");
-				response.setDate("Date", time);
-				response.setDate("Last-Modified", time);
-				
-				String bodyString = "";
-
-				ArrayList<String> replays = new ArrayList<String>();
-				//test = ArcadeServer.instance().getReplayStorage().getSessionsForGame( "connect4" );
-				replays.add( "1" );
-				replays.add( "false" );
-				replays.add( "replayers" );
-				replays.add( "1381639231100" );
-				replays.add( "" );
-				replays.add( "2" );
-				replays.add( "false" );
-				replays.add( "replayers" );
-				replays.add( "1381639268788" );
-				replays.add( "" );
-						
-				System.out.println( replays );
-				
-				bodyString = readFile( "webserver/html/template.html", Charset.forName("UTF-8" ) );
-				
-				String contentString = readFile( "webserver/html/replays.html", Charset.forName("UTF-8" ) );
-				
-				String tableString = "";
-				for ( int i = 0; i < replays.size(); i += 5 ) {
-					Date replayDate = new Date( Long.parseLong( replays.get( i + 3 ) ) );
-					String replayDateString = DateFormat.getDateTimeInstance().format( replayDate );
-					tableString += String.format( "<tr><td>%s</td><td>%s</td></tr>", replays.get( i + 2 ), replayDateString );
-				}
-				
-				contentString = contentString.replace( "#{{tablebody}}", tableString );
-				
-				bodyString = bodyString.replace( "#{{content}}", contentString );
-			
-				body.println( bodyString );
-				body.close();
+				respondToReplayRequest( response );
 			} else if ( request.getPath().toString().contains( "js" ) ){
 				
 				PrintStream body = response.getPrintStream();

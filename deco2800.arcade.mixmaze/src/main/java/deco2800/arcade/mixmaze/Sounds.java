4a03baa234c3d400729d8f11feb94c2f81faa738
Alex Hope-O'Connor <alex.hope.oconnor@live.com.au>
2013-10-13 18:51:14 +1000

    #32 - Mix Maze - Added in sound effects for build, destroy and TNT

--- deco2800.arcade.mixmaze/src/main/java/deco2800/arcade/mixmaze/Sounds.java
+++ deco2800.arcade.mixmaze/src/main/java/deco2800/arcade/mixmaze/Sounds.java
@@ -2,6 +2,7 @@ package deco2800.arcade.mixmaze;
 
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.audio.Music;
+import com.badlogic.gdx.audio.Sound;
 
 public final class Sounds {
 	private enum SoundType {
@@ -9,19 +10,33 @@ public final class Sounds {
 	}
 
 	private static Sounds INSTANCE;
+	
+	// Looping music
 	private Music menuLoop;
 	private Music actionLoop;
 	private SoundType playing;
+	
+	// Effect Sounds
+	private Sound buildSound;
+	private Sound destroySound;
+	private Sound tntSound;
 
 	private Sounds() {
+		// Menu Music
 		menuLoop = Gdx.audio.newMusic(Gdx.files.internal("lls_surge.mp3"));
 		menuLoop.setLooping(true);
-		menuLoop.setVolume(0.4f);
+		menuLoop.setVolume(0.5f);
 
+		// Game music
 		actionLoop = Gdx.audio.newMusic(Gdx.files
 				.internal("lls_aegis_sprint.mp3"));
 		actionLoop.setLooping(true);
-		actionLoop.setVolume(0.5f);
+		actionLoop.setVolume(0.4f);
+		
+		// Effect Sounds
+		buildSound = Gdx.audio.newSound(Gdx.files.internal("build.wav"));
+		destroySound = Gdx.audio.newSound(Gdx.files.internal("destroy.mp3"));
+		tntSound = Gdx.audio.newSound(Gdx.files.internal("tnt.mp3"));
 	}
 
 	private static Sounds getInstance() {
@@ -66,5 +81,32 @@ public final class Sounds {
 
 	public static void stop() {
 		getInstance()._stop();
+	}
+	
+	private void _playBuild() {
+		long id = buildSound.play();
+		buildSound.setVolume(id, 1.0f);
+	}
+	
+	public static void playBuild() {
+		getInstance()._playBuild();
+	}
+	
+	private void _playDestroy() {
+		long id = destroySound.play();
+		destroySound.setVolume(id, 1.0f);
+	}
+	
+	public static void playDestroy() {
+		getInstance()._playDestroy();
+	}
+	
+	private void _playTNT() {
+		long id = tntSound.play();
+		tntSound.setVolume(id, 1.0f);
+	}
+	
+	public static void playTNT() {
+		getInstance()._playTNT();
 	}
 }

f8d308a8115b8cc21f7b927b8ee86fbf07d526bf
simon-paris <simon.paris.93@gmail.com>
2013-10-14 01:42:05 +1000

    fix shader compile warning

--- deco2800.arcade.wl6/src/main/resources/wl6DoodadFragShader.glsl
+++ deco2800.arcade.wl6/src/main/resources/wl6DoodadFragShader.glsl
@@ -6,6 +6,6 @@ varying vec2 v_texpos;
 void main (void)  
 {
 	vec4 tex = texture2D(texture, v_texpos + u_texoffset);
-	if (tex.a != 1) discard;
+	if (tex.a != 1.0) discard;
 	gl_FragColor = tex;
 }

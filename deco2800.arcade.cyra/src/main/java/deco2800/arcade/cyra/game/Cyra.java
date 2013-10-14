751e215d9a1c1d163f3b19764e7f60ad802a1232
muirbren <brendanjamesmuir@gmail.com>
2013-10-14 16:45:15 +1000

    Changed folder structure
    package now deco2800.arcade.cyra

---
+++ deco2800.arcade.cyra/src/main/java/deco2800/arcade/cyra/game/Cyra.java
@@ -0,0 +1,88 @@
+package deco2800.arcade.cyra.game;
+
+import deco2800.arcade.client.AchievementClient;
+import deco2800.arcade.client.GameClient;
+import deco2800.arcade.client.network.NetworkClient;
+import deco2800.arcade.model.Player;
+import deco2800.arcade.model.Game.ArcadeGame;
+import deco2800.arcade.model.Game;
+
+/** The extension of Game class that is opened by the desktop application.
+ * Will need to be altered to extend GameClient instead.
+ * GameScreen.java might need to get merged with this one, so that this will
+ * be the class in control of level progression 
+ * @author Game Over
+ *
+ */
+@ArcadeGame(id = "Cyra")
+public class Cyra extends GameClient {
+
+	private NetworkClient networkClient;
+	private AchievementClient achievementClient;
+	
+      
+    public Cyra(Player player, NetworkClient networkClient) {
+		super(player, networkClient);
+		this.networkClient = networkClient; //this is a bit of a hack
+        this.achievementClient = new AchievementClient(networkClient);
+        
+	}
+
+	public SplashScreen getSplashScreen() {
+    	return new SplashScreen(this);
+    }
+	
+	@Override
+	public void create() {		
+	
+		super.create();
+		//setScreen(new MainMenu(this));
+		//Set to splash screen
+		setScreen(getSplashScreen());
+		//OR go straight to the action
+		//setScreen(new GameScreen(this));
+		
+	}
+
+	@Override
+	public void dispose() {
+		super.dispose();
+		//batch.dispose();
+		//texture.dispose();
+	}
+
+	@Override
+	public void render() {		
+		
+		super.render();
+		
+	}
+
+	@Override
+	public void resize(int width, int height) {
+		super.resize(width, height);
+	}
+
+	@Override
+	public void pause() {
+		super.pause();
+	}
+
+	@Override
+	public void resume() {
+		super.resume();
+	}
+	
+	private static final Game game;
+	static {
+		game = new Game();
+		game.id = "cyra";
+		game.name = "Cyra";
+		game.description = "Run around and kill stuff with sword, yay!";
+	}
+	
+	@Override
+	public deco2800.arcade.model.Game getGame() {
+		return game;
+	}
+}
---
+++ deco2800.arcade.cyra/src/main/java/deco2800/arcade/cyra/model/WallBoss.java
@@ -0,0 +1,65 @@
+package deco2800.arcade.cyra.model;
+
+import com.badlogic.gdx.graphics.OrthographicCamera;
+import com.badlogic.gdx.math.Vector2;
+import com.badlogic.gdx.utils.Array;
+
+public class WallBoss extends Enemy{
+
+	public static final float WIDTH = 4f;
+	public static final float HEIGHT = 14f;
+	public static final int INITIAL_HEALTH = 15;
+	public static final float INVINSIBLE_TIME = 0.3f;
+	
+	private int health;
+	private float count;
+	
+	public WallBoss(Vector2 pos) {
+		super(0, 0, pos, WIDTH, HEIGHT);
+		health = INITIAL_HEALTH;
+		count = 0;
+		healthName = "Wall Of Doom";
+	}
+
+	@Override
+	public Array<Enemy> advance(float delta, Player ship, float rank,
+			OrthographicCamera cam) {
+		count += delta;
+		Array<Enemy> newEnemies = new Array<Enemy>();
+		if (count > 3f-2.9*rank) {
+			newEnemies.add(new SoldierEnemy(new Vector2(cam.position.x-cam.viewportWidth-1.5f, cam.position.y), false));
+		}
+		return null;
+	}
+
+	@Override
+	public boolean isSolid() {
+		// TODO Auto-generated method stub
+		return true;
+	}
+
+	@Override
+	public void handleNoTileUnderneath() {
+		// TODO Auto-generated method stub
+		
+	}
+	
+	@Override
+	public boolean displayHealth() {
+		return true;
+	}
+	
+	@Override
+	public float getHealthPercentage() {
+		return ((float)health)/INITIAL_HEALTH;
+	}
+	
+	@Override
+	public void handleDamage(boolean fromRight) {
+		health--;
+		if (health==0) {
+			isDead = true;
+		}
+	}
+
+}

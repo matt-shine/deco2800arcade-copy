9519a2370c5626c99f882e142d20a21699f31c7b
Tony Lee <github@roganartu.com>
2013-10-14 15:01:19 +1000

    Added threading to FileServer

--- deco2800.arcade.server/src/main/java/deco2800/server/listener/FileServerListener.java
+++ deco2800.arcade.server/src/main/java/deco2800/server/listener/FileServerListener.java
@@ -2,22 +2,10 @@ package deco2800.server.listener;
 
 import com.esotericsoftware.kryonet.Connection;
 import com.esotericsoftware.kryonet.Listener;
-
+import deco2800.arcade.server.FileServer;
 import deco2800.arcade.protocol.packman.FetchGameRequest;
-import deco2800.arcade.protocol.packman.FetchGameResponse;
-import deco2800.arcade.protocol.BlockingMessage;
-import deco2800.server.ArcadeServer;
-import deco2800.arcade.packman.PackageServer;
-
-import java.lang.System;
-
-import java.io.FileInputStream;
-import java.io.DataInputStream;
-import java.io.IOException;
 
 public class FileServerListener extends Listener {
-
-    private static final String BASE_PATH = "Release";
 
     /**
      * Initializer
@@ -29,42 +17,9 @@ public class FileServerListener extends Listener {
 		super.received(connection, object);
 
         if (object instanceof FetchGameRequest) {
-            System.out.println("[Server]:  FetchGameRequest recieved");
-
-            PackageServer packServ = ArcadeServer.instance().packServ();
-            FetchGameRequest req = (FetchGameRequest) object;
-            FetchGameResponse resp = new FetchGameResponse();
-
-            // Build path
-            String path = req.gameID + "-" + req.version + ".tar.gz";
-            path = BASE_PATH + "/" + path;
-
-            resp.gameID = req.gameID;
-            resp.version = req.version;
-            resp.byteOffset = req.byteOffset;
-            resp.blockSize = req.blockSize;
-
-            // Read the file data
-            try {
-                System.out.println(path);
-                FileInputStream file = new FileInputStream(path);
-                DataInputStream d = new DataInputStream(file);
-
-                resp.totalBytes = d.available();
-
-                file.skip(req.byteOffset);
-                resp.data = new byte[req.blockSize];
-                resp.byteCount = file.read(resp.data);
-
-                file.close();
-                resp.status = 1;
-            } catch (IOException e) {
-                // TODO actually work out what the error really was
-                e.printStackTrace();
-                resp.status = -3;
-            }
-
-            BlockingMessage.respond(connection, req, resp);
+            FileServer fs = new FileServer((FetchGameRequest) object, connection);
+            Thread t = new Thread(fs);
+            t.start();
         }
 	}
 

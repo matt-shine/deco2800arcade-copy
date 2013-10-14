ef698c7e5287302f9ccc38abb9e59030ffcf5bb6
unknown <s4291529@AE316A-3067.labs.eait.uq.edu.au>
2013-10-14 13:47:37 +1000

    Test code cleaned

--- deco2800.arcade.server/src/test/java/deco2800/arcade/server/database/TestHighscoreDatabase.java
+++ deco2800.arcade.server/src/test/java/deco2800/arcade/server/database/TestHighscoreDatabase.java
@@ -27,12 +27,11 @@ import deco2800.server.database.HighscoreDatabase;
 
 /**
  * Test class for HighscoreDatabase
- * @author Team A (credit uqjstee8)
+ * @author Team A
  * @see deco2800.arcade.server.database.HighscoreDatabase
  */
 public class TestHighscoreDatabase {
 
-	private static IDatabaseTester databaseTester; //manage connections to the database
 	private HighscoreDatabase highscoreDatabase; //storage object to test
 
 	/**
@@ -41,46 +40,25 @@ public class TestHighscoreDatabase {
 	 */
 	@BeforeClass
 	public static void setUpClass() throws Exception {
-		databaseTester = new JdbcDatabaseTester(
-                "org.apache.derby.jdbc.EmbeddedDriver",
-                "jdbc:derby:Arcade;user=server;password=server;create=true");
+		
 	}
 
 	/**
-	 * Retrieve the dataset from an XML file
-	 * @return
-	 * @throws DataSetException
-	 * @throws IOException
-	 */
-	private IDataSet getDataSet() throws DataSetException, IOException {
-		URL url = TestHighscoreDatabase.class.getClassLoader().getResource("TestHighscoreDatabase.xml");
-		FlatXmlDataSetBuilder builder = new FlatXmlDataSetBuilder();
-		builder.setColumnSensing(true);
-		return builder.build(url);
-	}
-	
-	/**
-	 * Create a new highscore and initialise it,
-	 * load in a dataset from XML, and get the database ready (clean old data and put the new stuff in)
-	 * This method is run once before each test case.
+	 * Create a new highscore class object
 	 * @throws Exception
 	 */
 	@Before
 	public void setUp() throws Exception {
-		highscoreDatabase = new HighscoreDatabase();
-		highscoreDatabase.initialise();
-		IDataSet ds = getDataSet();
-        databaseTester.setDataSet(ds);
-		databaseTester.onSetup();
+		highscoreDatabase = new HighscoreDatabase();		
 	}
 	
 	/**
-	 * Allow DBUnit to clean up after the test case (restore the database to its pre-testing state)
+	 * 
 	 * @throws Exception
 	 */
 	@After
 	public void  tearDown() throws Exception {
-		databaseTester.onTearDown();
+		
 	}
 	
 	

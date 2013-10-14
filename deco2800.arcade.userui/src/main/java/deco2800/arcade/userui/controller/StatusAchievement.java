f52eabbfc3c4397c520b9ee9b5490dd6cf85eb48
pcetx <moulthunl@gmail.com>
2013-10-15 00:44:25 +1000

    Userui restructure folder

---
+++ deco2800.arcade.userui/src/main/java/deco2800/arcade/userui/controller/StatusAchievement.java
@@ -0,0 +1,61 @@
+package deco2800.arcade.userui.controller;
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import deco2800.arcade.userui.Model;
+import deco2800.arcade.userui.view.AchievementScreen;
+import deco2800.arcade.userui.view.StatusScreen;
+import deco2800.arcade.userui.view.UserScreen;
+
+public class StatusAchievement {
+	
+	private AchievementScreen achievementView;
+	private StatusScreen statusView;
+	private Model theModel;
+
+	/**
+	 * 
+	 * @param theModel
+	 * @param statusView
+	 * @param achievementView
+	 */
+	public StatusAchievement(Model theModel, StatusScreen statusView, AchievementScreen achievementView){
+		
+		this.theModel = theModel;
+		this.statusView = statusView;
+		this.achievementView = achievementView;
+		
+		this.statusView.addSaveListener(new SaveListener());
+		this.statusView.addCancelListener(new CancelListener());
+		
+	}
+
+	class SaveListener implements ActionListener{
+
+		@Override
+		public void actionPerformed(ActionEvent arg0) {
+			
+			statusView.dispose();
+			statusView.getStatusSelection();
+			achievementView.setStatus(theModel.statusIcon);
+			achievementView.setEnabled(true);
+			
+		}
+		
+	}
+	
+	public class CancelListener implements ActionListener{
+
+		@Override
+		public void actionPerformed(ActionEvent arg0) {
+			
+			statusView.dispose();
+			achievementView.setEnabled(true);
+			
+		}
+		
+	}
+	
+
+}

c5e625078cdd60e60e0b92a74c8713402c75b1e2
mitchell931993 <mitchell931993@gmail.com>
2013-10-17 16:52:28 +1000

    Filled out most of the functions in Accolade System. They no longer use seperately declared watchers and pause and unpause has been depriciated

--- deco2800.arcade.statistics/src/main/java/deco2800/arcade/accolades/AccoladeSystem.java
+++ deco2800.arcade.statistics/src/main/java/deco2800/arcade/accolades/AccoladeSystem.java
@@ -1,30 +1,41 @@
 package deco2800.arcade.accolades;
 import java.sql.ResultSet;
 import java.sql.SQLException;
-import java.util.HashMap;
+import java.util.*;
+
 //import deco.arcade.accolades.servercommunicator //this will be what ever jerry calls it
 
+/** CHANGES
+ * @author Mitch
+ * Accolades now can longer be referred to by string name. They must be declared in the xml
+ * The timer is now part of the accolade system, and multiple timer events are scheduled
+ */
+
 public class AccoladeSystem {
-	//TODO organise the methods so that all the string operations are at the bottom
 	//ServerCommunicator server = new ServerCommunicator();
-	ResultSet serverData; 
-	/**this will be the accolade info read from the xml 
-	 * (uses a writetoxml function that is handy dandy
-	 */
+		
 	
-	HashMap<Integer,int[]> localAccolades;
-	HashMap<String,Integer> nameIDPairs;
-	int playerID;
-	int gameID;
+	private HashMap<String,Integer> nameIDPairs;
+	private HashMap<Integer,int[]> localAccolades; //progress for popupevent
+	private HashMap<Integer,WatchedAccolade> watchedVariables; //Prepared variables ready for scheduling
+	private HashMap<Integer, TimedPush> timerTasks; //timertasks recreated when the timer begins
+	private Timer timer;
+	private ResultSet serverData; 
+	private int playerID;
+	private int gameID;
+	private boolean timerRunning = false; 
 	
-	final int PROGRESS = 0;
-	final int POPUP=1; //when the value is a multiple of the 
+	//when progress%popup = 0 a popup is overlayed on screen
+	private final int PROGRESS = 0;
+	private final int POPUP=1;
+	
 	
 	
 	/**The constructor used when automatically fetching the Game and Player ID
 	 */
 	public AccoladeSystem(){
 		//start the local variables for the overlay system
+		//TODO Automatically pull the game id and the player id
 	}
 	
 	//constructor used
@@ -37,6 +48,7 @@ public class AccoladeSystem {
 		this.playerID = playerID;
 		this.gameID = gameID;
 		//this.serverData = server.getTable(this.playerID + "," + this.gameID);
+		
 		//This is the local copy of the accolade progress, it's to save 
 		//bandwidth for checking when to do the accolade popup message
 		try {
@@ -57,15 +69,6 @@ public class AccoladeSystem {
 					+ "primary key pairs for the accolades: " + error.toString());
 			error.printStackTrace();
 		}		
-	}
-	
-	/** Increments an accolade by {@param increment} amount. Uses a string for
-	 * the accolade.
-	 * @param accolade The accolade to be modified in string form.
-	 * @param increment The total you would like to increment the accolade by.
-	 */
-	public void push(String accolade, int increment){
-		this.push(fetchID(accolade), increment);
 	}
 	
 	/** Increments an accolade by {@param increment} amount. Uses an integer for
@@ -93,7 +96,6 @@ public class AccoladeSystem {
 		 */
 	}
 	
-	//returns the Primary key for the string the accolade is associated with
 	/** Provides the int version of an accolade primary key to be used in the
 	 * server communication (names may be updated, but the int's will not).
 	 * @param accolade The string name for the accolade (matches database).
@@ -105,52 +107,6 @@ public class AccoladeSystem {
 		} else {
 			throw new NullPointerException("No accolade by that name exists. Check your XML");
 		}
-	}
-	
-	/** Constructs a watcher to be started at the beginning of
-	 * 	each round. Uses the string name of the accolade.
-	 * @param accolade The name of the accolade to be watched.
-	 * @param variable The change in this Integer will be increment the accolade.
-	 * @param time How often push should occur in seconds (defaults to 30 if 
-	 * not provided).
-	 * @return A new watcher to be used during gameplay.
-	 */
-	public Watcher watch(String accolade, Object variable, int time){
-		return new Watcher(fetchID(accolade), variable, time);
-	}
-	
-	/** Constructs a watcher with a default time of 30 seconds to be started
-	 *  at the beginning of each round. Uses the string name of the accolade.
-	 * @param accolade The name of the accolade to be watched.
-	 * @param variable The change in this Integer will be increment the accolade.
-	 * @return A new watcher to be used during gameplay.
-	 */
-	public Watcher watch(String accolade, Object variable){
-		return new Watcher(fetchID(accolade), variable, 30);
-	}
-	
-	/** Constructs a watcher with a default time of 30 seconds to be started
-	 *  at the beginning of each round. Uses the string name of the accolade.
-	 * @param accolade The primaryID of the accolade to be watched.
-	 * @param variable The change in this Integer will be increment the accolade.
-	 * @param time How often push should occur in seconds (defaults to 30 if 
-	 * not provided).
-	 * @return A new watcher to be used during gameplay.
-	 */
-	public Watcher watch(int accoladeID, Object variable, int time){
-		return new Watcher(accoladeID, variable, time);
-	}
-	
-	/** Constructs a watcher that periodically increments and accolade based
-	 * 	the difference between the initial value of the variable and every
-	 * 	push after that. This constructor defaults the push to every 30 
-	 *  seconds.
-	 * @param accolade The primaryID of the accolade to be watched.
-	 * @param variable The change in this Integer will be increment the accolade.
-	 * @return A new watcher to be used during gameplay.
-	 */
-	public Watcher watch(int accoladeID, Object variable){
-		return new Watcher(accoladeID, variable, 30);
 	}
 	
 	/** 
@@ -186,62 +142,106 @@ public class AccoladeSystem {
 	}
 	
 	
-	/**
-	 * @author Mitch
-	 * This is the class used to periodically push accolade increments.
+	/**TIMER STUFF **/
+	
+	/** Add a watcher to the timer schedule to push accolade progress to the server
+	 * @param accoladeID The primary key of the accolade to be updated
+	 * @param variable The Integer that is being watched
+	 * @param pushInterval How often in milliseconds the game performs the push
 	 */
-	public class Watcher{
-		private int cycleTime;
-		private int accoladeID;
-		private int prevValue;
-		private Object watchedVar;
-		
-		/** Constructs a watcher that periodically increments and accolade based
-		 * 	the difference between the initial value of the variable and every
-		 * 	push after that.
-		 * @param accolade The key of the accolade you're incrementing.
-		 * @param variable The variable that the accolade relates to (eg:score).
-		 * @param time How often in seconds the push is to occur.
-		 */
-		public Watcher(int accolade, Object variable, int time){
-			//TODO Create the timer!
-			this.accoladeID = accolade;
-			this.cycleTime = time;
-			this.watchedVar = variable;
-			//i have no clue what i'm doing here but i hope it works!
-			this.prevValue = int.class.cast(variable);
+	public void watch(int accoladeID, Object variable, int pushInterval){
+		//Creates a new scheduled push or updates the old one.
+		watchedVariables.put(accoladeID,
+					new WatchedAccolade(accoladeID, variable, pushInterval));
+	}
+	
+	/** Removes the accolade from the update schedule. Assumes that accolade exists
+	 * @param accoladeID The accolade that you do not want automatically updated anymore
+	 * @throws NullPointerException If an accolade with that accolade ID is not being watched.
+	 */
+	public void stopWatching(int accoladeID)throws NullPointerException{
+		if(!this.watchedVariables.containsKey(accoladeID)){
+			throw new NullPointerException("There is no accolade being tracked with that ID");			
 		}
-		
-
-		
-		/** Starts the timer and begins the push cycle. Use this at the start 
-		 * of your round.
-		 */
-		public void start(){
-			//TODO Finish this method
-		}
-		
-		/** Stops the timer and pushes the leftover amount of your integer.
-		 *  Use this when you finish a round.
-		 */
-		public void stop(){
-			//TODO Finish this method
-		}
-		
-		/** Pauses the timer allowing for resumption later.
-		 */
-		public void pause(){
-			//TODO Finish this method
-		}
-		
-		/** Resumes from {@link pause()} restarting the timer.
-		 */
-		public void unpause(){
-			//TODO Finish this method
+		if(this.timerRunning){
+			WatchedAccolade accolade = this.watchedVariables.get(accoladeID);
+			//Stops the timerTask running
+			timerTasks.get(accoladeID).cancel();
+			//then removes it so it isn't initialised again
+			timerTasks.remove(accoladeID);
 		}
 		
 		
 	}
+	
+	/**
+	 * Starts the timer for all the watched variables.
+	 */
+	public void start(){
+		this.timer = new Timer();
+		this.timerRunning = true;
+		
+		//Schedule all the watched variables as timertasks
+		for(Map.Entry entry: this.watchedVariables.entrySet()){
+			//make it a little easier to handle
+			WatchedAccolade accolade = (WatchedAccolade) entry.getValue(); 
+			TimedPush timedPush =  new TimedPush(accolade.accoladeID, accolade.variable);
+			timerTasks.put(accolade.accoladeID, timedPush);
+			timer.schedule(timedPush, accolade.interval);
+		}	
+	}
+	
+	/**
+	 * Stop the timer and remove all watched values (should set watched variables at the start of each round
+	 */
+	public void stop(){
+		this.timerRunning = false;
+		///discards all the events
+		this.timer.cancel();
+		this.timer.purge();
+		timerTasks.clear();
+	}
+	
+	/**NO LONGER NEED PAUSE AND UNPAUSE AS ONLY CHANGES IN THE VARIABLE ARE 
+	 * 						PUSHED TO THE SERVER **/
+	
+	/** END OF TIMER STUFF **/
+	
+	private class WatchedAccolade{		
+		public Object variable;
+		public int accoladeID;
+		public int interval;
+
+		public WatchedAccolade(int accoladeID, Object variable, int interval){
+			this.variable = variable;
+			this.accoladeID = accoladeID;
+			this.interval = interval;
+		}
+	} //end of watched accolade
+	
+	private class TimedPush extends TimerTask{
+		Object variable;
+		int accoladeID;
+		int prevValue;
+		
+		public TimedPush(int accoladeID, Object variable){
+			this.accoladeID = accoladeID;
+			this.variable = variable;
+			this.prevValue = Integer.valueOf((String) variable);
+		}
+		
+		public void run(){
+			int newValue = Integer.valueOf((String)this.variable);
+			//push different if more, or push full value if lower
+			if( newValue > this.prevValue){
+				push(this.accoladeID,newValue-this.prevValue);
+			} else {
+				push(this.accoladeID,newValue);
+			}
+		}
+	}//End of Timed Push
+	
+	
 }
 
 

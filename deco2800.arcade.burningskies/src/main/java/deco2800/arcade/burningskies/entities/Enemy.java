fb8ad3a402e61706a50d6ae16edb33c9c2d6d088
William Toohey <william.toohey@uqconnect.edu.au>
2013-10-17 23:16:16 +1000

    Committed a warning like a chump

--- deco2800.arcade.burningskies/src/main/java/deco2800/arcade/burningskies/entities/Enemy.java
+++ deco2800.arcade.burningskies/src/main/java/deco2800/arcade/burningskies/entities/Enemy.java
@@ -4,7 +4,6 @@ import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.TextureRegion;
 import com.badlogic.gdx.math.Vector2;
-import com.badlogic.gdx.scenes.scene2d.utils.Drawable;
 import com.badlogic.gdx.scenes.scene2d.utils.TextureRegionDrawable;
 
 import deco2800.arcade.burningskies.screen.PlayScreen;

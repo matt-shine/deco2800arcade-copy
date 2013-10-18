8f17e8231beffdf2a2a4c960a7c83b82744a1adb
gfmr90 <gfmr90@gmail.com>
2013-10-18 11:52:17 +1000

    JUNIT Tests Update
    Started Template

---
+++ deco2800.arcade.landInvaders/src/main/java/deco2800/arcade/landInvaders/Tests/LandInvadersGameTest.java
@@ -0,0 +1,22 @@
+package deco2800.arcade.landInvaders.Tests;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+import deco2800.arcade.landInvaders.*;
+import java.util.*;
+import java.io.*;
+
+
+
+/**
+ * Basic tests for the LandInvaders Game implementation class.
+ */
+public class LandInvadersGameTest {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}

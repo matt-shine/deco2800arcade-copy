9b6ec8a95b1496d74226259b3afa8e0812055ba1
philmok <philip.mok@uqconnect.edu.au>
2013-10-18 19:57:08 +1000

    Creating ThreadList Controller

--- deco2800.arcade.forum/src/main/java/deco2800/arcade/forum/ui/ThreadListController.java
+++ deco2800.arcade.forum/src/main/java/deco2800/arcade/forum/ui/ThreadListController.java
@@ -13,14 +13,42 @@ import deco2800.arcade.model.forum.ForumUser;
 import deco2800.arcade.protocol.Protocol;
 import deco2800.arcade.protocol.forum.*;
 
-public class ThreadListController implements ActionListener {
+public class ThreadListController {
 	private GeneralDiscussion view;
 	private ClientConnection connection;
-	
-	
-	@Override
-	public void actionPerformed(ActionEvent e) {
-		// TODO Auto-generated method stub
+	public ThreadListController (GeneralDiscussion viewModel) throws ForumException {
+		this.view = viewModel;
+		this.connection = new ClientConnection("", 0, 0);
 		
+		System.out.println("Request load");
+		GetParentThreadsRequest request = new GetParentThreadsRequest();
+		request.start = 0;
+		request.end = 0;
+		request.limit = 10;
+		System.out.println("request send");		
+		this.connection.getClient().sendTCP(request);
+		System.out.println("pThread request is sent");
+		
+		this.connection.addListener(new Listener() {
+			public void received(Connection con, Object object) {
+				if (object instanceof GetParentThreadsResponse) {
+					System.out.println("Server response is received");
+					GetParentThreadsResponse response = (GetParentThreadsResponse) object;
+					if (response.error != "") {
+						System.out.println("yeah... didnt work");
+					} else {
+						if (response.result != null) {
+							System.out.println("Thread achieved");
+						} else {
+							System.out.println("Failed to get thread");
+							JOptionPane.showMessageDialog(null, "Failed to get thread", 
+									"Error", JOptionPane.ERROR_MESSAGE);
+						}
+					}
+				}
+				return;
+			}
+		});
 	}
+
 }

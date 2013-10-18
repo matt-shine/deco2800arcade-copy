822a2a1db3013455e69b9f9f35e2cbeaf48f6348
Mithila Fox <mithila.fox@gmail.com>
2013-10-18 16:44:46 +1000

    Formatting stuff in arcade.server

--- deco2800.arcade.server/src/main/java/deco2800/server/Lobby.java
+++ deco2800.arcade.server/src/main/java/deco2800/server/Lobby.java
@@ -19,23 +19,26 @@ import deco2800.arcade.protocol.lobby.JoinLobbyMatchRequest;
 import deco2800.arcade.protocol.lobby.JoinLobbyMatchResponse;
 import deco2800.arcade.protocol.lobby.JoinLobbyMatchResponseType;
 import deco2800.arcade.protocol.multiplayerGame.NewMultiGameRequest;
+
 /**
  * The Lobby class - Singleton Pattern.
  * 
  * Holds the current state of the lobby.
  * 
  * @author matt-shine
- *
+ * 
  */
 public class Lobby {
 
 	/* The Lobby instance */
 	private static Lobby instance;
 
-
-	/* Holds the 'matches' in the lobby (gameid, <host username, host connection>)*/
+	/*
+	 * Holds the 'matches' in the lobby (gameid, <host username, host
+	 * connection>)
+	 */
 	private ArrayList<LobbyMatch> lobbyGames;
-	
+
 	/* Holds the players who are connected to the lobby */
 	private Map<Integer, Connection> connectedPlayers;
 
@@ -53,6 +56,7 @@ public class Lobby {
 
 	/**
 	 * Get the singleton Lobby instance
+	 * 
 	 * @return the Lobby
 	 */
 	public static Lobby instance() {
@@ -62,31 +66,32 @@ public class Lobby {
 		return instance;
 	}
 
-
-
-	/** 
+	/**
 	 * Create a match to be displayed by the lobby.
 	 * 
-	 * @param gameId - the game
-	 * @param user - Map <username, connection>  of the user
-	 * @return - True if match created successfully, false otherwise.
+	 * @param gameId: the game
+	 *            
+	 * @param user: Map <username, connection> of the user
+	 *            
+	 * @return True if match created successfully, false otherwise.
 	 */
 	public void createMatch(String gameId, int playerId, Connection connection) {
-		//TODO: handle user joining multiple matches?
+		// TODO: handle user joining multiple matches?
 		if (userHasMatch(playerId)) {
 			CreateMatchResponse response = new CreateMatchResponse();
-			//TODO: Failure Response or null check on other end.
+			// TODO: Failure Response or null check on other end.
 			connection.sendTCP(response);
 		} else {
-		/* Create the match and add to array of matches */
-		LobbyMatch match = new LobbyMatch(gameId, playerId, connection, this.matchIdCounter);
-		this.matchIdCounter++;
-		lobbyGames.add(match);
-		CreateMatchResponse response = new CreateMatchResponse();
-		response.matchId = match.getMatchId();
-		connection.sendTCP(response);
-		
-		this.sendGamesToLobbyUsers();
+			/* Create the match and add to array of matches */
+			LobbyMatch match = new LobbyMatch(gameId, playerId, connection,
+					this.matchIdCounter);
+			this.matchIdCounter++;
+			lobbyGames.add(match);
+			CreateMatchResponse response = new CreateMatchResponse();
+			response.matchId = match.getMatchId();
+			connection.sendTCP(response);
+
+			this.sendGamesToLobbyUsers();
 		}
 	}
 
@@ -99,14 +104,16 @@ public class Lobby {
 			connection.sendTCP(response);
 		} else {
 			/* Match found */
-			MatchmakerQueue.instance().addLobbyGame(match.getHostPlayerId(), playerId, match.getHostConnection(), connection, match.getGameId());
+			MatchmakerQueue.instance().addLobbyGame(match.getHostPlayerId(),
+					playerId, match.getHostConnection(), connection,
+					match.getGameId());
 			this.lobbyGames.remove(match);
 			JoinLobbyMatchResponse response = new JoinLobbyMatchResponse();
 			response.responseType = JoinLobbyMatchResponseType.OK;
 			connection.sendTCP(response);
 		}
 	}
-	
+
 	/**
 	 * Returns the LobbyMatch instance of the given id.
 	 * 
@@ -122,22 +129,24 @@ public class Lobby {
 		}
 		return null;
 	}
-	
+
 	/**
 	 * Add a player to the list of lobby players
+	 * 
 	 * @param playerId
 	 * @param connection
 	 */
 	public void addPlayerToLobby(int playerId, Connection connection) {
 		if (connectedPlayers.containsKey(playerId)) {
-			//Players already in the lobby list - remove to re-add
+			// Players already in the lobby list - remove to re-add
 			connectedPlayers.remove(playerId);
 		}
 		connectedPlayers.put(playerId, connection);
 	}
-	
+
 	/**
 	 * Remove a player from the list of lobby players
+	 * 
 	 * @param playerId
 	 * @param connection
 	 */
@@ -146,54 +155,56 @@ public class Lobby {
 			connectedPlayers.remove(playerId);
 		}
 	}
-	
+
 	/**
 	 * Sends the current active matches (i.e. matches with a host and a free
-	 * 	spot) to the users in the lobby.
+	 * spot) to the users in the lobby.
 	 * 
 	 * Currently unused.
 	 */
 	public void sendGamesToLobbyUsers() {
 		if (connectedPlayers.size() > 0 && lobbyGames.size() > 0) {
-				Iterator it = connectedPlayers.entrySet().iterator();
-				while (it.hasNext()) {
-					Map.Entry<Integer, Connection> pairs = (Entry<Integer, Connection>)it.next();
-					ClearListRequest clr = new ClearListRequest();
-					pairs.getValue().sendTCP(clr);
+			Iterator it = connectedPlayers.entrySet().iterator();
+			while (it.hasNext()) {
+				Map.Entry<Integer, Connection> pairs = (Entry<Integer, Connection>) it
+						.next();
+				ClearListRequest clr = new ClearListRequest();
+				pairs.getValue().sendTCP(clr);
 
-				for (int j=0;j<lobbyGames.size();j++) {
+				for (int j = 0; j < lobbyGames.size(); j++) {
 					/* Create the ActiveMatchDetails object to send */
 					ActiveMatchDetails amd = new ActiveMatchDetails();
 					amd.gameId = lobbyGames.get(j).getGameId();
 					amd.hostPlayerId = lobbyGames.get(j).getHostPlayerId();
 					amd.matchId = lobbyGames.get(j).getMatchId();
-					
+
 					/* Send it to the client */
 					pairs.getValue().sendTCP(amd);
 				}
 			}
 		}
 	}
-	
+
 	/**
-	 * Sends the current active matches (i.e. matches with a host and 
-	 * a free spot) to the specified user.
-	 * @param playerId - The id of the player to send the matches to.
+	 * Sends the current active matches (i.e. matches with a host and a free
+	 * spot) to the specified user.
+	 * 
+	 * @param playerId: The id of the player to send the matches to.
 	 */
 	public void sendGamesToLobbyUser(int playerId) {
 
 		/* Send a clear list request to the players client */
 		ClearListRequest clr = new ClearListRequest();
 		connectedPlayers.get(playerId).sendTCP(clr);
-		
+
 		/* Now send the matches through to the client */
-		for (int i=0;i<lobbyGames.size();i++) {
+		for (int i = 0; i < lobbyGames.size(); i++) {
 			/* Create the ActiveMatchDetails object to send */
 			ActiveMatchDetails amd = new ActiveMatchDetails();
 			amd.gameId = lobbyGames.get(i).getGameId();
 			amd.hostPlayerId = lobbyGames.get(i).getHostPlayerId();
 			amd.matchId = lobbyGames.get(i).getMatchId();
-			
+
 			/* Send it to the client */
 			connectedPlayers.get(playerId).sendTCP(amd);
 		}
@@ -201,22 +212,24 @@ public class Lobby {
 
 	/**
 	 * Checks if the user is hosting a match in the lobby.
-	 * @param username - the username to check
-	 * @return - true if user has a match in lobby, false otherwise.
+	 * 
+	 * @param username: the username to check
+	 * 
+	 * @return true if user has a match in lobby, false otherwise.
 	 */
 	public boolean userHasMatch(int playerId) {
-		for (int i = 0; i <lobbyGames.size(); i++) {
+		for (int i = 0; i < lobbyGames.size(); i++) {
 			if (lobbyGames.get(i).getHostPlayerId() == playerId) {
-				return true; /* User  hosting a match in lobby */
+				return true; /* User hosting a match in lobby */
 			}
 		}
 		return false;
 	}
-	
-	
+
 	/**
 	 * Removes the match from the lobby.
-	 * @param gameId - the id of the game the match is for
+	 * 
+	 * @param gameId the id of the game the match is for
 	 */
 	public void destroyMatch(int matchId) {
 		for (int i = 0; i < lobbyGames.size(); i++) {
@@ -226,23 +239,25 @@ public class Lobby {
 		}
 		this.sendGamesToLobbyUsers();
 	}
-	
+
 	/**
 	 * Returns the list of current matches in the lobby.
+	 * 
 	 * @return
 	 */
 	public ArrayList<LobbyMatch> getLobbyGames() {
 		return lobbyGames;
 	}
-	
+
 	/**
 	 * Returns the list of connected players in the lobby.
+	 * 
 	 * @return
 	 */
 	public Map<Integer, Connection> getConnectedPlayers() {
 		return connectedPlayers;
 	}
-	
+
 	public int getMatchIdCounter() {
 		return this.matchIdCounter;
 	}
--- deco2800.arcade.server/src/main/java/deco2800/server/MultiplayerServer.java
+++ deco2800.arcade.server/src/main/java/deco2800/server/MultiplayerServer.java
@@ -4,7 +4,6 @@ import com.esotericsoftware.kryonet.Connection;
 
 import deco2800.arcade.protocol.multiplayerGame.GameStateUpdateRequest;
 
-
 public class MultiplayerServer {
 
 	private int player1Id;
@@ -18,10 +17,9 @@ public class MultiplayerServer {
 	private MatchmakerQueue queue;
 	private Boolean matchmakerGame;
 
-	
-
 	/**
 	 * Creates a multiplayer server for a matchmaking game.
+	 * 
 	 * @param player1Id
 	 * @param player2Id
 	 * @param player1
@@ -30,7 +28,8 @@ public class MultiplayerServer {
 	 * @param sessionId
 	 * @param queue
 	 */
-	public MultiplayerServer(int player1Id, int player2Id, Connection player1, Connection player2, String gameId, int sessionId, 
+	public MultiplayerServer(int player1Id, int player2Id, Connection player1,
+			Connection player2, String gameId, int sessionId,
 			MatchmakerQueue queue, int p1Rating, int p2Rating) {
 		this.player1Id = player1Id;
 		this.player2Id = player2Id;
@@ -45,9 +44,10 @@ public class MultiplayerServer {
 		player1.sendTCP(this.sessionId);
 		player2.sendTCP(this.sessionId);
 	}
-	
+
 	/**
 	 * Creates a multiplayer server for a lobby game.
+	 * 
 	 * @param player1Id
 	 * @param player2Id
 	 * @param player1
@@ -56,20 +56,23 @@ public class MultiplayerServer {
 	 * @param sessionId
 	 * @param lobbyMatchId
 	 */
-	public MultiplayerServer(int player1Id, int player2Id, Connection player1, Connection player2, String gameId, int lobbyMatchId) {
+	public MultiplayerServer(int player1Id, int player2Id, Connection player1,
+			Connection player2, String gameId, int lobbyMatchId) {
 		this.player1Id = player1Id;
 		this.player2Id = player2Id;
 		this.player1 = player1;
 		this.player2 = player2;
 		this.gameId = gameId;
-		this.sessionId = lobbyMatchId;;
+		this.sessionId = lobbyMatchId;
+		;
 		this.matchmakerGame = false;
 		player1.sendTCP(this.sessionId);
 		player2.sendTCP(this.sessionId);
 	}
-	
+
 	/**
 	 * Returns the ID of player 1
+	 * 
 	 * @return The ID of player 1
 	 */
 	public int getPlayer1() {
@@ -78,6 +81,7 @@ public class MultiplayerServer {
 
 	/**
 	 * Returns the ID of player 2
+	 * 
 	 * @return The ID of player 2
 	 */
 	public int getplayer2() {
@@ -86,6 +90,7 @@ public class MultiplayerServer {
 
 	/**
 	 * Returns the ID of the game
+	 * 
 	 * @return The ID of the game
 	 */
 	public String getGameId() {
@@ -94,22 +99,25 @@ public class MultiplayerServer {
 
 	/**
 	 * Returns the session (server) number
+	 * 
 	 * @return the Session number
 	 */
 	public int getSessionId() {
 		return sessionId;
 	}
-	
+
 	/**
 	 * Returns player 1's rating
+	 * 
 	 * @return Player 1's rating
 	 */
 	public int getPlayer1Rating() {
 		return p1Rating;
 	}
-	
+
 	/**
 	 * Returns player 2's rating
+	 * 
 	 * @return Player 2's rating
 	 */
 	public int getPlayer2Rating() {
@@ -118,26 +126,29 @@ public class MultiplayerServer {
 
 	/**
 	 * Whether the server is being used for a matchmaking or lobby game
+	 * 
 	 * @return true if the server is a matchmaking server, false otherwise
 	 */
 	public Boolean getServerType() {
 		return matchmakerGame;
 	}
-	
+
 	/**
 	 * Responsible for forwarding on state update requests from game clients
-	 * @param request The request sent by the client
+	 * 
+	 * @param request: The request sent by the client
 	 */
 	public void stateUpdate(GameStateUpdateRequest request) {
 		if (request.gameOver == true && this.matchmakerGame) {
-			queue.gameOver(sessionId, player1Id, player2Id, gameId, request.winner);
+			queue.gameOver(sessionId, player1Id, player2Id, gameId,
+					request.winner);
 			return;
 		}
 		if (request.initial == false) {
-			player1.sendTCP(request);	
+			player1.sendTCP(request);
 			player2.sendTCP(request);
 		} else {
-			player2.sendTCP(request);			
+			player2.sendTCP(request);
 		}
 	}
 }

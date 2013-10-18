e946d794a43fb729c2ea56dbf89e147257b06bba
CupcakeSlayer <kieran5591@gmail.com>
2013-10-18 16:28:19 +1000

    Added New Chat Protocols

---
+++ deco2800.arcade.protocol/src/main/java/deco2800/arcade/protocol/lobby/LobbyMessageRequest.java
@@ -0,0 +1,9 @@
+package deco2800.arcade.protocol.lobby;
+
+public class LobbyMessageRequest extends LobbyRequest {
+
+	public String message;
+	public int playerID;
+	
+
+}
---
+++ deco2800.arcade.protocol/src/main/java/deco2800/arcade/protocol/lobby/LobbyMessageResponse.java
@@ -0,0 +1,8 @@
+package deco2800.arcade.protocol.lobby;
+
+public class LobbyMessageResponse {
+	
+	public String message;
+	public int playerID;
+
+}

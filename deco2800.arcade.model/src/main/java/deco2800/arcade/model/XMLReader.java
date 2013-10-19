094c8776a09150f623d7af89ac761b90a7fdf9cf
mitchell931993 <mitchell931993@gmail.com>
2013-10-19 23:47:59 +1000

    Added in some changes, not sure how it's different

---
+++ deco2800.arcade.model/src/main/java/deco2800/arcade/model/XMLReader.java
@@ -0,0 +1,55 @@
+package deco2800.arcade.model;
+
+import java.io.File;
+import java.util.List;
+
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+
+import deco2800.arcade.model.Accolade;
+import deco2800.arcade.model.AccoladeContainer;
+ 
+
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+import org.w3c.dom.Node;
+import org.w3c.dom.NodeList;
+
+public class XMLReader {
+	String fileLocation;
+	
+	public AccoladeContainer getAccolades(String fileLocation){
+		Accolade temp;
+		//The container to be stored to
+		AccoladeContainer accoladeContainer = new AccoladeContainer();
+		try {
+			DocumentBuilderFactory dbf = DocumentBuilderFactory.newInstance();
+            DocumentBuilder db = dbf.newDocumentBuilder();
+            File file = new File(fileLocation);
+            if (file.exists()) {
+                Document doc = db.parse(file);
+                Element xmlDoc = doc.getDocumentElement();
+                
+                NodeList accolades = xmlDoc.getElementsByTagName("accolade");
+                
+                if (accolades !=null && accolades.getLength() > 0){
+                	for(int x = 0; x < accolades.getLength(); x++){
+                		//THIS IS WHERE THE MAGIC HAPPENS
+                		Node accolade = accolades.item(x);
+                		//temp = new Accolade();
+                		//TODO create accolade after reading in from xml
+                		/** make the accolade without ID
+                		 * if the xml has an id, then assign it afterwards
+                		 */
+                	}//END OF FOR
+                }//END OF IF
+                
+            }//END OF IF EXISTS
+		} catch(Exception E){
+			//TODO add in catch statements for each exception
+		
+		}//END OF TRYCATCH
+		return accoladeContainer;
+	}//END OF GET ACCOLADES
+
+}

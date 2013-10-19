a53f541630ac7e7228934e7d0af1a79f2762ff34
Dane Cousins <zethex@hotmail.com>
2013-10-19 22:13:22 +1000

    Clearing garbage
    Cleaning un needed code

--- deco2800.arcade.blackjack/src/main/java/deco2800/arcade/blackjack/client/Blackjack.java
+++ deco2800.arcade.blackjack/src/main/java/deco2800/arcade/blackjack/client/Blackjack.java
@@ -4,27 +4,19 @@ import java.util.HashSet;
 import java.util.Set;
 
 import com.badlogic.gdx.Gdx;
-import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.graphics.GL10;
 import com.badlogic.gdx.graphics.OrthographicCamera;
-import com.badlogic.gdx.graphics.g2d.BitmapFont;
-import com.badlogic.gdx.graphics.g2d.SpriteBatch;
-import com.badlogic.gdx.graphics.glutils.ShapeRenderer;
-import com.badlogic.gdx.graphics.glutils.ShapeRenderer.ShapeType;
-import com.badlogic.gdx.math.Vector2;
 
 import deco2800.arcade.model.Achievement;
 import deco2800.arcade.model.Game;
 import deco2800.arcade.model.Game.ArcadeGame;
 import deco2800.arcade.model.Player;
-import deco2800.arcade.protocol.achievement.AddAchievementRequest;
 import deco2800.arcade.protocol.game.CasinoServerUpdate;
-import deco2800.arcade.protocol.game.GameStatusUpdate;
 import deco2800.arcade.client.GameClient;
 import deco2800.arcade.client.network.NetworkClient;
 /**
- * A Pong game for use in the Arcade
- * @author uqjstee8
+ * A Blackjack Cainso game for use in the Arcade
+ * @author Dane Cousins, Kristian Zembic, Robert Macdonald, Scott Fredericks, Fionntan Shanahan
  *
  */
 @ArcadeGame(id="blackjack")
@@ -32,30 +24,15 @@ public class Blackjack extends GameClient {
 	
 	private OrthographicCamera camera;
 	
-	private enum GameState {
-		READY,
-		INPROGRESS,
-		GAMEOVER
-	}
-	private GameState gameState;
-	private int[] scores = new int[2];
-	private String[] players = new String[2]; // The names of the players: the local player is always players[0]
-	
 	public static final int WINNINGSCORE = 3;
 	public static final int SCREENHEIGHT = 480;
 	public static final int SCREENWIDTH = 800;
-	
-	private ShapeRenderer shapeRenderer;
-	private SpriteBatch batch;
-	private BitmapFont font;
-
-	private String statusMessage;
 
 	//Reusable list of achievements
 	private static Set<Achievement> achievements = new HashSet<Achievement>();
 	static {
-		Achievement winPong = new Achievement("Win a game of Pong");
-		achievements.add(winPong);
+		Achievement randomAchievement = new Achievement("Create these later");
+		achievements.add(randomAchievement);
 	}
 	
 	//Network client for communicating with the server.
@@ -63,14 +40,12 @@ public class Blackjack extends GameClient {
 	private NetworkClient networkClient;
 
 	/**
-	 * Basic constructor for the Pong game
+	 * Basic constructor for the Blackjack game
 	 * @param userName The name of the player
 	 * @param client The network client for sending/receiving messages to/from the server
 	 */
 	public Blackjack(Player player, NetworkClient networkClient) {
 		super(player, networkClient);
-		players[0] = player.getUsername();
-		players[1] = "Player 2"; //TODO eventually the server may send back the opponent's actual username
 		this.networkClient = networkClient; //this is a bit of a hack
 	}
 	
@@ -81,6 +56,7 @@ public class Blackjack extends GameClient {
 	public void create() {
 		super.create();
 		
+		//  This is a test message and an example 
 		CasinoServerUpdate msg = new CasinoServerUpdate();
 		msg.username = "test";
 		msg.message = "message test";
@@ -88,19 +64,7 @@ public class Blackjack extends GameClient {
 		
 		//Initialise camera
 		camera = new OrthographicCamera();
-		camera.setToOrtho(false, SCREENWIDTH, SCREENHEIGHT);
-		
-		/**
-		 * TODO Allow network games
-		 * 1. Create local player (LOBBY)
-		 * 2. "Waiting for other players. Press '1' to play local game against the computer"
-		 * 3a. Receive game join request
-		 * 3b. "Player 'Bob' wishes to join the game. Press 'Y' to accept"
-		 * 3c1. ('Y') Create Network player, move to READY
-		 * 3c2. ('N') Go to 2.
-		 */
-		
-		
+		camera.setToOrtho(false, SCREENWIDTH, SCREENHEIGHT);		
 	}
 
 	@Override
@@ -118,8 +82,6 @@ public class Blackjack extends GameClient {
 	 */
 	@Override
 	public void render() {
-
-		
 		//Black background
 		Gdx.gl.glClearColor(0, 0, 0, 1);
 	    Gdx.gl.glClear(GL10.GL_COLOR_BUFFER_BIT);

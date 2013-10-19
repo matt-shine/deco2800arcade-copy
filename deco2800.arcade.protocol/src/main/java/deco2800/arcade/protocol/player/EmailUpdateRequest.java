82d6982466b7a95932cd1aae9f1a7ccd0ad305a7
Leggy <lachlan.j.healey@gmail.com>
2013-10-19 13:17:15 +1000

    Improved PlayerListener Efficiency

--- deco2800.arcade.protocol/src/main/java/deco2800/arcade/protocol/player/EmailUpdateRequest.java
+++ deco2800.arcade.protocol/src/main/java/deco2800/arcade/protocol/player/EmailUpdateRequest.java
@@ -1,8 +1,6 @@
 package deco2800.arcade.protocol.player;
 
-import deco2800.arcade.protocol.NetworkObject;
-
-public class EmailUpdateRequest extends NetworkObject {
+public class EmailUpdateRequest extends PlayerNetworkObject {
 	private int playerID;
 	private String email;
 	

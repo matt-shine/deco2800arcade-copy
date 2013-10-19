72b671a07a773d251b4e153fef2bfac6074057e8
kalby <daniel.kalb@uqconnect.edu.au>
2013-10-20 05:39:55 +1000

    sneaky rename.

--- deco2800.arcade.towerdefence/src/main/java/deco2800/arcade/towerdefence/model/effects/EffCryoEnemy.java
+++ deco2800.arcade.towerdefence/src/main/java/deco2800/arcade/towerdefence/model/effects/EffCryoEnemy.java
@@ -8,29 +8,29 @@ import deco2800.arcade.towerdefence.model.creationclasses.Enemy;
  * @author hadronn
  * 
  */
-public class EffFrostEnemy extends Effect {
+public class EffCryoEnemy extends Effect {
 
 	/**
-	 * The Effect wrapper for the frost enemy function.
+	 * The Effect wrapper for the Cryo enemy function.
 	 * 
 	 * @param name
 	 * @param amount
 	 * @param duration
 	 */
-	public EffFrostEnemy(String name, int amount, int duration) {
+	public EffCryoEnemy(String name, int amount, int duration) {
 		super(name, amount, duration);
 	}
 
 	/**
 	 * Slow the move speed and attack rate of an enemy by the effect amount. Use
-	 * for Frost towers.
+	 * for Cryo towers.
 	 * 
 	 * @param target
 	 */
 	public void function(Enemy target) {
-		Effect SlowMS = new EffSlowEnemyMS("Frosty SlowEnemyMS", (int) amount,
+		Effect SlowMS = new EffSlowEnemyMS("Cryo SlowEnemyMS", (int) amount,
 				duration);
-		Effect SlowAR = new EffSlowEnemyAR("Frosty SlowEnemyAR", (int) amount,
+		Effect SlowAR = new EffSlowEnemyAR("Cryo SlowEnemyAR", (int) amount,
 				duration);
 		SlowMS.function(target);
 		SlowAR.function(target);

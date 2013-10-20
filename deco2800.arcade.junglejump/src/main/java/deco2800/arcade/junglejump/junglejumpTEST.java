2e80b23d4f4a39be0b2e45db2b8834c589e409fe
kurtbornhutter <kurt.bornhutter@gmail.com>
2013-10-21 09:15:52 +1000

    Tests

--- deco2800.arcade.junglejump/src/main/java/deco2800/arcade/junglejump/junglejumpTEST.java
+++ deco2800.arcade.junglejump/src/main/java/deco2800/arcade/junglejump/junglejumpTEST.java
@@ -1,9 +1,26 @@
 package deco2800.arcade.junglejump;
 
+import org.junit.Before;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.rules.ExpectedException;
+
 public class junglejumpTEST {
 	
-	LevelContainer container = new LevelContainer();
+	@Rule 
+	public ExpectedException thrown = ExpectedException.none();
 	
-	Level level = new Level();
-
+	@Before
+	public void init() {
+		LevelContainer container = new LevelContainer();
+		Level level = new Level();
+	}
+	
+	@Test (expected = Exception.class)
+	public void initWithTooManyLevels () {
+		
+	}
+	
+	
+	
 }

e766566a4e1c292f20efc852ae973814cba35510
Dylan Lerch <dylan.lerch@uqconnect.edu.au>
2013-10-20 19:34:57 +1000

    Class comments added to highscore exceptions

--- deco2800.arcade/src/main/java/deco2800/arcade/client/highscores/NoQueuedScoresException.java
+++ deco2800.arcade/src/main/java/deco2800/arcade/client/highscores/NoQueuedScoresException.java
@@ -1,6 +1,13 @@
 package deco2800.arcade.client.highscores;
 
 @SuppressWarnings("serial")
+
+/**
+ * A runtime exception that is thrown by HighscoreClient when a multivalued
+ * scores are trying to be sent to the database when none have been queued.
+ * 
+ * @author TeamA
+ */
 public class NoQueuedScoresException extends RuntimeException {
 
 	public NoQueuedScoresException() {

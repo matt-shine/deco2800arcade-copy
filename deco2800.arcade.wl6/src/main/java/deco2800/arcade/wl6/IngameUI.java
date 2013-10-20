d539f856f59ffd903c89cf3b9ec4dd3ec60997dc
abbjohn <abbjohn@gmail.com>
2013-10-20 16:57:49 +1000

    Trying to get a background working.

--- deco2800.arcade.wl6/src/main/java/deco2800/arcade/wl6/IngameUI.java
+++ deco2800.arcade.wl6/src/main/java/deco2800/arcade/wl6/IngameUI.java
@@ -2,6 +2,7 @@ package deco2800.arcade.wl6;
 
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.Color;
+import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.glutils.ShapeRenderer;
 import com.badlogic.gdx.scenes.scene2d.Stage;
 import com.badlogic.gdx.scenes.scene2d.ui.Label;
@@ -15,6 +16,7 @@ public class IngameUI extends Stage {
     private static ShapeRenderer r = new ShapeRenderer();
     private Skin skin;
     private Table bottom;
+    private Label epi;
     private Label lvl;
     private Label score;
     private Label difficulty;
@@ -37,20 +39,23 @@ public class IngameUI extends Stage {
     
     public IngameUI() {
         skin = new Skin(Gdx.files.internal("wl6Skin.json"));
+        skin.add("background", new Texture("wolfUI.png"));
         setupUI();
     }
 
     private void setupUI() {
 
-        Label dffL = new Label("Difficulty", skin, "small");
+        Label epiL = new Label("Episode", skin, "small");
         Label lvlL = new Label("Level", skin, "small");
+        Label dffL = new Label("Difficulty", skin, "small");
         Label scoreL = new Label("Score", skin, "small");
         Label healthL = new Label("Health", skin, "small");
         Label ammoL = new Label("Ammo", skin, "small");
         Label keysL = new Label("Keys", skin, "small");
         Label gunL = new Label("Gun", skin, "small");
-        difficulty = new Label("", skin, "small");
+        epi = new Label("null", skin, "small");
         lvl = new Label("null", skin, "small");
+        difficulty = new Label("", skin, "small");
         score = new Label("null", skin, "small");
         health = new Label("null", skin, "small");
         ammo = new Label("null", skin, "small");
@@ -59,38 +64,42 @@ public class IngameUI extends Stage {
 
         bottom = new Table();
         bottom.setPosition(640, 50);
-
-        bottom.add(dffL).width(150);
-        bottom.add(lvlL).width(150);
-        bottom.add(scoreL).width(300);
-        bottom.add(healthL).width(150);
-        bottom.add(ammoL).width(150);
-        bottom.add(keysL).width(80);
-        bottom.add(gunL).width(300);
-        bottom.row();
-        bottom.add(difficulty).width(150);
-        bottom.add(lvl).width(105);
-        bottom.add(score).width(300);
-        bottom.add(health).width(150);
-        bottom.add(ammo).width(150);
-        bottom.add(keys).width(80);
-        bottom.add(gun).width(300);
-
-
+        bottom.setBackground(skin.getDrawable("background"));
         this.addActor(bottom);
+
+        bottom.add(epiL).width(134);
+        bottom.add(lvlL).width(134);
+        bottom.add(dffL).width(134);
+        bottom.add(scoreL).width(268);
+        bottom.add(healthL).width(134);
+        bottom.add(ammoL).width(134);
+        bottom.add(keysL).width(74);
+        bottom.add(gunL).width(268);
+        bottom.row();
+        bottom.add(epi).width(134);
+        bottom.add(lvl).width(134);
+        bottom.add(difficulty).width(134);
+        bottom.add(score).width(268);
+        bottom.add(health).width(134);
+        bottom.add(ammo).width(134);
+        bottom.add(keys).width(74);
+        bottom.add(gun).width(268);
     }
     
     
     public void draw(GameModel game) {
     	super.draw();
 
-        difficulty.setText("" + game.getDifficulty());
+        epi.setText(game.getChapter());
         lvl.setText(game.getLevelInChapter());
+        difficulty.setText("" + game.getDifficulty());
         score.setText("" + game.getPlayer().getPoints());
-        this.health.setText("" + game.getPlayer().getHealth() + "%");
-        this.ammo.setText("" + game.getPlayer().getAmmo());
+        health.setText("" + game.getPlayer().getHealth() + "%");
+        ammo.setText("" + game.getPlayer().getAmmo());
         keys.setText("" + (game.getPlayer().hasKey(KEY_TYPE.GOLD) ? "Gold " : "") + (game.getPlayer().hasKey(KEY_TYPE.SILVER) ? "Silver " : ""));
-        this.gun.setText("" + game.getPlayer().getCurrentGun());
+        gun.setText("" + game.getPlayer().getCurrentGun());
+
+        bottom.setBackground(skin.getDrawable("background"));
     }
 
 

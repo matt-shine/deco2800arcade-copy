fe608b12dd14d3878c3ab9ecead60b45cb6816b4
pcetx <moulthunl@gmail.com>
2013-10-20 10:31:25 +1000

    add extend

--- deco2800.arcade.userui/src/main/java/deco2800/arcade/userui/Profile.java
+++ deco2800.arcade.userui/src/main/java/deco2800/arcade/userui/Profile.java
@@ -6,11 +6,12 @@ import deco2800.arcade.client.GameClient;
 import deco2800.arcade.client.network.NetworkClient;
 import deco2800.arcade.model.Game;
 import deco2800.arcade.model.Game.ArcadeGame;
+import deco2800.arcade.model.Game.InternalGame;
 import deco2800.arcade.model.Player;
 import deco2800.arcade.userui.controller.ControllerMain;
 import deco2800.arcade.userui.view.UserScreen;
 
-
+@InternalGame
 @ArcadeGame(id = "profileui")
 public class Profile extends GameClient {
 	

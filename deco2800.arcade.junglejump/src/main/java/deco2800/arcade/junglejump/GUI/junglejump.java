88316cf21372a7e1768b5b263fe0eed03b8dd83c
cammyd2 <cammyd2@hotmail.com>
2013-10-21 04:46:44 +1000

    world 4 and 5 polished

--- deco2800.arcade.junglejump/src/main/java/deco2800/arcade/junglejump/GUI/junglejump.java
+++ deco2800.arcade.junglejump/src/main/java/deco2800/arcade/junglejump/GUI/junglejump.java
@@ -363,7 +363,7 @@ public class junglejump extends GameClient implements InputProcessor {
 			achievementTitleFont.draw(batch, "BACKSPACE for MENU", SCREENWIDTH-250, SCREENHEIGHT-30);
 			achievementTitleFont.draw(batch, ("Bananas remaining: " + (TOTAL_BANANAS - BANANAS_FOUND)), SCREENWIDTH-500, SCREENHEIGHT-10);
 			achievementTitleFont.draw(batch, ("Lives: " + lives), SCREENWIDTH-750, SCREENHEIGHT-10);
-			achievementTitleFont.draw(batch, ("Level " + LevelContainer.getCurrentWorld()+1 + " - " + (currentLevel.getIndex()+1)%5), SCREENWIDTH-750, SCREENHEIGHT-30);
+			achievementTitleFont.draw(batch, ("Level " + (LevelContainer.getCurrentWorld()+1) + " - " + (currentLevel.getIndex()+1)%6), SCREENWIDTH-750, SCREENHEIGHT-30);
 
 
 			batch.end();

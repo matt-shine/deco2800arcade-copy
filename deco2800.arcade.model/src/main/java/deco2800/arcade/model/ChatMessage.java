d34834f1977e08ec0aab4dfc60e435d7766d7f0b
TommyYE <Jyh@m6zais(Tkoun>
2013-10-22 00:39:14 +1000

    comment

--- deco2800.arcade.model/src/main/java/deco2800/arcade/model/ChatMessage.java
+++ deco2800.arcade.model/src/main/java/deco2800/arcade/model/ChatMessage.java
@@ -5,15 +5,24 @@ public class ChatMessage<username, message> {
 	private String username;
 	private String message;
 
+	/**
+	 * Initial the chat message
+	 * */
 	public ChatMessage(String username, String message) {
 		this.username = username;
 		this.message = message;
 	}
 
+	/**
+	 * Get the message sender
+	 * */
 	public String getUsername() {
 		return username;
 	}
 
+	/**
+	 * Get user's message
+	 * */
 	public String getMessage() {
 		return message;
 	}
